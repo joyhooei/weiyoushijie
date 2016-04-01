d41ae45992b8b8dcc3228e854a616aac23afcf3c
li weihai <weihailee@hotmail.com>
2016-04-01 08:23:03 +0000

    LoginUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/LoginUI.ts
+++ public/mobile/src/views/LoginUI.ts
@@ -11,12 +11,11 @@ class LoginUI extends eui.Component implements nest.easeuser.ILoginCallbacks{
     public constructor() {
         super();
       
-        this.skinName = = "resource/custom_skins/loginUISkin.exml";
+        this.addEventListener( eui.UIEvent.COMPLETE, this.uiCompHandler, this );
+        this.skinName = "resource/custom_skins/loginUISkin.exml";
 	  }
 	
-	  public createChildren(){
-        super.createChildren();  
-      
+	  private uiCompHandler():void {     
         this.login_button.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onTouchTapHandler,this);   
 	  }
 	
@@ -30,7 +29,7 @@ class LoginUI extends eui.Component implements nest.easeuser.ILoginCallbacks{
 	  }
 
     public onCreate = (data: nest.easeuser.ILoginTypes):void =>  {
-        router.changePage(new LoginTypeView(data));
+        router.changePage(new LoginTypeUI(data));
     };
 
     public onSuccess = (data: nest.user.LoginCallbackInfo):void => {
