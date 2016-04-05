848600326158b9e66d2c99a24654b1410a1dd2b8
li weihai <weihailee@hotmail.com>
2016-04-05 07:52:16 +0000

    Application.ts 已使用 Bitbucket 在线创建

---
+++ public/mobile/src/Application.ts
@@ -0,0 +1,9 @@
+module router {
+    export var router: Router;
+    export var dao: Dao;
+
+    export function init(main:Main){
+		router = new Router(main);
+		dao = new Dao("http://headlines.leanapp.cn/api/");
+    }
+}
