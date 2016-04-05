80d50a801a23ae3ce86a7e127786dac8734e2e07
li weihai <weihailee@hotmail.com>
2016-04-05 06:37:52 +0000

    LoginUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/LoginUI.ts
+++ public/mobile/src/views/LoginUI.ts
@@ -4,45 +4,39 @@
  *
  */
 class LoginUI extends eui.Component implements nest.easeuser.ILoginCallbacks{
-    public info_txt: eui.Label;
-    
-    public login_button: eui.Button;
-  
-    public constructor() {
-        super();
-      
-        this.addEventListener( eui.UIEvent.COMPLETE, this.uiCompHandler, this );
-        this.skinName = "resource/custom_skins/loginUISkin.exml";
-	  }
-	
-	  private uiCompHandler():void {     
-        this.login_button.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onTouchTapHandler,this);   
-	  }
-	
-	  private onTouchTapHandler(e:egret.TouchEvent):void{
-        var self = this;
-        nest.core.startup({egretAppId : 88888, version : 2, debug:true}, function(resultInfo:nest.core.ResultCallbackInfo) {
-            if (resultInfo.result == 0) {
-                self.checkLogin();
-            }
-        });
-	  }
+	public info_txt: eui.Label;
 
-    public onCreate = (data: nest.easeuser.ILoginTypes):void =>  {
-        router.changePage(new LoginTypeUI(data));
-    };
+	public login_button: eui.Button;
 
-    public onSuccess = (data: nest.user.LoginCallbackInfo):void => {
-        new Login().login(data);
-    };
+	public constructor() {
+		super();
 
-    public onFail = (data: nest.core.ResultCallbackInfo):void => {
-        egret.log("log Fail");
-    };
+		this.addEventListener( eui.UIEvent.COMPLETE, this.uiCompHandler, this );
+		this.skinName = "resource/custom_skins/loginUISkin.exml";
+	}
 
-	  private checkLogin():void{
-        egret.log("checkLogin start");
+	private uiCompHandler():void {     
+		this.login_button.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onTouchTapHandler,this);   
+	}
 
-        nest.easeuser.login(this);
-	  }
+	private onTouchTapHandler(e:egret.TouchEvent):void{
+		var self = this;
+		nest.core.startup({egretAppId : 90240, version : 2, debug:true}, function(resultInfo:nest.core.ResultCallbackInfo) {
+			if (resultInfo.result == 0) {
+				nest.easeuser.login(self);
+			}
+		});
+	}
+
+	public onCreate = (data: nest.easeuser.ILoginTypes):void =>  {
+		router.changePage(new LoginTypeUI(data));
+	}
+
+	public onSuccess = (data: nest.user.LoginCallbackInfo):void => {
+		new Login().login(data);
+	}
+
+	public onFail = (data: nest.core.ResultCallbackInfo):void => {
+		egret.log("log Fail");
+	}
 }
