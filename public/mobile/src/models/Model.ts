deea8a1a323a9e812e348a90ebeedb7bc0d5d820
li weihai <weihailee@hotmail.com>
2016-04-08 02:25:11 +0000

    Model.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/models/Model.ts
+++ public/mobile/src/models/Model.ts
@@ -1,4 +1,15 @@
 class Model {
+	public id:string;
+	
+	public attributes:any;
+	
 	public constructor() {
+	}
+	
+	public set(data:any):void{
+		attributes = data;
+		if (data.id) {
+			id = data.id;
+		}
 	}
 }
