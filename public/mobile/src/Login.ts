36ee69b7d6973351e940cb7783f910a28803499b
li weihai <weihailee@hotmail.com>
2016-04-08 02:15:21 +0000

    Login.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/Login.ts
+++ public/mobile/src/Login.ts
@@ -21,21 +21,7 @@ class Login extends egret.EventDispatcher{
         egret.log(JSON.stringify(data));
         if (data.result == 0){
 			//从后台获取用户信息
-			application.dao.rest(application.baseUrl, "login", data.token, (succeed: bool, data: any) => {
-				if (succeed) {
-					application.account.set(data);
-					application.dao.fetch(new Customer({id:application.account.customer_id}), (succeed: bool, data: any) => {
-						if (succeed) {
-							application.customer.set(data);
-							application.dispatchEvent(GameEvents.EVT_LOGIN_IN_SUCCESS);
-						} else {
-							Toast.launch("获取用户信息失败");
-						}
-					});
-				} else {
-					Toast.launch("获取账号信息失败");
-				}
-        	});
+			application.login(data.token);
         } else{
             Toast.launch("登录失败");
         }
