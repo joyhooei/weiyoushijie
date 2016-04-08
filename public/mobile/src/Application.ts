7949f8539c5e37a29eba6a89a6764b51340815ab
li weihai <weihailee@hotmail.com>
2016-04-08 02:14:24 +0000

    Application.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/Application.ts
+++ public/mobile/src/Application.ts
@@ -1,9 +1,35 @@
 module application {
+    var _main: Main;
     export var router: Router;
     export var dao: Dao;
+    export var account: Account;
+    export var customer: Customer;
 
-    export function init(main:Main){
+    export function init(main:Main) {
+		_main = main;
+		
 		router = new Router(main);
 		dao = new Dao("http://headlines.leanapp.cn/api/");
+        
+        account = new Account();
+        customer = new Customer();
     }
+    
+    export function login(token:string, cb:Function) {
+		dao.rest("login", token, (succeed: bool, data: any) => {
+			if (succeed) {
+				account.set(data);
+				dao.fetch("Customer", {"id": application.account.customer_id}, {}, (succeed: bool, data: any) => {
+					if (succeed) {
+						customer.set(data[0]);
+						_main.dispatchEvent(GameEvents.EVT_LOGIN_IN_SUCCESS);
+					} else {
+						Toast.launch("获取用户信息失败");
+					}
+				});
+			} else {
+				Toast.launch("获取账号信息失败");
+			}
+		});
+	}
 }
