fd595ee79013bcfb87e3d7b4f8a34a6d14023cb3
li weihai <weihailee@hotmail.com>
2016-04-11 00:50:34 +0000

    Application.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/Application.ts
+++ public/mobile/src/Application.ts
@@ -2,7 +2,6 @@ module application {
     export var main: Main;
     export var router: Router;
     export var dao: Dao;
-    export var account: any;
     export var customer: any;
 
     export function init(main:Main) {
@@ -16,8 +15,6 @@ module application {
     export function login(data?:string|nest.user.LoginCallbackInfo):void {
         if (data == null || typeof data == "string") {
             var loginInfo: nest.user.LoginInfo = data ? {"loginType":<string>data} : {};
-            egret.log(JSON.stringify(loginInfo));
-			
             nest.user.login(loginInfo, application.onLoginCallback);
         } else {
             application.onLoginCallback(<nest.user.LoginCallbackInfo>data);
@@ -25,22 +22,12 @@ module application {
     }
 
     export function onLoginCallback(data:nest.user.LoginCallbackInfo):void{
-        egret.log(JSON.stringify(data));
-		
         if (data.result == 0){
 			//从后台获取用户信息
 			application.dao.rest("login", {token: data.token}, (succeed: boolean, data: any) => {
 				if (succeed) {
-					application.account = data;
-					application.dao.fetch("Customer", {"id": application.account.customer_id}, {}, (succeed: boolean, data: any) => {
-						if (succeed) {
-							application.customer = data[0];
-
-							application.main.dispatchEventWith(GameEvents.EVT_LOGIN_IN_SUCCESS);
-						} else {
-							Toast.launch("获取用户信息失败");
-						}
-					});
+					application.customer = data;
+					application.main.dispatchEventWith(GameEvents.EVT_LOGIN_IN_SUCCESS);
 				} else {
 					Toast.launch("获取账号信息失败");
 				}
