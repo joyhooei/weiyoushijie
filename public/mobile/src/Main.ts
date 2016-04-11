c8c0480e90be5a99f2ec93813e1c51b80b071826
li weihai <weihailee@hotmail.com>
2016-04-11 02:37:37 +0000

    Main.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/Main.ts
+++ public/mobile/src/Main.ts
@@ -1,37 +1,6 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-2015, Egret Technology Inc.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
-
-class Main extends eui.UILayer implements nest.easeuser.ILoginCallbacks {
-    /**
-     * 加载进度界面
-     * loading process interface
-     */
+class Main extends eui.UILayer {
+    private _loadingUI: LoadingUI;
+    
     private _isThemeLoadEnd: boolean = false;
 
     private _isResourceLoadEnd: boolean = false;
@@ -54,7 +23,8 @@ class Main extends eui.UILayer implements nest.easeuser.ILoginCallbacks {
       
         //Config loading process interface
         //设置加载进度界面
-        this.addChild(new LoadingUI());
+        this._loadingUI = new LoadingUI();
+        this.stage.addChild(this._loadingUI);
       
         // initialize the Resource loading library
         //初始化Resource资源加载库
@@ -113,7 +83,13 @@ class Main extends eui.UILayer implements nest.easeuser.ILoginCallbacks {
                
                 nest.core.startup({ egretAppId: 90240,version: 2,debug: true },function(resultInfo: nest.core.ResultCallbackInfo) {
                     if(resultInfo.result == 0) {
-                        nest.easeuser.login(self);
+                        nest.easeuser.login(function (data) {
+							if(data.token) {
+								application.login(data);
+							} else {
+								Toast.launch("登录失败");
+							}
+						});
                     }
                 });
                 break;
@@ -224,17 +200,5 @@ class Main extends eui.UILayer implements nest.easeuser.ILoginCallbacks {
         if( this._trueLoadingUI.parent ){
             this._trueLoadingUI.parent.removeChild( this._trueLoadingUI );
         }
-    }
-    
-    public onCreate = (data: nest.easeuser.ILoginTypes): void => {
-        application.router.changePage(new LoginUI(data));
-    }
-
-    public onSuccess = (data: nest.user.LoginCallbackInfo): void => {
-        application.login(data);
-    }
-
-    public onFail = (data: nest.core.ResultCallbackInfo): void => {
-        egret.log("log Fail");
     }
 }
