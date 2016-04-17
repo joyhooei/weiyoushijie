7888337e8d307d16d3469bf30f43c83b9bdb92ed
li weihai <liweihai2009@gmail.com>
2016-04-17 21:50:03 +0800

    unlock

--- public/mobile/src/views/ProjectItem.ts
+++ public/mobile/src/views/ProjectItem.ts
@@ -60,4 +60,40 @@ class ProjectItem extends eui.Component {
             });
         }
     }
+    
+    private unlock(){
+        let self = this;
+
+        let p = this._project.priceOf(1);
+        if(application.customer.gold > p) {
+            self._myProject.unlocked = 1;
+            application.dao.save("Project",self._myProject,function(succeed,proj) {
+                if(succeed) {
+                    application.customer.gold -= p;
+                    application.customer.output += self._project.output(self._myProject.level,application.customer.achieve,application.customer.prop);
+                    application.dao.save("Customer",application.customer,function(succeed,c) {
+                        if(succeed) {
+                            var project:any;
+                            project.customer_id = application.customer.id;
+                            project.sequence = self._myProject.sequence + 1;
+                            project.unlocked = 0;
+                            project.achieve = 0;
+                            project.level = 0;
+                            application.dao.save("Project",project,function(succeed,proj) {
+                                if (succeed) {
+                                    
+                                } else {
+                                    Toast.launch("解锁失败");
+                                }
+                            });
+                        } else {
+                            Toast.launch("解锁失败");
+                        }
+                    });
+                } else {
+                    Toast.launch("解锁失败");
+                }
+            });
+        }
+    }
 }
