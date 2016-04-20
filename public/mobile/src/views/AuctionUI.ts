243ca61f028c04fc7a0bc5a74ed0de9d3482e941
li weihai <weihailee@hotmail.com>
2016-04-20 07:06:28 +0000

    AuctionUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/AuctionUI.ts
+++ public/mobile/src/views/AuctionUI.ts
@@ -2,23 +2,16 @@
  * Created by egret on 2016/1/26.
  */
 
-class AboutUI extends eui.Component{
-
+class AuctionUI extends eui.Component{
     constructor() {
         super();
         this.addEventListener( eui.UIEvent.COMPLETE, this.uiCompHandler, this );
-        this.skinName = "resource/custom_skins/aboutUISkin.exml";
+        this.skinName = "resource/custom_skins/auctionUISkin.exml";
     }
 
     private uiCompHandler():void {
-        console.log( "\t\tAboutUI uiCompHandler" );
         this.btnClose.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
-            this.dispatchEventWith( GameEvents.EVT_CLOSE_ABOUT );
-        }, this );
-        this.addEventListener( egret.TouchEvent.TOUCH_TAP, ( evt:egret.TouchEvent )=>{
-            if( evt.stageX > 94 && evt.stageX < 194 && evt.stageY > 314 && evt.stageY < 414 ){
-                Toast.launch( "美术：刘国萍\n程序：王建文\n策划：范承鑫" ); 
-            }
+            this.dispatchEventWith( GameEvents.EVT_RETURN );
         }, this );
     }
     
