2171fd3bd4497b566a8a6c52b94dd065d05a7d53
li weihai <weihailee@hotmail.com>
2016-04-23 03:10:05 +0000

    Application.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/Application.ts
+++ public/mobile/src/Application.ts
@@ -42,7 +42,7 @@ module application {
         application.customer.output    += output;
         application.dao.save("Customer", application.customer, function(succeed, c){
             if (succeed) {
-                application.main.homeUI.refreshCustomer(gold, diamond, output);
+                application.main.homeUI.animateCustomer(gold, diamond, output);
             }
 			
 			cb(succeed, c);
