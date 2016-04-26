c0afa23fc8b1f9be65927587e0b724a622ff1aeb
li weihai <weihailee@hotmail.com>
2016-04-26 03:35:46 +0000

    api.js 已在 Bitbucket 中在线编辑过

--- routes/api.js
+++ routes/api.js
@@ -88,7 +88,7 @@ router.post('/login', function(req, res, next) {
 				query.equalTo("uid", result.data.id);
 				query.find().then(function(customers){
 					if (customers.length > 0) {
-						_succeed(res, _decode(customers[0]));
+						_logined(res, customers[0]);
 					} else {
 						var customer = new dao.Customer();
 						customer.set("uid", result.data.id);
@@ -101,7 +101,7 @@ router.post('/login', function(req, res, next) {
 						customer.set("diamond", 100);
 						customer.set("metal", 0);
 						customer.save().then(function(){
-							_succeed(res, _decode(customer));
+							_logined(res, customer);
 						}, function(error){
 							_failed(res, error);
 						})				
@@ -118,6 +118,25 @@ router.post('/login', function(req, res, next) {
 	});
 });
 
+function _logined(customer, res) {
+	var now  = moment();
+	var last = moment(customer.updatedAt);
+	
+	var diff = last.diff(now, 'seconds');
+	
+	//最多8个小时
+	diff = Math.min(8 * 60 * 60, diff);
+	if (diff > 60) {
+		customer.increment("gold", diff * customer.get("output"));
+		customer.save().then(function(c){
+			_succeed(res, _decode(c));
+		}, function(error) {
+			_failed(res, error);
+		});
+	} else {
+		_succeed(res, _decode(customer));
+	}
+};
 
 router.post('/select/:model', function(req, res, next) {
 	var query = new AV.Query(dao[req.params.model]);
