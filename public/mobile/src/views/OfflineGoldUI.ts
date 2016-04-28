0faab2b6618296f9d8552ed562119e79fc6c55bf
li weihai <weihailee@hotmail.com>
2016-04-28 07:16:58 +0000

    OfflineGoldUI.ts 已使用 Bitbucket 在线创建

---
+++ public/mobile/src/views/OfflineGoldUI.ts
@@ -0,0 +1,16 @@
+class OfflineGoldUI extends eui.Component{
+    constructor() {
+        super();
+        
+        this.addEventListener( eui.UIEvent.COMPLETE, this.uiCompHandler, this );
+        this.skinName = "resource/custom_skins/auctionUISkin.exml";
+    }
+
+    private uiCompHandler():void {
+        this.btnClose.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
+            this.dispatchEventWith( GameEvents.EVT_RETURN );
+        }, this );
+    }
+    
+    private btnClose:eui.Button;
+}
