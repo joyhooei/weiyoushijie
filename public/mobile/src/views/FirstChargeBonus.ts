9cd6c1627f905d449f10db1fc0a01febb5bc8719
li weihai <weihailee@hotmail.com>
2016-04-28 07:22:29 +0000

    FirstChargeBonus.ts 已使用 Bitbucket 在线创建

---
+++ public/mobile/src/views/FirstChargeBonus.ts
@@ -0,0 +1,15 @@
+class FirstChargeBonusUI extends eui.Component{
+    constructor() {
+        super();
+        this.addEventListener( eui.UIEvent.COMPLETE, this.uiCompHandler, this );
+        this.skinName = "resource/custom_skins/auctionUISkin.exml";
+    }
+
+    private uiCompHandler():void {
+        this.btnClose.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
+            this.dispatchEventWith( GameEvents.EVT_RETURN );
+        }, this );
+    }
+    
+    private btnClose:eui.Button;
+}
