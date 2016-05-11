f9169c943f7b6b3985a2f98a46c3fe89ac83b7a6
li weihai <weihailee@hotmail.com>
2016-05-11 01:23:43 +0000

    AuctionUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/AuctionUI.ts
+++ public/mobile/src/views/AuctionUI.ts
@@ -5,7 +5,8 @@ class AuctionUI extends eui.Component{
 	private lblMaxBid:eui.Label;
 	private lblCurrentBid:eui.Label;
 	
-	private btnBid:eui.Button;
+	private imgBid:eui.Image;
+	private imgRet:eui.Image;
 	
 	private grpTrack: eui.Group;
 	private lblTrack: eui.Label;
@@ -13,8 +14,6 @@ class AuctionUI extends eui.Component{
 	private imgThumb: eui.Image;
 	
 	private bid:any;
-	
-	private gold:number;
 	
     constructor() {
         super();
@@ -31,6 +30,22 @@ class AuctionUI extends eui.Component{
 		
 		var dt = new Date();
 		var today = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
+		
+		self.renderLastBid(today);
+		self.renderMaxBid(today);		
+		
+        self.imgBid.addEventListener(egret.TouchEvent.TOUCH_BEGIN, self.onBid, this);
+		self.imgRet.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function() {
+			this.back();
+        }, this);
+		
+		self.grpTrack.touchEnabled = true;
+		self.grpTrack.addEventListener(egret.TouchEvent.TOUCH_MOVE, self.onChangeBid , this);
+    }
+	
+	private renderLastBid(today:string): void {
+		var self = this;
+		
         application.dao.fetch("Bid",{ succeed: 0, day :today}, {limit : 1}, function(succeed, bids){
             if (succeed && bids.length > 0) {
 				self.bid = bids[0];				
@@ -39,7 +54,13 @@ class AuctionUI extends eui.Component{
 			}
 			
 			self.lblLastBid.text = application.format(self.bid.gold);
+			
+			self.renderCurrentBid(self.bid.gold);
         })
+	}
+	
+	private renderMaxBid(today:string): void {
+		var self = this;
 		
         application.dao.fetch("Bid",{ succeed: 0, day :today }, {limit : 1, order:"gold DESC"}, function(succeed, bids){
             if (succeed && bids.length > 0) {
@@ -48,33 +69,42 @@ class AuctionUI extends eui.Component{
 				self.lblMaxBid.text = "0";
 			}
         })
+	}
+	
+	private renderCurrentBid(gold: number): void {
+		this.bid.gold = Math.round(gold);
 		
-		self.lblCurrentBid.text = "0";
-		self.gold = 0;
+		this.lblCurrentBid.text = application.format(this.bid.gold);
+	}
+	
+	private onBid(): void {
+		var self = this;
 		
-        self.btnBid.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function() {
-			self.bid.gold = self.gold;
-			application.dao.save("Bid", self.bid, null);
-			
-			self.dispatchEventWith( GameEvents.EVT_RETURN );
-        }, this);		
-		
-		self.grpTrack.touchEnabled = true;
-		self.grpTrack.addEventListener(egret.TouchEvent.TOUCH_MOVE,function(e:egret.TouchEvent){
-			console.log('touch move', e);
-			
-			let tx:number = e.localX;
-			tx = Math.max(self.imgThumb.x, tx);
-			tx = Math.min(self.imgThumb.width + self.imgThumb.x - this.grpTrack.width, tx);
-			self.grpTrack.x = tx;
-			
-			var percent = Math.round( 100 * (tx - self.imgThumb.x)  / (self.imgThumb.width - self.grpTrack.width) );
-			self.lblTrack.text = percent.toString() + "%";
-			
-			self.imgFront.width = tx - self.imgThumb.x;
-			
-			self.gold = Math.round(application.customer.gold * percent / 100);
-			self.lblCurrentBid.text = application.format(self.gold);
-		}, this);
-    }
+		application.dao.save("Bid", self.bid, function(succeed, bid){
+			if (succeed) {
+				Toast.launch("投标成功");
+				
+				self.back();
+			} else {
+				Toast.launch("投标失败，请稍后再试");
+			}
+		});
+	}
+	
+	private back(): void {
+		this.dispatchEventWith( GameEvents.EVT_RETURN );
+	}
+	
+	private onChangeBid(e:egret.TouchEvent): void {
+		let tx:number = e.stageX;
+		tx = Math.max(this.imgThumb.x, tx);
+		tx = Math.min(this.imgThumb.width + this.imgThumb.x - this.grpTrack.width, tx);
+		this.grpTrack.x = tx;
+
+		var percent = Math.round( 100 * (tx - this.imgThumb.x)  / (this.imgThumb.width - this.grpTrack.width) );
+		this.lblTrack.text = percent.toString() + "%";
+
+		this.renderCurrentBid(application.customer.gold * percent / 100);
+		this.imgFront.width = tx - this.imgThumb.x;	
+	}
 }
