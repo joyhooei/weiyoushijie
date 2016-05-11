297c217fae8620ff3c88ecb51e16a5edbf55db4c
li weihai <weihailee@hotmail.com>
2016-05-11 01:39:30 +0000

    RankUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/RankUI.ts
+++ public/mobile/src/views/RankUI.ts
@@ -5,17 +5,30 @@ class RankUI extends eui.Component {
         super();
         
         this.addEventListener( eui.UIEvent.COMPLETE, this.uiCompHandler, this );
+        
         this.skinName = "resource/custom_skins/rankUISkin.exml";
     }
 
     private uiCompHandler():void {
 		var self = this;
-        application.dao.fetch("Customer", {}, {limit: 8, order: ' DESC'}, function(succeed, customers){
-               var dsCustomers:Array<Object> = new Array<Object>();
+		
+        application.dao.fetch("Customer", {}, {limit: 8, order: 'metal DESC'}, function(succeed, customers){
+            var dsCustomers:Array<Object> = new Array<Object>();
 			   
             if (succeed) {
                 for(var i = 0; i < customers.length; i++) {
-                    dsCustomers.push({ icon: customers[i].avatar,name: customers[i].name,metal: customers[i].metal});
+					if (i == 0) {
+						var bg = "RR_png";
+					} else if (i == 1) {
+						var bg = "RBlue_png";
+					} else if (i == 2) {
+						var bg = "RB_png";
+					} else {
+						var bg = "RG_png";
+					}
+					
+					var c = customers[i];
+                    dsCustomers.push({ bg: bg, icon: c.avatar, name: c.name, metal: c.metal, gold: c.gold });
                 }
             }
 				
