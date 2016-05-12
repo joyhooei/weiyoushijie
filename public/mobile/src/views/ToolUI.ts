7ba27c7672c689944f9675200154e5a23c4dccb0
li weihai <weihailee@hotmail.com>
2016-05-12 01:48:41 +0000

    ToolUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/ToolUI.ts
+++ public/mobile/src/views/ToolUI.ts
@@ -1,6 +1,11 @@
 class ToolUI extends eui.Component {
     private imgRet: eui.Image;
     
+    private imgHit: eui.Image;
+    private imgTime: eui.Image;
+    private imgTicket: eui.Image;
+    private imgVIP: eui.Image;
+    
     private grpProject: eui.List;
     
     constructor() {
@@ -28,11 +33,55 @@ class ToolUI extends eui.Component {
         tLayout.requestedColumnCount = 2;  /// 设置两列显示
         this.grpProject.layout = tLayout;    /// 网格布局
         
-        for(var i = 0; i < 20; i++) {
-            let item: ToolItem = new ToolItem(application.projects[i], (i + 1).toString() + "_png","t" + (i + 1).toString() + "_png");
+        self.grpProject.removeChildren();
+        application.dao.fetch("Project",{ customer_id: application.customer.id },{ order: 'sequence asc' },function(succeed, projects) {
+            if(succeed && projects.length > 0) {
+                for(var i = 0; i < projects.length; i ++){                    
+                    self.addProject(projects[i]);
+                }
+            }
+        });
+        
+        self.imgHit.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function() {
+            this.buyHit();
+        },this);
+        
+        self.imgTime.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function() {
+            this.buyTime();
+        },this);
+        
+        self.imgTicket.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function() {
+            this.buyTicket();
+        },this);
+        
+        self.imgVIP.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function() {
+            this.buyVIP();
+        },this);
+    }
+    
+	//爆击。每4小时自动获取一个，最多拥有3个。“点击”可以获取10倍的收益，持续60秒。100钻石可以增加至3个。说明里提醒玩家先用完已有的，再购买，因为最多只能拥有3个。
+    private buyHit() {
+    }
+    
+	//时光沙漏， 需要500钻石购买，产生相当于2天的产量。 总秒产*3600*48
+    private buyTime() {
+    }
+    
+	//月票，19元每月(30天）。每天登录可以领取300钻石，离线收益增加至90%，持续12小时。普通情况下离线收益为70%，持续8小时。首次购买获得1个勋章
+    private buyTicket() {
+    }
+    
+	//终身VIP，49元。每天登录可以领取300钻石，离线收益增加至90%，持续12小时。
+    private buyVIP() {
+    }
+    
+	private addProject(proj) {
+    	if (proj) {
+            let i = proj.sequence;
+            let item: ToolItem = new ToolItem(proj, application.projects[i], (i + 1).toString() + "_png", "t" + (i + 1).toString() + "_png");
             this.grpProject.addChildAt(item,i);
         }
-    }
+	}
     
     private back(): void {
         this.dispatchEventWith(GameEvents.EVT_RETURN);
