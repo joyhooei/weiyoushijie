3702eab734c67a1ac5e117d2d17ba3d2d9dde4cb
li weihai <weihailee@hotmail.com>
2016-05-12 12:11:38 +0000

    cloud.js 已在 Bitbucket 中在线编辑过

--- cloud.js
+++ cloud.js
@@ -1,9 +1,14 @@
 var AV = require('leanengine');
 
 var Customer = require('./models/customer.js');
+var Bid = require('./models/bid.js');
 
 AV.Cloud.afterSave("Customer", function(request, response) {
 	Customer.afterSave(request, response);
+});
+
+AV.Cloud.define('open_bid', function(request, response) {
+	Bid.open(request, response);
 });
 
 module.exports = AV.Cloud;
