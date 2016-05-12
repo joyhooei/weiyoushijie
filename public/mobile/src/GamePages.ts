3c6edca2953a6d375bb8121b28e34bd82d4d8392
li weihai <weihailee@hotmail.com>
2016-05-12 03:38:10 +0000

    GamePages.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/GamePages.ts
+++ public/mobile/src/GamePages.ts
@@ -1,1 +1,13 @@
-/** * Created by egret on 2016/1/26. */class GamePages{    public static HOME:string = "home";    public static RANK:string = "rank";    public static TOOL:string = "tool";    public static AUCTION:string = "auction";}
+/**
+ * Created by egret on 2016/1/26.
+ */
+class GamePages{
+    public static HOME:string = "home";
+    public static RANK:string = "rank";
+    public static TOOL:string = "tool";
+    public static AUCTION:string = "auction";
+	
+	public static HELP:string = "help";
+	public static OFFLINEGOLD:string = "offline gold";
+	public static CHARGEBONUS:string = "charge bonus";
+}
