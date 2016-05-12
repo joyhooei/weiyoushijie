e11c049784b4244a2fcfbd5bbe84aa8e86f88ba0
li weihai <weihailee@hotmail.com>
2016-05-12 01:28:09 +0000

    ToolItem.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/ToolItem.ts
+++ public/mobile/src/views/ToolItem.ts
@@ -3,15 +3,37 @@ class ToolItem extends eui.Component {
 
     private imgIcon: eui.Image;
     private imgTitle: eui.Image;
+	
+	private img100: eui.Image;
+	private img900: eui.Image;
 
     public constructor(project: Project,iconName: string,titleName: string) {
         super();
 
         this._project = project;
 
+        this.addEventListener(eui.UIEvent.COMPLETE,this.uiCompHandler,this);
+        
         this.skinName = "resource/custom_skins/toolItemSkin.exml";
 
-        this.imgIcon.source = iconName;
+        this.imgIcon.source  = iconName;
         this.imgTitle.source = titleName;
     }
+    
+    private uiCompHandler(): void {
+		this.img100.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+			this.buy(100, 1);
+		}, this);
+		
+		this.img900.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+			this.buy(900, 10);
+		}, this);		
+    }
+	
+	private buy(price: number, level: number): void {
+		if (application.customer.diamond < price) {
+			//显示充值界面
+		} else {
+		}	
+	}
 }
