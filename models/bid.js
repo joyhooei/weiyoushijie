712f7a23dc1c96dec01a6b9cb200f49834e31b8d
li weihai <weihailee@hotmail.com>
2016-05-12 12:22:18 +0000

    bid.js 已使用 Bitbucket 在线创建

---
+++ models/bid.js
@@ -0,0 +1,44 @@
+var AV = require('leanengine');
+
+module.exports.open = function(request, response) {
+	var dt = new Date();
+	var today = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
+
+    var query = new AV.Query(dao.Bid);
+    query.equalTo('day', today);
+    query.addDescending("gold");
+	query.limit(1);
+    query.find().then(function(bids){
+    	if (bids.length > 0) {
+			var bid = bids[0];
+			bid.set("succeed", 1);
+			bid.save().then(function(b) {
+				var query = new AV.Query(dao.Customer);
+				query.get(bid.get("customer_id")).then(function(customer) {
+					customer.increment("gold", 0 - bid.get("gold"));
+					customer.increment("metal", 1);
+					customer.increment("diamond", 2000);
+					customer.save().then(function(c){
+						console.log(customer.get("name") + "获得了" + today + "的头条");
+    					response.success(customer.get("name") + "获得了" + today + "的头条");
+					}, function(error){
+						console.error(error.message);
+						response.error(error.message);
+					});
+				}, function(error) {
+					console.error(error.message);
+					response.error(error.message);
+				});
+			}, function(error){
+				console.error(error.message);
+				response.error(error.message);
+			});
+    	} else {
+			console.error("没有投标");
+    		response.error();
+    	}
+    }, function(error){
+		console.error(error.message);
+    	response.error(error.message);
+    });	
+}
