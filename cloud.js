141ca5d51f4638d2fc29d03875e3efba0d4efac1
li weihai <weihailee@hotmail.com>
2016-05-13 02:46:42 +0000

    cloud.js 已在 Bitbucket 中在线编辑过

--- cloud.js
+++ cloud.js
@@ -7,10 +7,6 @@ AV.Cloud.afterSave("Customer", function(request, response) {
 	Customer.afterSave(request, response);
 });
 
-AV.Cloud.beforeUpdate("Customer", function(request, response) {
-	Customer.beforeUpdate(request, response);
-});
-
 AV.Cloud.define('open_bid', function(request, response) {
 	Bid.open(request, response);
 });
