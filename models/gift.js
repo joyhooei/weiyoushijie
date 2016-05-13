b737f4eca9ff0d19d5628be14840cb93a6628457
li weihai <weihailee@hotmail.com>
2016-05-13 08:49:01 +0000

    gift.js 已使用 Bitbucket 在线创建

---
+++ models/gift.js
@@ -0,0 +1,12 @@
+var AV = require('leanengine');
+
+module.exports.create = function(customerId, category, diamond, metal, gold) {
+    var gift = new dao.Gift();
+    gift.set("customer_id", customerId);
+    gift.set("category", category);
+    gift.set("diamond", diamond);
+    gift.set("metal", metal);
+    gift.set("gold", gold);
+    gift.set("unlocked", 1);
+	return gift.save()；
+}
