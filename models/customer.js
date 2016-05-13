ed2a7ade4f3d815a3afd0187133c703a2e992d14
li weihai <weihailee@hotmail.com>
2016-05-13 02:46:13 +0000

    customer.js 已在 Bitbucket 中在线编辑过

--- models/customer.js
+++ models/customer.js
@@ -1,15 +1,5 @@
 var AV = require('leanengine');
 
-module.exports.beforeUpdate = function(request, response) {
-    var customer = request.object;
-	
-	customer.increment("online_seconds", moment().diff(customer.get("last_login"), 'seconds'));
-    customer.set("last_login", moment().toDate());
-    customer.set("last_hit", moment(customer.get("last_hit")).toDate());
-    
-	response.success();
-};
-
 module.exports.afterSave = function(request, response) {
     var customer = request.object;
     
