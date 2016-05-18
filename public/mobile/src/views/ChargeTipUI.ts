adcf3a6dfbf4d47085232304fcb4ce03aa4466d3
li weihai <weihailee@hotmail.com>
2016-05-18 09:03:56 +0000

    ChargeTipUI.ts 已使用 Bitbucket 在线创建

---
+++ public/mobile/src/views/ChargeTipUI.ts
@@ -0,0 +1,18 @@
+class ChargeTipUI extends eui.Component{
+    private btnCancel:eui.Button;
+    private btnCharge:eui.Button;
+
+    constructor() {
+        super();
+		
+        this.skinName = "resource/custom_skins/chargeTipUISkin.exml";
+		
+        this.btnCancel.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
+            application.back(this);
+        }, this );
+        
+        this.btnCharge.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.charge();
+        },this);        		
+    }
+}
