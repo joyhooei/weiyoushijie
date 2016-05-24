9b3149c227c0b20a90e5851572249d7ee637718e
li weihai <weihailee@hotmail.com>
2016-05-24 12:09:29 +0000

    ToolUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/ToolUI.ts
+++ public/mobile/src/views/ToolUI.ts
@@ -57,12 +57,12 @@ class ToolUI extends eui.Component {
     
 	//爆击。每4小时自动获取一个，最多拥有3个。“点击”可以获取10倍的收益，持续60秒。100钻石可以增加至3个。说明里提醒玩家先用完已有的，再购买，因为最多只能拥有3个。
     private buyHit() {
-		application.showUI(new BuyToolUI("hit", 100, null, null, 0));
+		application.showUI(new BuyToolUI("hit", 100));
     }
     
 	//时光沙漏， 需要500钻石购买，产生相当于2天的产量。 总秒产*3600*48
     private buyTime() {
-		application.showUI(new BuyToolUI("time", 500, null, null, 0));
+		application.showUI(new BuyToolUI("time", 500));
     }
     
 	//月票，19元每月(30天）。每天登录可以领取300钻石，离线收益增加至90%，持续12小时。普通情况下离线收益为70%，持续8小时。首次购买获得1个勋章
