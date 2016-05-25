7b77f337eeb058d7bd5eac25acebf9acfe5d1789
li weihai <weihailee@hotmail.com>
2016-05-25 08:18:54 +0000

    HomeUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/HomeUI.ts
+++ public/mobile/src/views/HomeUI.ts
@@ -147,7 +147,7 @@ class HomeUI extends eui.Component{
 		var timer: egret.Timer = new egret.Timer(1000 * 60, 0);
 		timer.addEventListener(egret.TimerEvent.TIMER, function(event:egret.TimerEvent){
 			var dt = new Date();
-			if (dt.getHours() == 12) {
+			if (dt.getHours() == 12 && dt.getMiniutes() == 0) {
 				self.renderBid();
 				
 				application.refreshBid(function(bid){
