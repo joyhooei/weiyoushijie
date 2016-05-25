1e01f9e32c31c2e156e42af13d1e05dd1e1d1698
li weihai <weihailee@hotmail.com>
2016-05-25 08:29:17 +0000

    WinUI.ts 已使用 Bitbucket 在线创建

---
+++ public/mobile/src/views/WinUI.ts
@@ -0,0 +1,42 @@
+class WinUI extends eui.Component{
+    private btnCancel:eui.Button;
+	
+    private imgHide:eui.Image;
+	private imgAvatar:eui.Image;
+
+    constructor() {
+        super();
+        
+        this.skinName = "resource/custom_skins/winUISkin.exml";
+        
+        this.btnCancel.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
+            application.hideUI(this);
+        }, this );
+        
+        this.imgHide.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+			if (application.customer.hide_avatar == 1) {
+            	application.customer.hide_avatar = 0;
+			} else {
+				application.customer.hide_avatar = 1;
+			}
+			
+			this.renderAvatar();
+			
+			application.dao.save("Customer", application.customer);
+        },this);
+		
+		this.renderAvatar();
+    }
+	
+	private renderAvatar(): void {
+		if (application.customer.hide_avatar == 1) {
+			this.imgHide.source == "";
+			
+			this.imgAvatar.source = "Ahide_png"
+		} else {
+			this.imgHide.source == "";
+			
+			this.imgAvatar.source = application.customer.avatar;
+		}
+	}
+}
