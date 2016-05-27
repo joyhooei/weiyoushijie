7eca8813ef5b1208c4d5d9e968cdeb8ab55577c4
li weihai <weihailee@hotmail.com>
2016-05-27 04:26:19 +0000

    Project.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/models/Project.ts
+++ public/mobile/src/models/Project.ts
@@ -46,212 +46,101 @@ class Project {
     static createAllProjects(): Array<Project> {
 		let projects = new Array<Project>();
 		
-		projects.push(Project.createProjectOne());
-		projects.push(Project.createProjectTwo());
-		projects.push(Project.createProjectThree());
-		projects.push(Project.createProjectFour());
-		projects.push(Project.createProjectFive());
-		projects.push(Project.createProjectSix());
-		projects.push(Project.createProjectSeven());
-		projects.push(Project.createProjectEight());
-		projects.push(Project.createProjectNine());
-		projects.push(Project.createProjectTen());
-		projects.push(Project.createProjectEleven());
-		projects.push(Project.createProjectTwelve());
-		projects.push(Project.createProjectThirteen());
-		projects.push(Project.createProjectFourteen());
-		projects.push(Project.createProjectFifteen());
-		projects.push(Project.createProjectSixteen());
-		projects.push(Project.createProjectSeventeen());
-		projects.push(Project.createProjectEighteen());
-		projects.push(Project.createProjectNinteen());
-		projects.push(Project.createProjectTwenty());
+		let data = [
+			[1, 1, 2, 1000],
+			[10, 5, 2, 1000],
+			[300, 25, 2, 1000],
+			[6000, 160, 2, 1000],
+			[30000, 3200, 2, 1000],
+			
+			[45000000, 64000, 4, 1000],
+			[6750000000, 320000, 4, 1000],
+			[1350000000000, 6500000, 4, 1000],
+			[270000000000000, 32000000, 4, 1000],
+			[54000000000000000, 640000000, 4, 1000],
+			
+			[10800000000000000000, 3200000000, 8, 10000],
+			[2160000000000000000000, 64000000000, 8, 10000],
+			[432000000000000000000000, 320000000000, 8, 10000],
+			[86400000000000000000000000, 6400000000000, 8, 10000],
+			[17300000000000000000000000000, 32000000000000, 8, 10000],
+			
+			[3460000000000000000000000000000, 640000000000000, 16, 10000],
+			[691000000000000000000000000000000, 3200000000000000, 16, 10000],
+			[138000000000000000000000000000000000, 64000000000000000, 16, 10000],
+			[27600000000000000000000000000000000000, 320000000000000000, 16, 10000],
+			[5530000000000000000000000000000000000000, 6400000000000000000, 16, 10000]	
+		];
+
+		for (var i = 0; i < data.length; i++) {
+			projects.push(Project.createProject(data[i][0], data[i][1], data[i][2], data[i][3]));
+		}
 		
 		return projects;
 	}
 	
-	static createProjectOne(): Project {
+	static createProject(priceLevelOne: number, outputLevelOne: number, achieve1OutputRatio:number, achieve5OutputRatio:number): Project {
 		//priceLevelOne: number, outputLevelOne: number, propPrice: number, propOutputRatio: number
-		let project = new Project(1, 1, 100, 0.1);
+		let project = new Project(priceLevelOne, outputLevelOne, 100, 0.1);
 		
 		//lowerLevel: number, upperLevel: number, priceRatio: number, outputRatio: number
 		project.addLevelRatio(2, 99, 1.05, 1);
 		project.addLevelRatio(100, 199, 1.05, 1);
-		project.addLevelRatio(200, 299, 1.05, 1);
-		project.addLevelRatio(300, 399, 1.05, 1);
 		
+		project.addLevelRatio(200, 299, 1.03, 1);
+		project.addLevelRatio(300, 399, 1.03, 1);
 		project.addLevelRatio(400, 499, 1.03, 1);
-		project.addLevelRatio(500, 599, 1.03, 1.01);
-		project.addLevelRatio(600, 699, 1.03, 1.01);
-		project.addLevelRatio(700, 799, 1.03, 1.01);
-		project.addLevelRatio(800, 899, 1.03, 1.01);
-		project.addLevelRatio(900, 999, 1.03, 1.01);
 		
-		project.addLevelRatio(1000, 1199, 1.02, 1.005);
+		project.addLevelRatio(500, 599, 1.05, 1.01);
+		project.addLevelRatio(600, 699, 1.03, 1.01);
+		project.addLevelRatio(700, 799, 1.02, 1.01);
+		project.addLevelRatio(800, 899, 1.02, 1.01);
+		project.addLevelRatio(900, 999, 1.02, 1.01);
+		
+		project.addLevelRatio(1000, 1199, 1.03, 1.005);
 		project.addLevelRatio(1200, 1399, 1.02, 1.005);
 		project.addLevelRatio(1400, 1599, 1.02, 1.005);
 		project.addLevelRatio(1600, 1799, 1.02, 1.005);
 		project.addLevelRatio(1800, 1999, 1.02, 1.005);
-		project.addLevelRatio(2000, 2499, 1.02, 1.005);
+		project.addLevelRatio(2000, 2499, 1.01, 1.005);
 		project.addLevelRatio(2500, 2999, 1.02, 1.005);
-		project.addLevelRatio(3000, 3499, 1.02, 1.005);
-		project.addLevelRatio(3500, 3999, 1.02, 1.005);
-		project.addLevelRatio(4000, 4499, 1.02, 1.005);
-		project.addLevelRatio(4500, 4999, 1.02, 1.005);
+		project.addLevelRatio(3000, 3499, 1.01, 1.005);
+		project.addLevelRatio(3500, 3999, 1.01, 1.005);
+		project.addLevelRatio(4000, 4499, 1.01, 1.005);
+		project.addLevelRatio(4500, 4999, 1.01, 1.005);
 		
 		project.addLevelRatio(5000, 5999, 1.01, 1.005);
 		project.addLevelRatio(6000, 6999, 1.01, 1.005);
-		project.addLevelRatio(7000, 7999, 1.01, 1.005);
-		project.addLevelRatio(8000, 8999, 1.01, 1.005);
-		project.addLevelRatio(9000, 9999, 1.01, 1.005);
-		project.addLevelRatio(10000, 99999, 1.01, 1.005);
+		project.addLevelRatio(7000, 7999, 1.007, 1.005);
+		project.addLevelRatio(8000, 8999, 1.007, 1.003);
+		project.addLevelRatio(9000, 9999, 1.007, 1.003);
+		project.addLevelRatio(10000, 99999, 1.003, 1.002);
 		
-		//level: number, outputRatio: number, prieUseGold: number, priceUseDiamond: number
-		project.addAchieve(50, 		2, 							1000, 					100);
-		project.addAchieve(100, 	5, 							5000, 					100);
-		project.addAchieve(200, 	5, 							5000, 					100);
-		project.addAchieve(300, 	5, 							5000, 					100);
-		project.addAchieve(500, 	1000, 						100000, 				1000);
-		project.addAchieve(1000, 	1000, 						100000000, 				1000);
-		project.addAchieve(2000, 	1000000, 					100000000000, 			2000);
-		project.addAchieve(5000, 	1000000000, 				10000000000000, 		2000);
-		project.addAchieve(10000, 	1000000000000000, 			100000000000000000, 	5000);
-		project.addAchieve(100000, 	10000000000000000000000000, 1000000000000000000000, 10000);
+		//level: number, outputRatio: number, priceUseDiamond: number, prieUseGold: number
+		project.addAchieve(50, 		achieve1OutputRatio, 		100, 	1000);
+		project.addAchieve(100, 	achieve1OutputRatio * 2, 	100, 	1190);
+		project.addAchieve(200, 	achieve1OutputRatio * 4, 	100, 	157000);
+		project.addAchieve(300, 	achieve1OutputRatio* 8, 	100, 	6030000);
+		project.addAchieve(500, 	achieve5OutputRatio, 		1000, 	10000000000);
+		project.addAchieve(1000, 	achieve5OutputRatio, 		2000, 	100000000000000000);
+		project.addAchieve(2000, 	achieve5OutputRatio, 		3000, 	50000000000000000000000000);
+		project.addAchieve(5000, 	10000, 		 				4000, 	800000000000000000000000000000000000000000000000000);
+		project.addAchieve(10000, 	1000000, 	 				5000, 	40000000000000000000000000000000000000000000000000000000000000000);
+		project.addAchieve(100000, 	10000000000, 				10000,	100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
 		
 		return project;
 	}
 	
-	static createProjectTwo(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectThree(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectFour(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectFive(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
 	static createProjectSix(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
+        return this.createProject();
 	}
 	
 	static createProjectSeven(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
+        return this.createProject();
 	}
 	
 	static createProjectEight(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectNine(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectTen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectEleven(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectTwelve(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectThirteen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectFourteen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectFifteen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectSixteen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectSeventeen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectEighteen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectNinteen(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
-	}
-	
-	static createProjectTwenty(): Project {
-		let project = new Project(1, 1, 100, 0.1);
-		project.addLevelRatio(2, 99, 1.05, 1);
-		
-        return this.createProjectOne();
+        return this.createProject();
 	}
 	
 	//根据级别、成就和道具个数计算本项目的秒产
