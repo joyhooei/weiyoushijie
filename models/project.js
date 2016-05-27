8a654d023f8588a180a87fca0a299fe8ce3f659f
li weihai <weihailee@hotmail.com>
2016-05-27 08:41:05 +0000

    project.js 已在 Bitbucket 中在线编辑过

--- models/project.js
+++ models/project.js
@@ -1,12 +1,13 @@
 var AV = require('leanengine');
 
-module.exports.create = function(customerId) {
+module.exports.create = function(customerId, game) {
     var project = new dao.Project();
     project.set("customer_id", customerId);
     project.set("sequence", 0);
     project.set("level", 1);
     project.set("achieve", 0);
     project.set("unlocked", 1);
+    project.set("game", game);
 	return project.save();
 }
 	
