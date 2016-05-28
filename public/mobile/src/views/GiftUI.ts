30aece99fdfb1cf3e88242e35d44c79e852b7637
li weihai <weihailee@hotmail.com>
2016-05-28 04:14:08 +0000

    GiftUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/GiftUI.ts
+++ public/mobile/src/views/GiftUI.ts
@@ -1,3 +1,14 @@
+enum GiftCategory {
+    Login = 1,
+    Online,
+    Bid,
+    Ticket,
+	Share,
+	Charge,
+	Output,
+	Attention
+}
+
 class GiftUI extends eui.Component {
     private imgPick1: eui.Image;
     private imgPick2: eui.Image;
@@ -11,6 +22,8 @@ class GiftUI extends eui.Component {
     private imgRet: eui.Image;
     
     private gifts: any[];
+	
+	private imgPicks: eui.Image[];
 	
     //在线礼物计时
 	private onlineGiftTimeout: number;
@@ -28,114 +41,117 @@ class GiftUI extends eui.Component {
         this.addEventListener(eui.UIEvent.COMPLETE,this.uiCompHandler,this);
 
         this.skinName = "resource/custom_skins/giftUISkin.exml";
+		
+		this.imgPicks = [this.imgPick1, this.imgPick2, this.imgPick3, this.imgPick4, this.imgPick5, this.imgPick6, this.imgPick7, this.imgPick8];
 
         this.imgRet.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function() {
             application.hideUI(this);
         },this);	
         
         this.imgPick1.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function(ev){
-            this.onLoginGift();
+            this.pickGift(this.gift(GiftCategory.Login));
         },this);
         this.imgPick2.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function(ev) {
-            this.onOnlineGift();
+            this.pickGift(this.gift(GiftCategory.Online));
         },this);
         this.imgPick3.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function(ev) {
-            this.onBidGift();
+            this.pickBidGift();
         },this);
         this.imgPick4.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function(ev) {
-            this.onTicketGift();
+            this.pickTicketGift();
         },this);
         this.imgPick5.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function(ev) {
-            this.onShareGift();
+            this.pickShareGift();
         },this);
         this.imgPick6.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function(ev) {
-            this.onFirstChargeGift();
+            this.pickFirstChargeGift();
         },this);
         this.imgPick7.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function(ev) {
-            this.onOutputGift();
+            this.pickOutputGift();
         },this);
         this.imgPick8.addEventListener(egret.TouchEvent.TOUCH_BEGIN,function(ev) {
-            this.onAttentionGift();
+            this.pickAttentionGift();
         },this);
     }
     
-    private onLoginGift() {
-        this.pick(0, this.imgPick1);
-    }
-    
-    private onOnlineGift() {
-        this.pick(1,this.imgPick2);
+    private pickGift(gift) {
+        if(gift.locked == 0) {
+            this.lockGift(gift, 2);
+            this.updateCustomer(gift);
+        }
     }
 	
-	private onBidGift() {
-        if (this.gifts[2].locked == 0) {
-            this.pick(2,this.imgPick3);
-        } else if (this.gifts[2].locked == 1) {
+	private pickBidGift() {
+		var gift = this.gift(GiftCategory.Bid);
+        if (gift.locked == 0) {
+            this.pickGift(gift);
+        } else if (gift.locked == 1) {
             application.hideUI(this);
             application.gotoAuction();
         }
 	}
 	
-	private onTicketGift() {
-        if (this.gifts[3].locked == 0) {
-            this.pick(3,this.imgPick4);
-        } else if (this.gifts[3].locked == 1) {
+	private pickTicketGift() {
+		var gift = this.gift(GiftCategory.Ticket);
+        if (gift.locked == 0) {
+            this.pickGift(gift);
+        } else if (gift.locked == 1) {
             application.hideUI(this);
             application.gotoTool();
         }
 	}
     
-    private onShareGift() {
+    private pickShareGift() {
 		var self = this;
 		
-        if (self.gifts[4].locked == 0) {
-            self.pick(4,self.imgPick5);
-        } else if (self.gifts[4].locked == 1) {
+		var gift = self.gift(GiftCategory.Share);
+        if (gift.locked == 0) {
+            self.pickGift(gift);
+        } else if (gift.locked == 1) {
             application.share(function () {
-                self.gifts[4].locked = 0;
-
-                application.dao.save("Gift",self.gifts[4], null);
-
-                self.setImage(self.imgPick5,self.gifts[4]);
+                self.lockGift(gift, 0);
 			})
         }
     }
     
-    private onFirstChargeGift() {
-        if (this.gifts[5].locked == 0) {
-            this.pick(5,this.imgPick6);
-        } else if (this.gifts[5].locked == 1) {
+    private pickFirstChargeGift() {
+		var gift = this.gift(GiftCategory.Charge);
+		
+        if (gift.locked == 0) {
+            this.pickGift(gift);
+        } else if (gift.locked == 1) {
             application.hideUI(this);
             application.showUI(new FirstChargeBonusUI());
         }
     }
 	
-	private onOutputGift() {
-		var gift = this.gifts[6];
+	private pickOutputGift() {
+		var gift = this.gift(GiftCategory.Output);
 		
 		if (application.log10(application.customer.output) > application.log10(parseInt(gift.data))) {
-			this.pick(1,this.imgPick7);
-		} else {
-			if(gift.locked == 0) {
-                this.income(gift,this.imgPick7);
-			}
+			this.pickGift(gift);
 		}
 	}
 	
-	private onAttentionGift() {
+	private pickAttentionGift() {
 		var self = this;
 		
-        if (self.gifts[7].locked == 0) {
-            self.pick(7,self.imgPick8);
-        } else if (self.gifts[7].locked == 1) {
+		var gift = self.gift(GiftCategory.Attention);
+		
+        if (gift.locked == 0) {
+            self.pickGift(gift);
+        } else if (gift.locked == 1) {
             application.attention(function () {
-                self.gifts[7].locked = 0;
-
-                application.dao.save("Gift",self.gifts[7], null);
-
-                self.setImage(self.imgPick8,self.gifts[7]);
+				self.lockGift(gift, 0);
 			})
         }
+	}
+	
+	private lockGift(gift: any, lock:number) {
+		gift.locked = lock;
+		this.renderGift(gift);
+		
+		application.dao.save("Gift",gift);
 	}
 
     public refresh(): void {
@@ -146,72 +162,101 @@ class GiftUI extends eui.Component {
         var self = this;
 
         application.dao.fetch("Gift", {customer_id: application.customer.id}, {order : 'category ASC'}, function(succeed, gifts){
-            //1、登录200钻。每天领取一次
-            self.setImage(self.imgPick1,gifts[0]);
-            
-            //2、在线奖励，每天200钻。
-            //如果今天的在线时间礼物还没有领取，检查一下是否可以领取了
-            if(gifts[1].locked == 1) {
-                var lastLogin = new Date(application.customer.last_login);
-                var today = new Date();
-                var diff = (today.getTime() - lastLogin.getTime()) / 1000;
-                if(diff > 60 * 60) {
-                    //已经过了一小时，可以领取了
-                    gifts[1].locked = 0;
-                } else {
-                    //在线还不到1个小时，启动定时器
-                    var timer: egret.Timer = new egret.Timer(1000,diff);
-                    self.onlineGiftTimeout = diff;
-                    timer.addEventListener(egret.TimerEvent.TIMER,function(event: egret.TimerEvent) {
-                        self.lblOnlineGiftTimeout.text = (Math.floor(self.onlineGiftTimeout / 60)).toString() + ":" + (Math.floor(self.onlineGiftTimeout % 60)).toString();
-                        self.onlineGiftTimeout -= 1;
-                    },self);
+            if (succeed) {
+				self.gifts = gifts;
 
-                    timer.start();
-                }
-            }
-            self.setImage(self.imgPick2,gifts[1]);        
-            
-            //3、拍卖100钻。每天领取一次。灰色点击直接跳去拍卖页面。
-            self.setImage(self.imgPick3, gifts[2]);
-            
-            //4、永久会员/月票 300钻。每天领取一次。灰色是点击跳入会员购买页面（参照道具弹出窗口） 月票默认30天，会显示剩余月票天数，如果是永久则显示永久
-			var ticketDay = application.ticketDay();
-            if (ticketDay == 0) {
-                self.lblTicketGiftTimeout.text = "";
-            } else if (ticketDay < 0) {
-                self.lblTicketGiftTimeout.text = "永久";
-            } else {
-                self.lblTicketGiftTimeout.text = ticketDay.toString() + "天";
-            }
-            self.setImage(self.imgPick4,gifts[3]);
-            
-            //5、分享100钻。每天任意在微博，微信等地方分享一次就可以领取。灰色时点击跳入分享页面。
-            self.setImage(self.imgPick5,gifts[4]);
+				//1、登录200钻。每天领取一次
+				self.renderGift(self.gift(GiftCategory.Login));
 
-            //6、首冲 1500钻+1勋章+1M 金币。 只能领取一次，不再刷新。灰色时点击跳转首冲页面。
-            self.setImage(self.imgPick6,gifts[5]);
-            
-            //7、秒产每增加一个数量级，就得100个钻石
-            if (gifts[6].data && gifts[6].data.length > 0) {
-                self.lblOutputGift.text = application.format(parseInt(gifts[6].data) * 10);
-            } else {
-                self.lblOutputGift.text = "100";
-            }
-            self.setImage(self.imgPick7,gifts[6]);
-            
-            //8、关注
-            self.setImage(self.imgPick8,gifts[7]);
-            
-            self.gifts = gifts;
+				this.renderOnlineGift();
+
+				//3、拍卖100钻。每天领取一次。灰色点击直接跳去拍卖页面。
+				self.renderGift(self.gift(GiftCategory.Bid));
+				
+				self.renderTicketGift();
+
+				//5、分享100钻。每天任意在微博，微信等地方分享一次就可以领取。灰色时点击跳入分享页面。
+				self.renderGift(self.gift(GiftCategory.Share));
+
+				//6、首冲 1500钻+1勋章+1M 金币。 只能领取一次，不再刷新。灰色时点击跳转首冲页面。
+				self.renderGift(self.gift(GiftCategory.Charge));
+
+				self.renderOutputGift();
+
+				//8、关注
+				self.renderGift(self.gift(GiftCategory.Attention));
+			}
         })
     }
+	
+	//2、在线奖励，每天200钻。
+	//如果今天的在线时间礼物还没有领取，检查一下是否可以领取了
+	private renderOnlineGift() {
+		var gift = this.gift(GiftCategory.Online);
+		
+		if(gift.locked == 1) {
+			var lastLogin = new Date(application.customer.last_login);
+			var today     = new Date();
+			var diff      = (today.getTime() - lastLogin.getTime()) / 1000;
+			if(diff > 60 * 60) {
+				//已经过了一小时，可以领取了
+				this.lockGift(gift, 0);
+			} else {
+				//在线还不到1个小时，启动定时器
+				var timer: egret.Timer = new egret.Timer(1000, diff);
+				this.onlineGiftTimeout = diff;
+				timer.addEventListener(egret.TimerEvent.TIMER,function(event: egret.TimerEvent) {
+					this.lblOnlineGiftTimeout.text = (Math.floor(this.onlineGiftTimeout / 60)).toString() + ":" + (Math.floor(this.onlineGiftTimeout % 60)).toString();
+					this.onlineGiftTimeout -= 1;
+				},this);
+				timer.addEventListener(egret.TimerEvent.COMPLETE,function(event: egret.TimerEvent) {
+					this.lockGift(gift, 0);
+				},this);					
+				timer.start();
+				
+				this.renderGift(gift);
+			}
+		} else {		
+			this.renderGift(gift);
+		}
+	}
+	
+	private renderTicketGift() {
+		//4、永久会员/月票 300钻。每天领取一次。灰色是点击跳入会员购买页面（参照道具弹出窗口） 月票默认30天，会显示剩余月票天数，如果是永久则显示永久
+		var gift = this.gift(GiftCategory.Ticket);
+		var ticketDay = application.ticketDay();
+		if (ticketDay == 0) {
+			this.lblTicketGiftTimeout.text = "";
+		} else if (ticketDay < 0) {
+			this.lblTicketGiftTimeout.text = "永久";
+		} else {
+			this.lblTicketGiftTimeout.text = ticketDay.toString() + "天";
+		}
+		this.renderGift(gift);
+	}
+	
+	private renderOutputGift() {
+		//7、秒产每增加一个数量级，就得100个钻石
+		var gift = this.gift(GiftCategory.Output);
+		if (gift.data && gift.data.length > 0) {
+			this.lblOutputGift.text = application.format(parseInt(gift.data) * 10);
+		} else {
+			this.lblOutputGift.text = "100";
+		}
+		this.renderGift(gift);
+	}
+	
+	private gift(category: CardCategory) {
+		return this.gifts[category - 1];
+	}
     
-    private setImage(imgPic: eui.Image, gift:any) {
+    private renderGift(gift: any) {
+		var imgPic = this.imgPicks[gift.category - 1];
+		
         if(gift.locked == 1) {
-            if (gift.category == 5) {
+            if (gift.category == GiftCategory.Share) {
                 imgPic.source = "share_png";
-            } else if(gift.category == 8) {
+            } else if(gift.category == GiftCategory.Attention) {
                 imgPic.source = "att_png";
             } else {
                 imgPic.source = "unpick_png";
@@ -222,29 +267,16 @@ class GiftUI extends eui.Component {
             imgPic.source = "picked_png";
         }
     }
-    
-    private pick(index: number,imgPic: eui.Image) {
-        var gift = this.gifts[index];
-        if(gift.locked == 0) {
-            gift.locked = 2;
-            application.dao.save("Gift", gift);
-            
-            this.income(gift,imgPic);
-        }
-    }
 	
-    private income(gift: any,imgPic: eui.Image): void {
+    private updateCustomer(gift: any): void {
         var self = this;
         
-		application.customer.metal += gift.metal;
-		application.customer.gold  += gift.gold;
+		application.customer.metal   += gift.metal;
+		application.customer.gold    += gift.gold;
 		application.customer.diamond += gift.diamond;
-
 		application.dao.save("Customer",application.customer,function(succeed,c) {
 			if(succeed) {
-				application.refreshCustomer(-gift.gold, -gift.diamond, 0,0,null);
-
-                self.setImage(gift,imgPic);
+				application.refreshCustomer(gift.gold, gift.diamond, 0, 0, null);
 			}
 		});	
 	}
