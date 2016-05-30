d464e6a745343e6aaac37026fa00f4c189e648f5
li weihai <weihailee@hotmail.com>
2016-05-30 06:27:28 +0000

    BlockUI.ts 已在 Bitbucket 中在线编辑过

--- public/mobile/src/views/BlockUI.ts
+++ public/mobile/src/views/BlockUI.ts
@@ -1,10 +1,23 @@
 class BlockUI extends egret.Sprite {
-    public constructor() {
+    public constructor(ui:egret.Component) {
         super();
 		
 		this.width  = 400;
 		this.height = 640;
 		
-		this.touchEnabled = false;
+		this.graphics.beginFill(0xff0000);
+        this.graphics.drawRect(0, 0, 400, 640);
+        this.graphics.endFill();
+		
+		this.alpha = 0.2;
+		
+        ui.horizontalCenter = 0;
+        ui.verticalCenter   = 0;
+		this.addChild(ui);
+		
+		this.touchEnabled = true;
+		this.addEventListener(egret.TouchEvent.TOUCH_END, function(){
+			application.hideUI(ui);
+		}, this);
     }
 }
