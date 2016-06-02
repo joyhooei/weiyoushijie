04b7849d789fc7ebd5d275ff2fabe946108cab52
li weihai <weihailee@hotmail.com>
2016-06-02 01:40:17 +0000

    GiftUI.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/views/GiftUI.ts
+++ public/headline/src/views/GiftUI.ts
@@ -212,15 +212,15 @@ class GiftUI extends eui.Component {
 		if(gift.locked == 1) {
 			var lastLogin = new Date(application.customer.last_login);
 			var today     = new Date();
-			var diff      = (today.getTime() - lastLogin.getTime()) / 1000;
+			var diff      = Math.floor((today.getTime() - lastLogin.getTime()) / 1000);
 			if(diff > 60 * 60) {
 				//已经过了一小时，可以领取了
 				this.lockGift(gift, 0);
 			} else {
 				//在线还不到1个小时，启动定时器
-				this.onlineGiftTimeout = diff;
+				this.onlineGiftTimeout = 3600 - diff;
 				
-				var timer: egret.Timer = new egret.Timer(1000, diff);
+				var timer: egret.Timer = new egret.Timer(1000, this.onlineGiftTimeout);
 				timer.addEventListener(egret.TimerEvent.TIMER,function(event: egret.TimerEvent) {
 					this.lblOnlineGiftTimeout.text = (Math.floor(this.onlineGiftTimeout / 60)).toString() + ":" + (Math.floor(this.onlineGiftTimeout % 60)).toString();
 					if (this.onlineGiftTime > 0) {
