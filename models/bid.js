5e72425120238aa5339c07847b8661561aebf498
li weihai <weihailee@hotmail.com>
2016-06-06 01:09:33 +0000

    bid.js 已在 Bitbucket 中在线编辑过

--- models/bid.js
+++ models/bid.js
@@ -8,6 +8,7 @@ module.exports.open = function(request, response) {
 
     var query = new AV.Query(dao.Bid);
     query.equalTo('day', today);
+	query.equalTo('game', 'headline');
     query.addDescending("gold");
 	query.limit(1);
     query.find().then(function(bids){
@@ -35,6 +36,50 @@ module.exports.open = function(request, response) {
 				console.error(error.message);
 				response.error(error.message);
 			});
+    	} else {
+			console.error("没有投标");
+    		response.error();
+    	}
+    }, function(error){
+		console.error(error.message);
+    	response.error(error.message);
+    });	
+}
+
+module.exports.max = function(request, response) {
+	var dt = new Date();
+	var today = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
+
+    var query = new AV.Query(dao.Bid);
+    query.equalTo('day', today);
+	query.equalTo('game', 'headline');
+    query.addDescending("gold");
+	query.limit(1);
+    query.find().then(function(bids){
+    	if (bids.length > 0) {
+			var bid = bids[0];
+			
+			var q = new AV.Query(dao.MaxBid);
+			q.equalTo('day', today);
+			q.equalTo('game', bid.get("game"));
+			q.limit(1);
+			q.find().then(function(mbs){
+				if (mbs.length > 0) {
+					var maxbid = mbs[0];
+				} else {
+					var maxbid = new dao.MaxBid();
+					maxbid.set("day", today);
+					maxbid.set("game", bid.get("game"));
+				}
+				
+				maxbid.set("gold", bid.get("gold"));
+				maxbid.set("customer_id", bid.get("customer_id");
+				maxbid.save().then(function(){
+					response.success();
+				}, function(error){
+					console.error(error.message);
+					response.error(error.message);				
+				});
     	} else {
 			console.error("没有投标");
     		response.error();
