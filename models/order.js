01fa9ee14c8fabc4b46c43474040d87d007cb035
li weihai <weihailee@hotmail.com>
2016-06-06 06:35:55 +0000

    order.js 已在 Bitbucket 中在线编辑过

--- models/order.js
+++ models/order.js
@@ -6,18 +6,28 @@ module.exports.pay = function(order) {
 		query.get(order.get("customer_id")).then(function(customer){
 			customer.increment("charge", order.get("price"));
 			
-			if (order.get("product") == "Ticket") {
-				customer.set("ticket", moment().add(30, 'days').format());
-			} else if (order.get("product") == "VIP") {
-				customer.set("ticket", moment().add(30 * 12 * 100, 'days').format());
-			}
+			query = new AV.Query(dao.Order);
+			query.equalTo("customer_id", customer.id);
+			query.equalTo("product", order.get("product"));
+			query.equalTo("state", 1);
+			query.count().then(function(count) {
+				if (order.get("product") == "Ticket") {
+					customer.set("ticket", moment().add(30, 'days').format());
+				} else if (order.get("product") == "VIP") {
+					customer.set("ticket", moment().add(30 * 12 * 100, 'days').format());
+				}
 
-			customer.save().then(function(c){
-				order.set("state", 1);
-				resolve(order);
-			}, function(err){
-				reject(new Error("_payForTicket save customer " + err.message));
-			});			
+				customer.save().then(function(c){
+					if (count == 0) {
+						Gift.unlockFirstCharge();
+					}
+				
+					order.set("state", 1);
+					resolve(order);
+				}, function(err){
+					reject(new Error("_payForTicket save customer " + err.message));
+				});			
+			});
 		}, function(err){
 			reject(new Error("_payForTicket get customer " + err.message));
 		});
