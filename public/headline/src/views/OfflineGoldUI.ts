b75ed695e881482ba590badec78cdd720f7c7df2
li weihai <weihailee@hotmail.com>
2016-06-07 03:17:25 +0000

    OfflineGoldUI.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/views/OfflineGoldUI.ts
+++ public/headline/src/views/OfflineGoldUI.ts
@@ -5,18 +5,18 @@ class OfflineGoldUI extends eui.Component{
     
     private imgOK: eui.Image;
     
-    constructor(gold: number, hour: string, minute: string) {
+    constructor() {
         super();
         
         this.skinName = "resource/custom_skins/offlineGoldUISkin.exml";
 		
-        this.lblGold.text   = application.format(gold);
-        this.lblHour.text   = hour; 
-        this.lblMinute.text = minute;
+        this.lblGold.text   = application.format(application.customer.offline_gold);
+        this.lblHour.text   = application.customer.offline_hours; 
+        this.lblMinute.text = application.customer.offline_minutes;
         
         this.imgOK.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
-            application.customer.gold += gold;
-            application.customer.accumulated_gold += gold;
+            application.customer.gold += application.customer.offline_gold;
+            application.customer.accumulated_gold += application.customer.offline_gold;
             application.customer.offline_gold = 0;
             application.customer.offline_minutes = 0;
             application.customer.offline_hours = 0;
