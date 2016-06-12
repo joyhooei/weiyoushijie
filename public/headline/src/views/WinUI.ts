d3cfd702a518e246d7bc3d1b45da44a9dad40b5f
li weihai <weihailee@hotmail.com>
2016-06-12 03:16:13 +0000

    WinUI.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/views/WinUI.ts
+++ public/headline/src/views/WinUI.ts
@@ -10,24 +10,10 @@ class WinUI extends eui.Component{
         this.skinName = "resource/custom_skins/winUISkin.exml";
         
         this.imgBack.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
-			var self = this;
-			
-			application.dao.fetch("Bid", {customer_id: application.customer.id, succeed: 1, claimed: 0}, {}, function(succeed, bids){
-				if (succeed) {
-					for(var i = 0; i < bids.length; i++) {
-						application.customer.gold -= bids[i].gold;
-						application.customer.metal++;
-						application.customer.diamond += 2000;
-
-						bids[i].claimed = 1;
-						application.dao.save("Bid", bids[i]);
-					}
-				}
-			
-				application.earnGold(application.customer.offline_gold);
-            	application.hideUI(self);
-			});
-        }, this );
+			application.earnBids();			
+			application.earnOfflineGold();
+            application.hideUI(this);
+        }, this);
         
         this.imgHide.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
 			if (application.customer.hide_winner == 1) {
