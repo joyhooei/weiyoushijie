454653f1f4d14a60d0a787811cb269061bba2b7b
li weihai <weihailee@hotmail.com>
2016-06-21 06:41:51 +0000

    ChargeTipUI.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/views/ChargeTipUI.ts
+++ public/headline/src/views/ChargeTipUI.ts
@@ -1,18 +1,54 @@
 class ChargeTipUI extends eui.Component{
-    private imgCancel: eui.Image;
-    private imgCharge:eui.Image;
+    private imgBack: eui.Image;
+	
+    private imgCharge1:eui.Image;
+    private imgCharge2:eui.Image;
+    private imgCharge3:eui.Image;
+    private imgCharge4:eui.Image;
+    private imgCharge5:eui.Image;
+    private imgCharge6:eui.Image;
+    private imgCharge7:eui.Image;
+    private imgCharge8:eui.Image;
 
     constructor() {
         super();
 		
         this.skinName = "resource/custom_skins/chargeTipUISkin.exml";
 		
-        this.imgCancel.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
+        this.imgBack.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
             application.hideUI(this);
         }, this );
         
-        this.imgCharge.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
-            application.charge();
-        },this);        		
+        this.imgCharge1.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.charge(2);
+        },this);
+        
+        this.imgCharge2.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.charge(5);
+        },this);
+        
+        this.imgCharge3.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.charge(10);
+        },this);
+        
+        this.imgCharge4.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.charge(30);
+        },this);
+        
+        this.imgCharge5.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.charge(100);
+        },this);
+        
+        this.imgCharge6.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.charge(500);
+        },this);
+        
+        this.imgCharge7.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.buyTicket();
+        },this);
+        
+        this.imgCharge8.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+            application.buyVIP();
+        },this);
     }
 }
