333ffc8298504fce0f7444d6640524c20ff4b118
li weihai <weihailee@hotmail.com>
2016-06-22 06:24:59 +0000

    HelpUI.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/views/HelpUI.ts
+++ public/headline/src/views/HelpUI.ts
@@ -1,5 +1,6 @@
 class HelpUI extends eui.Component{
     private imgBack:eui.Image;
+    private imgReport:eui.Image;
     
     private lblContent:eui.Label;
     
@@ -9,6 +10,12 @@ class HelpUI extends eui.Component{
         this.skinName = "resource/custom_skins/helpUISkin.exml";
         
         this.lblContent.text = content;
+        
+        this.imgReport.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
+            application.hideUI(this);
+            
+            application.showUI(new ReportUI());
+        }, this ); 
         
         this.imgBack.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
             application.hideUI(this);
