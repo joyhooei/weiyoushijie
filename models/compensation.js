8565ad2fc0a1308beae7d649c28fdb85fbb107c7
li weihai <weihailee@hotmail.com>
2016-06-22 08:31:41 +0000

    compensation.js 已在 Bitbucket 中在线编辑过

--- models/compensation.js
+++ models/compensation.js
@@ -4,17 +4,14 @@ module.exports.makeup = function(customer) {
 	return Q.Promise(function(resolve, reject, notify) {
 		var query = new AV.Query(dao.Compensation);
 		query.equalTo("customer_id", customer.id);
+		query.equalTo("state", 0);
 		query.first().then(function(compensation){
 			customer.increment("metal", compensation.get("metal"));
 			customer.increment("gold", compensation.get("gold"));
 			customer.increment("diamond", compensation.get("diamond"));
 			
-			if (compensation.get("vip") >= customer.get("vip")) {
-				customer.set("vip", compensation.get("vip"));
-				customer.set("ticket", compensation.get("ticket"));
-			}
-			
-			compensation.destroy();
+			compesation.set("state", 1);
+			compensation.save();
 			
 			resolve(customer);
 		}, function(error){
