991015e68f6af6d4326812dea7f30fca9a824172
li weihai <weihailee@hotmail.com>
2016-06-23 02:34:05 +0000

    rank.js 已在 Bitbucket 中在线编辑过

--- models/rank.js
+++ models/rank.js
@@ -1,32 +1,40 @@
 var AV = require('leanengine');
 
 module.exports.rank = function(req, res) {
-	AV.Query.doCloudQuery('delete from Rank where game = "headline"').then(function () {
-		var q = new AV.Query(dao.Customer);
-		q.equalTo("game", "headline");
-		q.select('objectId', 'name', 'metal', 'accumulated_gold', 'avatar');
-		q.addDescending("metal");
-		q.addDescending("accumulated_gold");
-		Helper.findAll(q).then(function(customers){
-			var ranks = [];
-			for(var i = 0; i < customers.length; i++) {
-				ranks.push(new Rank({customer_id: customers[i].id, rank: i + 1, game: "headline", name: customers[i].name, metal: customers[i].metal, accumulated_gold: customers[i].accumulated_gold, avatar: customers[i].avatar}));
-			}
-			
-			AV.Object.saveAll(ranks).then(function (avobjs) {
-				res.success("rank succeed");
-			}, function (error) {
-				console.error("rank saveAll " + error.message);
+	var query = new AV.Query(dao.Rank);
+	query.equalTo("game", "headline");
+	Helper.findAll(query).then(function (rs) {
+		AV.Object.destroyAll(rs).then(function () {
+			var q = new AV.Query(dao.Customer);
+			q.equalTo("game", "headline");
+			q.select('objectId', 'name', 'metal', 'accumulated_gold', 'avatar');
+			q.addDescending("metal");
+			q.addDescending("accumulated_gold");
+			Helper.findAll(q).then(function(customers){
+				var ranks = [];
+				for(var i = 0; i < customers.length; i++) {
+					ranks.push(new Rank({customer_id: customers[i].id, rank: i + 1, game: "headline", name: customers[i].name, metal: customers[i].metal, accumulated_gold: customers[i].accumulated_gold, avatar: customers[i].avatar}));
+				}
+
+				AV.Object.saveAll(ranks).then(function (avobjs) {
+					res.success("rank succeed");
+				}, function (error) {
+					console.error("rank saveAll " + error.message);
+
+					res.error(error.message);
+				});
+			}, function(error){
+				console.error("rank findAll customer " + error.message);
 
 				res.error(error.message);
 			});
-		}, function(error){
-			console.error("rank findAll " + error.message);
-			
+		}, function (error) {
+			console.error("rank destroyAll " + error.message);
+
 			res.error(error.message);
-		});		
+		});	
   	}, function (error) {
-		console.error("rank doCloudQuery " + error.message);
+		console.error("rank findAll rank " + error.message);
 		
 		res.error(error.message);
 	});	
