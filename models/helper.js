05edd616ead1fa814c1d1d10ea13f2acf9a26b31
li weihai <weihailee@hotmail.com>
2016-06-23 01:03:16 +0000

    helper.js 已在 Bitbucket 中在线编辑过

--- models/helper.js
+++ models/helper.js
@@ -12,26 +12,30 @@ module.exports.findAll = function(query) {
 			var offset  = 0; 
 			var result = [];
 			while (offset < total) {
-				query.skip(offset);
-				query.limit(1000);
-				query.find().then(function(models){
-					console.log("notify " + models.length);
-					notify(models);
-					
-					if (offset + models.length >= total) {
-						console.log("resolve " + total);
-						resolve(total);
-					}
-				}, function(error){
-					console.error("Helper findAll find " + error.message);
-					reject(error);
-				});
-
+				_query(query, offset, total, resolve, reject, notify);
+				
 				offset += 1000;
 			}
 		}, function(error){
 			console.error("Helper findAll count " + error.message);
 			reject(error);
 		});
+	});
+}
+
+function _query(query, offset, total, resolve, reject, notify) {
+	query.skip(offset);
+	query.limit(1000);
+	query.find().then(function(models){
+		console.log("notify " + models.length);
+		notify(models);
+
+		if (offset + models.length >= total) {
+			console.log("resolve " + total);
+			resolve(total);
+		}
+	}, function(error){
+		console.error("Helper findAll find " + error.message);
+		reject(error);
 	});
 }
