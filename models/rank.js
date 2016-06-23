5899be90cf2371169821c757dd5fd959031da5d6
li weihai <weihailee@hotmail.com>
2016-06-23 03:27:51 +0000

    rank.js 已在 Bitbucket 中在线编辑过

--- models/rank.js
+++ models/rank.js
@@ -4,18 +4,37 @@ var Helper = require('./helper');
 module.exports.rank = function(req, res) {
 	var query = new AV.Query(dao.Rank);
 	query.equalTo("game", "headline");
-	Helper.findAll(query).then(function (rs) {
-		if (rs.length > 0) {
-			AV.Object.destroyAll(rs).then(function () {
-				_createRanks(req, res);
+	q.addAscending("rank");
+	Helper.findAll(query).then(function (ranks) {
+		var q = new AV.Query(dao.Customer);
+		q.equalTo("game", "headline");
+		q.select('objectId', 'name', 'metal', 'accumulated_gold', 'avatar');
+		q.addDescending("metal");
+		q.addDescending("accumulated_gold");
+		Helper.findAll(q).then(function(customers){
+			for(var i = 0; i < customers.length; i++) {
+				if (i > ranks.length) {
+					var rank = new dao.Rank({game: "headline", rank: i + 1});
+					ranks.push(rank);
+				} else {
+					var rank = ranks[i];
+				}
+				
+				rank.set({customer_id: customers[i].id, name: customers[i].name, metal: customers[i].metal, accumulated_gold: customers[i].accumulated_gold, avatar: customers[i].avatar});
+			}
+
+			AV.Object.saveAll(ranks).then(function (avobjs) {
+				res.success("rank succeed");
 			}, function (error) {
-				console.error("rank destroyAll " + error.message);
+				console.error("rank saveAll " + error.message);
 
 				res.error(error.message);
 			});
-		} else {
-			_createRanks(req, res);
-		}
+		}, function(error){
+			console.error("rank findAll customer " + error.message);
+
+			res.error(error.message);
+		});
   	}, function (error) {
 		console.error("rank findAll rank " + error.message);
 		
@@ -24,27 +43,5 @@ module.exports.rank = function(req, res) {
 }
 
 function _createRanks(req, res) {
-	var q = new AV.Query(dao.Customer);
-	q.equalTo("game", "headline");
-	q.select('objectId', 'name', 'metal', 'accumulated_gold', 'avatar');
-	q.addDescending("metal");
-	q.addDescending("accumulated_gold");
-	Helper.findAll(q).then(function(customers){
-		var ranks = [];
-		for(var i = 0; i < customers.length; i++) {
-			ranks.push(new dao.Rank({customer_id: customers[i].id, rank: i + 1, game: "headline", name: customers[i].name, metal: customers[i].metal, accumulated_gold: customers[i].accumulated_gold, avatar: customers[i].avatar}));
-		}
 
-		AV.Object.saveAll(ranks).then(function (avobjs) {
-			res.success("rank succeed");
-		}, function (error) {
-			console.error("rank saveAll " + error.message);
-
-			res.error(error.message);
-		});
-	}, function(error){
-		console.error("rank findAll customer " + error.message);
-
-		res.error(error.message);
-	});
 }
