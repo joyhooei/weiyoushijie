03a3f0aa26afce6a7579b1ed5187e779cf4df25d
li weihai <weihailee@hotmail.com>
2016-06-23 02:54:52 +0000

    rank.js 已在 Bitbucket 中在线编辑过

--- models/rank.js
+++ models/rank.js
@@ -5,38 +5,46 @@ module.exports.rank = function(req, res) {
 	var query = new AV.Query(dao.Rank);
 	query.equalTo("game", "headline");
 	Helper.findAll(query).then(function (rs) {
-		AV.Object.destroyAll(rs).then(function () {
-			var q = new AV.Query(dao.Customer);
-			q.equalTo("game", "headline");
-			q.select('objectId', 'name', 'metal', 'accumulated_gold', 'avatar');
-			q.addDescending("metal");
-			q.addDescending("accumulated_gold");
-			Helper.findAll(q).then(function(customers){
-				var ranks = [];
-				for(var i = 0; i < customers.length; i++) {
-					ranks.push(new Rank({customer_id: customers[i].id, rank: i + 1, game: "headline", name: customers[i].name, metal: customers[i].metal, accumulated_gold: customers[i].accumulated_gold, avatar: customers[i].avatar}));
-				}
-
-				AV.Object.saveAll(ranks).then(function (avobjs) {
-					res.success("rank succeed");
-				}, function (error) {
-					console.error("rank saveAll " + error.message);
-
-					res.error(error.message);
-				});
-			}, function(error){
-				console.error("rank findAll customer " + error.message);
+		if (rs.length > 0) {
+			AV.Object.destroyAll(rs).then(function () {
+				_createRanks(req, res);
+			}, function (error) {
+				console.error("rank destroyAll " + error.message);
 
 				res.error(error.message);
 			});
-		}, function (error) {
-			console.error("rank destroyAll " + error.message);
-
-			res.error(error.message);
-		});	
+		} else {
+			_createRanks(req, res);
+		}
   	}, function (error) {
 		console.error("rank findAll rank " + error.message);
 		
 		res.error(error.message);
 	});	
+}
+
+function _createRanks(req, res) {
+	var q = new AV.Query(dao.Customer);
+	q.equalTo("game", "headline");
+	q.select('objectId', 'name', 'metal', 'accumulated_gold', 'avatar');
+	q.addDescending("metal");
+	q.addDescending("accumulated_gold");
+	Helper.findAll(q).then(function(customers){
+		var ranks = [];
+		for(var i = 0; i < customers.length; i++) {
+			ranks.push(new Rank({customer_id: customers[i].id, rank: i + 1, game: "headline", name: customers[i].name, metal: customers[i].metal, accumulated_gold: customers[i].accumulated_gold, avatar: customers[i].avatar}));
+		}
+
+		AV.Object.saveAll(ranks).then(function (avobjs) {
+			res.success("rank succeed");
+		}, function (error) {
+			console.error("rank saveAll " + error.message);
+
+			res.error(error.message);
+		});
+	}, function(error){
+		console.error("rank findAll customer " + error.message);
+
+		res.error(error.message);
+	});
 }
