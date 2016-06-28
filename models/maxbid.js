5ec797ec595bf64637393c3e9bd2c628b471ddc6
li weihai <weihailee@hotmail.com>
2016-06-28 02:41:03 +0000

    maxbid.js 已在 Bitbucket 中在线编辑过

--- models/maxbid.js
+++ models/maxbid.js
@@ -17,39 +17,11 @@ module.exports.max = function(request, response) {
 	query.limit(1);
     query.find().then(function(bids){
     	if (bids.length > 0) {
-			var bid = bids[0];
-			
-			var q = new AV.Query(dao.MaxBid);
-			q.equalTo('day', today);
-			q.equalTo('game', bid.get("game"));
-			q.limit(1);
-			q.find().then(function(mbs){
-				if (mbs.length > 0) {
-					var maxbid = mbs[0];
-				} else {
-					var maxbid = new dao.MaxBid();
-					maxbid.set("day", today);
-					maxbid.set("game", bid.get("game"));
+			for (var i = 0; i < bids.length; i ++) {
+				if (i == 0) {
+					_updateMaxBid(bids[0]);
 				}
-				
-				maxbid.set("gold", bid.get("gold"));
-				maxbid.set("customer_id", bid.get("customer_id"));
-				
-				var qry = new AV.Query(dao.Customer);
-				qry.get(bid.get("customer_id")).then(function(c){
-					maxbid.set("name", c.get("name"));
-					maxbid.set("avatar", c.get("avatar"));
-					maxbid.save().then(function(){
-						response.success();
-					}, function(error){
-						console.error("max save bid " + error.message);
-						response.error(error.message);				
-					});
-				}, function(error){
-					console.error("max get customer " + error.message);
-					response.error(error.message);
-				});
-			});
+			}
     	} else {
 			console.error("没有投标");
     		response.error();
@@ -58,4 +30,38 @@ module.exports.max = function(request, response) {
 		console.error("max find bids " + error.message);
     	response.error(error.message);
     });	
+}
+
+function _updateMaxBid(bid) {
+	var q = new AV.Query(dao.MaxBid);
+	q.equalTo('day', bid.get("day"));
+	q.equalTo('game', bid.get("game"));
+	q.limit(1);
+	q.find().then(function(mbs){
+		if (mbs.length > 0) {
+			var maxbid = mbs[0];
+		} else {
+			var maxbid = new dao.MaxBid();
+			maxbid.set("day", bid.get("day"));
+			maxbid.set("game", bid.get("game"));
+		}
+
+		maxbid.set("gold", bid.get("gold"));
+		maxbid.set("customer_id", bid.get("customer_id"));
+
+		var qry = new AV.Query(dao.Customer);
+		qry.get(bid.get("customer_id")).then(function(c){
+			maxbid.set("name", c.get("name"));
+			maxbid.set("avatar", c.get("avatar"));
+			maxbid.save().then(function(){
+				response.success();
+			}, function(error){
+				console.error("max save bid " + error.message);
+				response.error(error.message);				
+			});
+		}, function(error){
+			console.error("max get customer " + error.message);
+			response.error(error.message);
+		});
+	});
 }
