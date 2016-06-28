0ef8479199f61ce9aaf035de20f192cfff540447
li weihai <weihailee@hotmail.com>
2016-06-28 03:12:14 +0000

    message.js 已在 Bitbucket 中在线编辑过

--- models/message.js
+++ models/message.js
@@ -1,11 +1,11 @@
-module.exports.send = function(customer, title, content, attach, quantity) {
+module.exports.send = function(customerId, title, content, attach, quantity) {
     var message = new dao.Message();
-    message.set("customer_id", customer.id);
+    message.set("customer_id", customerId);
     message.set("title", title);
     message.set("content", content);
     message.set("attach_category", attach);
     message.set("attach_quantity", quantity);
     message.set("state", 0);
-    message.set("game", customer.get("game"));
+    message.set("game", "headline");
     return message.save();
 }
