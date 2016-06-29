65c1b8af6adb48735a91f33b789cc6f872e0a83c
li weihai <weihailee@hotmail.com>
2016-06-29 02:50:29 +0000

    server.js 已在 Bitbucket 中在线编辑过

--- platforms/standalone/server.js
+++ platforms/standalone/server.js
@@ -1,14 +1,8 @@
 'use strict';
 
-var AV = require('leanengine');
-
 var APP_ID = process.env.LC_APP_ID;
 var APP_KEY = process.env.LC_APP_KEY;
 var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
-
-AV.initialize('lcM4PuQxRsdVmalV8NbyE4If-gzGzoHsz', 'J3THSeE0XwaSqIQxCkezkvSD', 'bufePXDxXK6Cxck3SEjsIfOC');
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
 
 var app = require('./app');
 
