536a934dc6f703a5635bad2f62037faaaa60dc08
li weihai <weihailee@hotmail.com>
2016-07-01 06:59:47 +0000

    Application.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/Application.ts
+++ public/headline/src/Application.ts
@@ -76,11 +76,37 @@ module application {
 		
 		application.saveCustomerNow();
 	}
+    
+    export function getCookie(name:string){
+        //先查询cookie是否为空，为空就return ""
+　　　　if (document.cookie.length > 0) {
+            //通过String对象的indexOf()来检查这个cookie是否存在，不存在就为 -1　　
+            var begin = document.cookie.indexOf(name + "=");
+            if (begin != -1) {
+                //最后这个+1其实就是表示"="号啦，这样就获取到了cookie值的开始位置
+　　　　　　　　 begin = begin + name.length + 1;
+         
+                //其实我刚看见indexOf()第二个参数的时候猛然有点晕，后来想起来表示指定的开始索引的位置...这句是为了得到值的结束位置。因为需要考虑是否是最后一项，所以通过";"号是否存在来判断
+　　　　　　　　 var end = document.cookie.indexOf(";", begin);
+         
+　　　　　　　　 if (end == -1) {
+                    end = document.cookie.length;
+                }
+                
+                　　//通过substring()得到了值。想了解unescape()得先知道escape()是做什么的，都是很重要的基础，想了解的可以搜索下，在文章结尾处也会进行讲解cookie编码细节
+                return unescape(document.cookie.substring(begin, end));
+　　　　　　} 
+　　　　}
+    
+　　　　return ""
+　　}    
 
     export function onLoginCallback(data:nest.user.LoginCallbackInfo):void{
         //从后台获取用户信息
         application.dao.rest("login", {token: data.token}, (succeed: boolean, customer: any) => {
             if (succeed) {
+                application.token = application.getCookie("token");
+                
                 application.customer = customer;
                 
                 application.vip = Vip.createVip(application.customer.charge);
