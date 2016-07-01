4a24a129071ddbe9a48c750a4f72ef8ff33c20f7
li weihai <weihailee@hotmail.com>
2016-07-01 05:01:15 +0000

    account.js 已使用 Bitbucket 在线创建

---
+++ models/account.js
@@ -0,0 +1,64 @@
+var AV = require('leanengine');
+var crypto = require('crypto');
+
+module.exports.update = function(customer_id) {
+	return Q.Promise(function(resolve, reject, notify) {
+        var query = new AV.Query(dao.Account);
+        query.equalTo("customer_id", customer_id);
+        query.find().then(function(accounts){
+            if (accounts.length > 0) {
+                var account = accounts[0];
+            } else {
+                var account = new dao.Account();
+            }
+            
+			_updateToken(account, resolve, reject);
+        }, function(error){
+			console.error("find account failed " + error.message);
+            reject(error);
+        });
+    });
+}
+
+module.exports.check = function(customer_id, token) {
+	return Q.Promise(function(resolve, reject, notify) {
+        var query = new AV.Query(dao.Account);
+        query.equalTo("customer_id", customer_id);
+        query.find().then(function(accounts){
+            if (accounts.length > 0) {
+                var account = accounts[0];
+				
+				if (account.get("token") != token) {
+					reject(new Error("错误的token"));
+				} else {
+					resolve(account);
+				}
+            } else {
+				var account = new dao.Account();
+				account.set("customer_id", customer_id);
+				_updateToken(account, resolve, reject);
+			}
+        }, function(error){
+			console.error("find account failed " + error.message);
+            reject(error);
+        });
+    });
+}
+
+function _updateToken(account, resolve, reject) {
+	var account = new dao.Account();
+	crypto.randomBytes(64, function(error, token) {
+		if (error) {
+			console.error("check new token failed " + error.message);
+			reject(error);
+		} else {			
+			account.set("token", token);
+			account.save().then(function(a){
+				resolve(a);
+			}, function(error){
+				console.error("save account failed " + error.message);
+				reject(error);
+			});
+		}
+	});
+}
