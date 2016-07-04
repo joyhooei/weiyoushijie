06a773a7090b4d920dfc513117a6350ba4a9a804
li weihai <weihailee@hotmail.com>
2016-07-04 03:28:53 +0000

    app.js 已在 Bitbucket 中在线编辑过

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var api = require('./routes/api');
+var om = require('./routes/om');
 var cloud = require('./cloud');
 
 GLOBAL.moment = require("moment");
@@ -56,6 +57,7 @@ app.get('/', function(req, res) {
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/api', api);
+app.use('/om', om);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
