2f796500c0fb7ba2db526b612b3f57b71745a6f5
li weihai <weihailee@hotmail.com>
2016-07-05 01:30:40 +0000

    Application.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/Application.ts
+++ public/headline/src/Application.ts
@@ -22,7 +22,7 @@ module application {
     
     export var vip: Vip;
     
-    export var version: string = '1.5.4';
+    export var version: string = '1.5.5';
     
     export var token: string = "";
 
