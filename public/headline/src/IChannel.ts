96d8b2b79210b8bde745f8ac0ac63196ba175f76
li weihai <weihailee@hotmail.com>
2016-07-07 08:15:42 +0000

    IChannel.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/IChannel.ts
+++ public/headline/src/IChannel.ts
@@ -1,5 +1,45 @@
-interface IChannel {
-    share(options:any): any;
+class Channel {
+    public function share(options:any): Q.Promise<any> {
+        var deferred = Q.defer<any>();
+		
+        nest.share.isSupport({}, function (data) {
+			if (data.share == 1) {
+				nest.share.share(options, function (data) {
+					if(data.result == 0) {
+						deferred.resolve("");
+					} else if(data.result == -1) {
+						deferred.reject("取消了分享");
+					} else {
+						deferred.reject("分享失败");
+					}
+				})
+			} else {
+				deferred.reject("当前平台不支持分享");
+			}
+		});
+		
+		return deferred.promise;
+    }
     
-    attention(): any;
+    public function attention(options:any): Q.Promise<any> {
+        var deferred = Q.defer<any>();
+		
+        nest.app.isSupport({}, function (data) {
+			if (data.attention == 1) {
+                nest.app.attention({}, function (data) {
+					if(data.result == 0) {
+						deferred.resolve();
+					} else if(data.result == -1) {
+						deferred.reject("取消了关注");
+					} else {
+						deferred.reject("关注失败");
+					}
+				})
+			} else {
+				deferred.reject("当前平台不支持关注");
+			}
+		});	
+		
+		return deferred.promise;
+	}
 }
