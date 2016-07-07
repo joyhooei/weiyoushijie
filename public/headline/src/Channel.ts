c628219e76fa0dbf17e238c5c054335fca1c5be4
li weihai <weihailee@hotmail.com>
2016-07-07 08:40:15 +0000

    Channel.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/Channel.ts
+++ public/headline/src/Channel.ts
@@ -1,4 +1,52 @@
+enum ChannelId {
+	CHANNEL_DEFAULT,
+    CHANNEL_1758,
+    CHANNEL_7K7K,
+}
+
 class Channel {
+	static create(channelId:ChannelId): Channel {
+		if (channelId === ChannelId.CHANNEL_1758) {
+			return new Channel1758();
+		} else {
+			return new Channel();
+		}
+	}
+	
+	constructor() {
+    }
+	
+	public function login(options:any): Q.Promise<any> {
+		var deferred = Q.defer<any>();
+		
+        if (options == null || typeof options == "string") {
+            var loginInfo: nest.user.LoginInfo = options ? {"loginType":<string>options} : {};
+            nest.user.login(loginInfo, function(data:nest.user.LoginCallbackInfo){
+				deferred.resolve(data);
+			});
+        } else {
+            deferred.resolve(<nest.user.LoginCallbackInfo>options);
+        }
+		
+		return deferred.promise;
+	}
+	
+	public function pay(options:any): Q.Promise<any> {
+		var deferred = Q.defer<any>();
+		
+		nest.iap.pay(options}, function(data) {
+			if(data.result == 0) {
+				deferred.resolve(data);
+			} else if(data.result == -1) {
+				deferred.reject("用户取消了支付");
+			} else {
+				deferred.reject("支付失败");
+			}
+		})
+		
+		return deferred.promise;
+	}
+	
     public function share(options:any): Q.Promise<any> {
         var deferred = Q.defer<any>();
 		
@@ -16,7 +64,7 @@ class Channel {
 			} else {
 				deferred.reject("当前平台不支持分享");
 			}
-		});
+		})
 		
 		return deferred.promise;
     }
@@ -38,7 +86,7 @@ class Channel {
 			} else {
 				deferred.reject("当前平台不支持关注");
 			}
-		});	
+		})
 		
 		return deferred.promise;
 	}
