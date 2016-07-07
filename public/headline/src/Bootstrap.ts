6bf675b4aa719e9266ada0ae30ca8b1b3f8c1793
li weihai <weihailee@hotmail.com>
2016-07-07 06:41:10 +0000

    Bootstrap.ts 已使用 Bitbucket 在线创建

---
+++ public/headline/src/Bootstrap.ts
@@ -0,0 +1,19 @@
+require.config({
+    baseUrl: 'src',
+
+    paths: {
+    },
+
+    shim: {
+    }
+});
+
+require([], () => {
+	var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+	var element = document.getElementById('egret-player');
+	element.style.width = (Math.min(width, height) - 1) + "px";
+
+	egret.runEgret();
+});
