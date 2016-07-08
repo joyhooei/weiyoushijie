1572b3160531a917c56e929a711536c11730dfc5
li weihai <weihailee@hotmail.com>
2016-07-08 08:45:24 +0000

    dao.js 已在 Bitbucket 中在线编辑过

--- platforms/standalone/dao.js
+++ platforms/standalone/dao.js
@@ -317,14 +317,9 @@ module.exports = function() {
 			console.log("connect mongodb succeed");
 		});
 		
-		if (process.env.LC_APP_ID) {
-			var AV = require('leanengine');
-			app.use(AV.Cloud);
-			
-			mongoose.connect('mongodb://weiyoushijie:weiyugame@ds023644.mlab.com:23644/weiyoushijie');
-		} else {
-			mongoose.connect('mongodb://9b18dc67c08b4434bdf68b0c3ff45477:d35f2aa56b1b4806b9934950c3d89bea@mongo.bce.duapp.com:8908/gmkSqUizKEatLnvxuIcZ', {db: {w: 1}})
-		}
+		//var url = 'mongodb://weiyoushijie:weiyugame@ds023644.mlab.com:23644/weiyoushijie';
+		var url = 'mongodb://9b18dc67c08b4434bdf68b0c3ff45477:d35f2aa56b1b4806b9934950c3d89bea@mongo.bce.duapp.com:8908/gmkSqUizKEatLnvxuIcZ';
+		mongoose.connect(url, {db: {w: 1}});
 	}
 	
 	this.addModel = function(className, schema, uniques) {
