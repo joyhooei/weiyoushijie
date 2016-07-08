a6a6af5f8dfc65bc014594d5ebbe56b374dc6de5
li weihai <weihailee@hotmail.com>
2016-07-08 07:21:23 +0000

    LandingUI.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/views/LandingUI.ts
+++ public/headline/src/views/LandingUI.ts
@@ -13,8 +13,8 @@ class LandingUI extends eui.Component {
         var self = this;
         
         self.btnLogin.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
-            application.channel.login().then(function(data:any){
-                application.logined(data.token);
+            application.channel.login().then(function(token:string){
+                application.logined(token);
 				application.hideUI(self);
 			}, function(error){
 				Toast.launch(error);
