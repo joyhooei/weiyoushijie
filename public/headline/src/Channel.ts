99a32f9beb2eb1895ae0f32ceeaac47317126336
li weihai <weihailee@hotmail.com>
2016-07-08 07:30:43 +0000

    Channel.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/Channel.ts
+++ public/headline/src/Channel.ts
@@ -1,24 +1,29 @@
-const CHANNEL_1758    = "10016";
+const CHANNEL_EGRET   = "egret";
+const CHANNEL_1758    = "1758";
 const CHANNEL_7K7K    = "";
 
 declare var loadfile;
 
 class Channel {
 	static create(): Channel {
-		let cid = egret.getOption("channelId") || egret.getOption("wysj_channel");
+		let cid = egret.getOption("channelId") || egret.getOption("wysj_channel") || "egret";
 		
 		if (cid === CHANNEL_1758) {
 			console.info("using channel 1758");
 			return new Channel1758();
 		} else {
 			console.info("using default channel");
-            return new Channel();
+            return new ChannelEgret();
 		}
 	}
 	
     private _deferred: Q.Deferred<any>;
 	
-	constructor() {
+	public name: string;
+	
+	constructor(name:string) {
+		this.name = name;
+		this._deferred = null;
     }
 	
 	public loadjs(url:string) {
@@ -33,6 +38,18 @@ class Channel {
         this._deferred = Q.defer<any>();
         return this._deferred.promise;        
     }
+	
+	public resolvedPromise() {
+        var promise = this.promise();
+		this._deferred.resolve();
+        return promise;  		
+	}
+	
+	public rejectedPromise() {
+        var promise = this.promise();
+		this._deferred.reject("目前不支持");
+        return promise;  		
+	}
     
     public resolve(data?:any) {
         if (this._deferred) {
@@ -51,99 +68,18 @@ class Channel {
     }
     
     public login(): Q.Promise<any> {
-        let self = this;
-        
- 		nest.easyuser.startup({ egretAppId: 90359,version: 2,debug: true },function(result: nest.core.ResultCallbackInfo) {
-			if(result.result == 0) {
-				var loginTypes: Array<nest.easyuser.ILoginType> = nest.easyuser.getLoginTypes();
-				if (loginTypes.length > 0) {
-					//需要显示对应的登录按钮
-					var loginView: LoginUI = new LoginUI(loginTypes,function(logType: nest.easyuser.ILoginType) {
-						nest.easyuser.login(logType, function (data:nest.user.LoginCallbackInfo) {
-							if (data.result == 0) {
-                                self.resolve(data.token);
-							} else {
-                                self.reject("登录失败");
-							}
-						});
-					});
-
-					application.showUI(loginView);
-				} else {
-					//不需要登录按钮，直接调用登录进游戏
-					nest.easyuser.login({}, function (data:nest.user.LoginCallbackInfo) {
-						if (data.result == 0) {
-                            self.resolve(data.token);
-						} else {
-                            self.reject("登录失败");
-						}
-					});
-				}
-			} else {
-                self.reject("初始化失败");
-			}
-		});
-		
-        return self.promise();
+        return this.rejectedPromise();
 	}
 	
     public pay(options: any): Q.Promise<any> {
-        let self = this;
-		
-		nest.iap.pay(options, function(data) {
-			if(data.result == 0) {
-                self.resolve(data);
-			} else if(data.result == -1) {
-                self.reject("用户取消了支付");
-			} else {
-                self.reject("支付失败");
-			}
-		})
-		
-        return self.promise();
+        return this.rejectedPromise();
 	}
 	
     public share(options:any): Q.Promise<any> {
-        let self = this;
-
-        nest.share.isSupport({}, function (data) {
-			if (data.share == 1) {
-				nest.share.share(options, function (data) {
-					if(data.result == 0) {
-                        self.resolve("");
-					} else if(data.result == -1) {
-                        self.reject("取消了分享");
-					} else {
-                        self.reject("分享失败");
-					}
-				})
-			} else {
-                self.reject("当前平台不支持分享");
-			}
-		})
-		
-        return self.promise();
+        return this.rejectedPromise();
     }
     
     public attention(options:any): Q.Promise<any> {
-        let self = this;
-		
-        nest.app.isSupport({}, function (data) {
-			if (data.attention == 1) {
-                nest.app.attention({}, function (data) {
-					if(data.result == 0) {
-                        self.resolve();
-					} else if(data.result == -1) {
-                        self.reject("取消了关注");
-					} else {
-                        self.reject("关注失败");
-					}
-				})
-			} else {
-				self.reject("当前平台不支持关注");
-			}
-		})
-		
-        return self.promise();
+        return this.rejectedPromise();
 	}
 }
