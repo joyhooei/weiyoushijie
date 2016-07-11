31c46589d14a1e089134bf851ec4975a7c294dab
li weihai <weihailee@hotmail.com>
2016-07-11 01:13:42 +0000

    Channel1758.js 已在 Bitbucket 中在线编辑过

--- public/headline/bin-debug/channels/Channel1758.js
+++ public/headline/bin-debug/channels/Channel1758.js
@@ -13,58 +13,7 @@ var Channel1758 = (function (_super) {
         };
         return self.promise();
     };
-    p.login = function () {
-        var self = this;
-        var token = egret.getOption("userToken");
-        if (token) {
-            application.dao.rest("login", { token: token, wysj_channel: "10016" }, function (succeed, account) {
-                if (succeed) {
-                    self.resolve(account);
-                }
-                else {
-                    self.reject("登录失败");
-                }
-            });
-        }
-        else {
-            application.delay(function () {
-                self.reject("授权失败");
-            }, 100);
-        }
-        return self.promise();
-    };
-    p.pay = function (options) {
-        var self = this;
-        hlmy.pay({
-            itemCode: options.goodsId,
-            txId: options.ext,
-            state: options.ext //可选. CP 自定义参数 , 1758 平台会在支付结果通知接口中, 把该参数透传给 CP 游戏服务器
-        });
-        return self.promise();
-    };
-    p.attention = function (options) {
-        var self = this;
-        application.dao.fetch("Gift", { "category": GiftCategory.Attention, "customer_id": application.customer.id }, { limit: 1 }, function (succeed, gifts) {
-            if (succeed && gifts.length > 0) {
-                var gift = gifts[0];
-                if (gift.locked == 2) {
-                    self.reject(new Error("已经领取过礼物"));
-                }
-                else {
-                    if (parseInt(application.customer.channel_data) == 0) {
-                        window.location.href = "http://mp.weixin.qq.com/s?__biz=MjM5MjQyOTg3MA==&mid=207867528&idx=1&sn=19c7b9";
-                    }
-                    else {
-                        self.resolve();
-                    }
-                }
-            }
-            else {
-                self.reject("内部错误");
-            }
-        });
-        return self.promise();
-    };
+
     return Channel1758;
 }(ChannelEgret));
 egret.registerClass(Channel1758,'Channel1758');
