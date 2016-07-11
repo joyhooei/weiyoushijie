35523c46f08a8ad2fb5a23b8e8fb2a3878d3e6cd
li weihai <weihailee@hotmail.com>
2016-07-11 08:45:29 +0000

    helper.js 已在 Bitbucket 中在线编辑过

--- channels/helper.js
+++ channels/helper.js
@@ -50,6 +50,30 @@ module.exports.get = function(url) {
 	});	
 }
 
+module.exports.pay = function(channel, orderId, price) {
+	var self = this;
+	
+	return Q.Promise(function(resolve, reject, notify) {
+		dao.get("Order", orderId).then(function(order){
+			if (order.get("state") != 1) {
+				order.set("price", parseInt(price));
+				order.set("channel", channel);
+				Order.pay(order).then(function(o){
+					resolve('pay successfully');
+				}, function(error){
+					console.error("order pay failed " + channelId + " " + orderId + " " +  error.message);
+					reject("pay failed");
+				});
+			} else {
+				resolve('paid yet');
+			}
+		}, function(error){
+			console.error("order does not existed " + channelId + " " + orderId + " " +  error.message);
+			reject("order not existed");
+		});
+	});
+}
+
 module.exports.crypto = function(data) {
 	return crypto.createHash('md5').update(data).digest('hex');
 }
