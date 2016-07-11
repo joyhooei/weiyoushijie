c1b5dd2f7526153022b3bf6eb9888660b0d94f2c
li weihai <weihailee@hotmail.com>
2016-07-11 08:12:31 +0000

    channel_huhuh5.js 已使用 Bitbucket 在线创建

---
+++ channels/channel_huhuh5.js
@@ -0,0 +1,17 @@
+var Helper = require('./helper');
+
+module.exports.login = function(token) {
+	var self = this;
+	
+	return Q.Promise(function(resolve, reject, notify) {
+
+	});    	
+}
+
+module.exports.pay = function(options) {
+	var self = this;
+	
+	return Q.Promise(function(resolve, reject, notify) {
+
+	});
+}
