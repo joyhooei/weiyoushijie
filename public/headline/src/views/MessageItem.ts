ecd03ca313ed17a1377ddbdf607fbaf9792e8ae7
li weihai <weihailee@hotmail.com>
2016-07-12 11:53:18 +0000

    MessageItem.ts 已在 Bitbucket 中在线编辑过

--- public/headline/src/views/MessageItem.ts
+++ public/headline/src/views/MessageItem.ts
@@ -33,6 +33,8 @@ class MessageItem extends eui.Component {
             if(this.imgOperate.source == "get_png") {
                 if(message.attach_category == "diamond") {
                     application.customer.diamond += message.attach_quantity;
+                    
+                    application.channel.track(TRACK_CATEGORY_DIAMOND, TRACK_ACTION_INC, "奖品奖励", message.attach_quantity); 
                 } else if(message.attach_category == "gold") {
                     application.customer.gold += message.attach_quantity;
                 } else {
