43d75224d4b14e961e741aae7944879ff9f4b082
liweihai <liweihai@huawei.com>
2016-07-13 16:30:32 +0800

    Update gifts.js

--- routes/gifts.js
+++ routes/gifts.js
@@ -6,7 +6,8 @@ var helper = require("./helper");
 var _restfulName  = "gifts";
 
 router.get('/', helper.ensureAuthenticated, function(req, res, next) {
-	helper.listModel("Gift", "gifts", _restfulName, req, res);
+	var query = dao.find("Gift", {customer_id: req.query.customer_id}, {order: 'category ASC'});
+	helper.queryModel(query, "gifts", _restfulName, req, res);	
 });
 
 module.exports = router;
