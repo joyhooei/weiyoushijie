f19837cddc26b8fcbfc5ee5dbe3d776992ca92bd
liweihai <liweihai@huawei.com>
2016-07-13 13:05:50 +0800

    Update rank.js

--- models/rank.js
+++ models/rank.js
@@ -1,36 +1,37 @@
-module.exports.rank = function(req, res) {
-	dao.findAll("Rank", {"game":"headline"}, {"order":"rank ASC"}).then(function (ranks){
-		dao.findAll("Customer", {"game":"headline"}, {"order":"metal DESC, accumulated_gold DESC", 'select':'objectId'}).then(function(customers){
-			var newRanks = [];
-			
-			for(var i = 0; i < customers.length; i++) {
-				var c = customers[i];
+module.exports.rank = function() {
+	return Q.Promise(function(resolve, reject, notify) {
+		dao.findAll("Rank", {"game":"headline"}, {"order":"rank ASC"}).then(function (ranks){
+			dao.findAll("Customer", {"game":"headline"}, {"order":"metal DESC, accumulated_gold DESC", 'select':'objectId'}).then(function(customers){
+				var newRanks = [];
 				
-				if (i >= ranks.length) {
-					newRanks.push(new dao.Rank({game: "headline", rank: i + 1, customer_id: c.id}));
-				} else {
-					var r = ranks[i];
-					if (r.get("customer_id") != c.id) {
-						r.set("customer_id", c.id);
-						newRanks.push(r);
+				for(var i = 0; i < customers.length; i++) {
+					var c = customers[i];
+					
+					if (i >= ranks.length) {
+						newRanks.push(new dao.Rank({game: "headline", rank: i + 1, customer_id: c.id}));
+					} else {
+						var r = ranks[i];
+						if (r.get("customer_id") != c.id) {
+							r.set("customer_id", c.id);
+							newRanks.push(r);
+						}
 					}
 				}
-			}
-				
-			res.success("rank succeed " + newRanks.length);
-
-			dao.saveAll(newRanks).then(function (avobjs) {
-				console.log("rank " + newRanks.length);				
-			}, function (error) {
-				console.error("rank saveAll " + error.message);
-				res.error(error.message);
+	
+				dao.saveAll(newRanks).then(function (avobjs) {
+					console.log("rank " + newRanks.length);	
+					resolve(newRanks.length);
+				}, function (error) {
+					console.error("rank saveAll " + error.message);
+					reject(error.message);
+				});
+			}, function(error){
+				console.error("rank findAll customer " + error.message);
+				reject(error.message);
 			});
-		}, function(error){
-			console.error("rank findAll customer " + error.message);
-			res.error(error.message);
-		});
-  	}, function (error) {
-		console.error("rank findAll rank " + error.message);		
-		res.error(error.message);
-	});	
+	  	}, function (error) {
+			console.error("rank findAll rank " + error.message);		
+			reject(error.message);
+		});	
+	});
 }
