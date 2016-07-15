20400f4e572fe27dce66a54d70fdd9e0ef6daa8e
liweihai <liweihai@huawei.com>
2016-07-15 11:56:56 +0800

    Create Utility.ts

---
+++ public/headline/src/Utility.ts
@@ -0,0 +1,50 @@
+class Utility {
+    static function log10(d:number):number {
+        let result:number = 0;
+        
+        //可能出现9.9999999e+25的情况
+        while(d >= 9) {
+            result += 1;
+            d = d / 10;
+        }
+        
+        return result;
+    }
+    
+    static function format(d:number): string {
+		try {
+			if (d <= 99999) {
+				return new Number(d).toFixed();
+			}
+        
+			let unit:string  = "";		
+			for (var i = 0; i < application.units.length; i++) {
+				if (d < 10) {
+                    return new Number(d).toFixed(2) + unit;
+				} else if (d < 100) {
+                    return new Number(d).toFixed(1) + unit;
+				} else if (d < 1000) {
+                    return new Number(d).toFixed() + unit;
+				} else {
+					unit = application.units[i];
+					d = d / 1000;
+				}
+			}
+			
+            return new Number(d).toFixed() + unit;
+		} catch (error) {
+            console.error("format " + d + " error " + error.message);
+			
+			return "0";
+		}
+    }
+    
+	static function delay(cb: Function, miniseconds: number) {
+		var timer: egret.Timer = new egret.Timer(miniseconds, 1);
+		timer.addEventListener(egret.TimerEvent.TIMER,function(event: egret.TimerEvent) {
+			cb();
+		},this);
+		timer.start();	
+	} 
+}
+        
