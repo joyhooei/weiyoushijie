162115117388927c22332e0b7b7dd2e09343c53c
liweihai <liweihai@huawei.com>
2016-07-15 11:31:36 +0800

    Update Dao.ts

--- public/headline/src/Dao.ts
+++ public/headline/src/Dao.ts
@@ -9,69 +9,84 @@ class Dao extends egret.EventDispatcher {
         this._gameName = gameName;
     }
 	
-	public fetch(model:string, conditions:{}, filters:{}, cb: Function) {
+	public fetch(model:string, conditions:{}, filters?:{}, cb?: Function) {
         if (conditions) {
             conditions["game"] = this._gameName;
         } else {
             conditions = {game: this._gameName};
         }
         
-		this.rest("select/" + model, {conditions: conditions, filters: filters}, cb);
+		return this.rest("select/" + model, {conditions: conditions, filters: filters}, cb);
 	}
 	
 	public save(model:string, data:any, cb?: Function) {
-    	var self = this;
+    	let self = this;
     	
-        var _cb = function(succeed:number, result: any) {
-            if (succeed) {
-                data = result;
-            } else {
-                Toast.launch(result.$target.response);
-            }
-            
-            self.dispatchEventWith(model,true,data);
+        let doUpdate = data.id;
 
-            if (cb) {
-                cb(succeed, result);
-            }
-        }
-        
         data.game = this._gameName;
         
-		if (data.id){
-			this.rest("update/" + model + "/" + data.id, data, _cb);
+		if (doUpdate){
+			var promise = this.rest("update/" + model + "/" + data.id, data);
+			
+			self.dispatchEventWith(model, true, data);
 		} else {
-			this.rest("create/" + model, data, _cb);
+			var promise = this.rest("create/" + model, data);
 		}
+		
+		promise.then(function(d){
+			data = d;
+			
+			if (!doUpdate) {
+				self.dispatchEventWith(model, true, data);
+			}
+
+			if (cb) {
+				cb(true, result);
+			}
+		}, function(error){
+			Toast.launch(error);
+
+			if (cb) {
+				cb(false, error);
+			}
+		});
+		
+		return promise;
 	}
     
-    public rest(method: string, data: {}, cb: Function) {
-		var request = new egret.HttpRequest();
-		request.responseType = egret.HttpResponseType.TEXT;
-		
-		//设置为 POST 请求
-		request.open(this._baseUrl + method + "?game=" + this._gameName + "&token=" + application.token, egret.HttpMethod.POST);
-		request.setRequestHeader("Content-Type", "application/json");
+    public rest(method: string, data: {}, cb?: Function) {
+    	return Q.Promise(function(resolve, reject, notify) {
+			var request = new egret.HttpRequest();
+			request.responseType = egret.HttpResponseType.TEXT;
+			
+			//设置为 POST 请求
+			request.open(this._baseUrl + method + "?game=" + this._gameName + "&token=" + application.token, egret.HttpMethod.POST);
+			request.setRequestHeader("Content-Type", "application/json");
+	
+	        request.addEventListener(egret.Event.COMPLETE, (evt: egret.Event) => {
+	            var request = <egret.HttpRequest>evt.currentTarget;
 
-        request.addEventListener(egret.Event.COMPLETE, (evt: egret.Event) => {
-            var request = <egret.HttpRequest>evt.currentTarget;
-            console.log("post data : ",request.response);
-
-            if(cb) {
-                cb(true,JSON.parse(request.response));
-            }            
-        }, this);
-            
-        request.addEventListener(egret.IOErrorEvent.IO_ERROR,(evt: egret.IOErrorEvent) => {
-            if(cb) {
-                cb(false,evt);
-            }       
-        }, this);
-        
-        request.addEventListener(egret.ProgressEvent.PROGRESS,(evt: egret.ProgressEvent) => {
-            console.log("post progress : " + Math.floor(100 * evt.bytesLoaded / evt.bytesTotal) + "%");
-        },this);
-		
-		request.send(JSON.stringify(data));
+	            if(cb) {
+	                cb(true,JSON.parse(request.response));
+	            }            
+	            	
+	            resolve(JSON.parse(request.response));
+	        }, this);
+	            
+	        request.addEventListener(egret.IOErrorEvent.IO_ERROR,(evt: egret.IOErrorEvent) => {
+	            if(cb) {
+	                cb(false, evt.$target.response);
+	            }
+	            
+                reject(evt.$target.response);
+	        }, this);
+	        
+	        request.addEventListener(egret.ProgressEvent.PROGRESS,(evt: egret.ProgressEvent) => {
+	            notify(Math.floor(100 * evt.bytesLoaded / evt.bytesTotal));
+	        },this);
+			
+			request.send(JSON.stringify(data));
+		});
     }
 }
