0c969b1b553a43439e01eac52d3b39247ecfaf04
liweihai <liweihai@huawei.com>
2016-07-18 17:07:14 +0800

    Update dao.js

--- platforms/standalone/dao.js
+++ platforms/standalone/dao.js
@@ -406,15 +406,17 @@ module.exports = function() {
 		});
 		
 		if (process.env.LC_APP_ID) {
+			//leancloud
 			var AV = require('leanengine');
 			app.use(AV.Cloud);
 
 			var url = 'mongodb://weiyoushijie:weiyugame@ds023644.mlab.com:23644/weiyoushijie';
-			
-			//mongoose.set('debug', true);
-		} else {
+		} else if (process.env.PORT) {
+			//heroku
 			var url = 'mongodb://weiyoushijie:weiyugame@ds023644.mlab.com:23644/weiyoushijie';
-			//var url = 'mongodb://9b18dc67c08b4434bdf68b0c3ff45477:d35f2aa56b1b4806b9934950c3d89bea@mongo.bce.duapp.com:8908/gmkSqUizKEatLnvxuIcZ';
+		} else {
+			//bae
+			var url = 'mongodb://9b18dc67c08b4434bdf68b0c3ff45477:d35f2aa56b1b4806b9934950c3d89bea@mongo.bce.duapp.com:8908/gmkSqUizKEatLnvxuIcZ';
 		}
 
 		mongoose.connect(url, {db: {w: 1}});
