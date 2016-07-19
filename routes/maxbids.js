a53048c8f3c5c29fc0291f369808ffc20909b469
liweihai <liweihai@huawei.com>
2016-07-19 09:42:22 +0800

    Update maxbids.js

--- routes/maxbids.js
+++ routes/maxbids.js
@@ -6,7 +6,7 @@ var helper = require("./helper");
 var _restfulName  = "maxbids";
 
 router.get('/', helper.ensureAuthenticated, function(req, res, next) {
-	helper.listModel("Maxbid", "maxbids", _restfulName, req, res);
+	helper.listModel("MaxBid", "maxbids", _restfulName, req, res);
 });
 
 module.exports = router;
