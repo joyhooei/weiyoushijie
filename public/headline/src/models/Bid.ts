e5f94a62a2586d15bf1632795929fb308a06804f
liweihai <liweihai@huawei.com>
2016-07-19 16:16:00 +0800

    Update Bid.ts

--- public/headline/src/models/Bid.ts
+++ public/headline/src/models/Bid.ts
@@ -1,4 +1,10 @@
 class Bid {
+	public current: any;
+	
+	public constructor() {
+		this.current = null;
+	}
+	
     public static day(): string {
 		//中午12点开标，所以12点之后的投标算明天的
 		var dt = new Date();
@@ -9,29 +15,33 @@ class Bid {
 		return dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
     }
     
-    public static refresh() {
+    public refresh(customer:Customer) {
+    	var self = this;
+    	
         return Q.Promise(function(resolve, reject, notify) {
-            application.dao.fetch("Bid",{ succeed: 0, day :application.bidDay(), customer_id: application.customer.id}, {limit : 1}).then(function(bids){
+            application.dao.fetch("Bid",{ succeed: 0, day :Bid.day(), customer_id: customer.me.id}, {limit : 1}).then(function(bids){
                 if (bids.length > 0) {
-                    application.bid = bids[0];				
+                    self.bid = bids[0];
                 } else {
-                    application.bid = null;
+                    self.bid = null;
                 }
     
-                resolve(application.bid);
+                resolve(self.bid);
             })
         }, function(error) {
-            resolve(application.bid);
+            resolve(self.bid);
         });
     }
     
-    public static earn(): void {
-		application.dao.fetch("Bid", {customer_id: application.customer.id, succeed: {$gte:1}, claimed: 0}, {}).then(function(bids){
+    public static earn(customer:Customer): void {
+    	var self = this;
+    	
+		application.dao.fetch("Bid", {customer_id: customer.me.id, succeed: {$gte:1}, claimed: 0}, {}).then(function(bids){
 			if (bids.length > 0) {
 				for(var i = 0; i < bids.length; i++) {
-					application.customer.gold -= bids[i].gold;
+					customer.me.gold -= bids[i].gold;
 					if (bids[i].rank == 1) {
-						application.customer.diamond += 2000;
+						customer.me.diamond += 2000;
 						application.channel.track(TRACK_CATEGORY_DIAMOND, TRACK_ACTION_INC, "拍卖头名", 2000); 
 					}
 
@@ -39,7 +49,7 @@ class Bid {
 					application.dao.save("Bid", bids[i]);
 				}
 				
-				Customer.saveNow();
+				customer.saveNow();
 			}
 		});
     }
