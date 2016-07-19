f9abe25a0c138b6a44b1e595458529232f17da01
liweihai <liweihai@huawei.com>
2016-07-19 16:26:08 +0800

    Update WinUI.ts

--- public/headline/src/views/WinUI.ts
+++ public/headline/src/views/WinUI.ts
@@ -10,15 +10,15 @@ class WinUI extends eui.Component{
         this.skinName = "resource/custom_skins/winUISkin.exml";
         
         this.imgBack.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
-			application.earnBids();
+			Bid.earn(application.customer);
             application.hideUI(this);
         }, this);
         
         this.imgHide.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
-			if (application.customer.hide_winner == 1) {
-            	application.customer.hide_winner = 0;
+			if (application.customer.me.hide_winner == 1) {
+            	application.customer.me.hide_winner = 0;
 			} else {
-				application.customer.hide_winner = 1;
+				application.customer.me.hide_winner = 1;
 			}
 			
 			this.renderAvatar();
@@ -28,14 +28,14 @@ class WinUI extends eui.Component{
     }
 	
     private renderAvatar(): void {
-		if (application.customer.hide_winner == 1) {
+		if (application.customer.me.hide_winner == 1) {
 			this.imgHide.source = "hidecancel_png";
 			
 			this.imgAvatar.source = "Ahide_png"
 		} else {
 			this.imgHide.source = "Awinhide_png";
 			
-            this.imgAvatar.source = application.avatarUrl(application.customer);
+            this.imgAvatar.source = Customer.avatarUrl(application.customer.me);
 		}
 	}
 }
