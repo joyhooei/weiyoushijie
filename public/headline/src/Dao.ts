b917bda2f3e81ab461bad33e50c7d0e4abcbb1db
liweihai <liweihai@huawei.com>
2016-07-19 17:26:01 +0800

    Update Dao.ts

--- public/headline/src/Dao.ts
+++ public/headline/src/Dao.ts
@@ -1,30 +1,29 @@
 class Dao extends egret.EventDispatcher {
-    private _baseUrl: string;
-    private _gameName: string;
+    private _url: string;
+    private _game: string;
     
-    constructor(baseUrl: string, gameName: string) {
+    constructor(url: string, game: string) {
         super();
         
-        this._baseUrl = baseUrl;
-        this._gameName = gameName;
+        this._url = url;
+        this._game = game;
     }
 	
-	public fetch(model:string, conditions:{}, filters?:{}, cb?: Function) {
-        if (conditions) {
-            conditions["game"] = this._gameName;
-        } else {
-            conditions = {game: this._gameName};
-        }
+	public fetch(model:string, conditions:{}, filters?:{}) {
+		conditions = conditions || {};
+        conditions["game"] = this._game;
         
-		return this.rest("select/" + model, {conditions: conditions, filters: filters}, cb);
+        filters = filters || {};
+        
+		return this.rest("select/" + model, {conditions: conditions, filters: filters});
 	}
 	
-	public save(model:string, data:any, cb?: Function) {
+	public save(model:string, data:any) {
     	let self = this;
     	
         let doUpdate = data.id;
 
-        data.game = this._gameName;
+        data.game = this._game;
         
 		if (doUpdate){
 			var promise = this.rest("update/" + model + "/" + data.id, data);
@@ -40,45 +39,29 @@ class Dao extends egret.EventDispatcher {
 			if (!doUpdate) {
 				self.dispatchEventWith(model, true, data);
 			}
-
-			if (cb) {
-				cb(true, result);
-			}
 		}, function(error){
 			Toast.launch(error);
-
-			if (cb) {
-				cb(false, error);
-			}
 		});
 		
 		return promise;
 	}
     
-    public rest(method: string, data: {}, cb?: Function) {
+    public rest(method: string, data: {}) {
     	return Q.Promise(function(resolve, reject, notify) {
 			var request = new egret.HttpRequest();
 			request.responseType = egret.HttpResponseType.TEXT;
 			
 			//设置为 POST 请求
-			request.open(this._baseUrl + method + "?game=" + this._gameName + "&token=" + application.token, egret.HttpMethod.POST);
+			request.open(this._url + method + "?game=" + this._game + "&token=" + application.token, egret.HttpMethod.POST);
 			request.setRequestHeader("Content-Type", "application/json");
 	
 	        request.addEventListener(egret.Event.COMPLETE, (evt: egret.Event) => {
 	            var request = <egret.HttpRequest>evt.currentTarget;
-
-	            if(cb) {
-	                cb(true,JSON.parse(request.response));
-	            }            
 	            	
 	            resolve(JSON.parse(request.response));
 	        }, this);
 	            
 	        request.addEventListener(egret.IOErrorEvent.IO_ERROR,(evt: egret.IOErrorEvent) => {
-	            if(cb) {
-	                cb(false, evt.$target.response);
-	            }
-	            
                 reject(evt.$target.response);
 	        }, this);
 	        
