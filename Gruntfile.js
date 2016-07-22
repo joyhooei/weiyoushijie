0682270fbc0c382bec8bda4c84f0ff1d611eb235
liweihai <liweihai@huawei.com>
2016-07-22 15:55:39 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,16 +22,45 @@ module.exports = function(grunt) {
             }
         },
         
+       shell: {
+            options: {
+                stderr: false
+            },
+            target: {
+                command: [
+                    './public/headline/egret publish',
+                    './public/headline/egret publish --runtime native'
+                ]
+            }
+        },        
+        
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: ['./app.conf'], dest: '../wysj/app.conf'},
+                    {expand: true, src: ['./package.json'], dest: '../wysj/package.json'},
+
+                    {expand: true, src: ['./dist/server.min.js'], dest: '../wysj/server.js'},
+                    {expand: true, src: ['./views/**'], dest: '../wysj/views/'},
+                    {expand: true, src: ['./public/css/**', './public/font/**','./public/images/**','./public/img/**','./public/javascripts/**','./public/js/**','./public/stylesheets/**', './public/*.*'], dest: '../wysj/public/'},
+                    
+                    {expand: true, src: ['./public/headline/bin-release/*'], dest: '../wysj/public/headline/bin-release/'},
+                ],
+            },
+        },        
+        
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint']
-        }        
+        },
     });
   
     grunt.loadNpmTasks('grunt-node-optimize');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-shell');
     
     // Default task(s).
-    grunt.registerTask('default', ['node_optimize', 'jshint']);
+    grunt.registerTask('default', ['copy', 'node_optimize', 'shell', 'jshint']);
 };
