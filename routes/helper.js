8d8442d5a38c4ec8c181c984bbd8b839136546dc
liweihai <liweihai@huawei.com>
2016-07-26 14:28:39 +0800

    Update helper.js

--- routes/helper.js
+++ routes/helper.js
@@ -2,6 +2,33 @@ var fs = require('fs');
 var crypto = require('crypto');
 var multer  = require('multer');
 
+module.exports.getChannel = function(req) {
+	var channel = req.body.wysj_channel || req.query.wysj_channel || "egret";
+
+	if (channel === "1758") {
+		return require("../channels/channel_1758");
+	} else if (channel === "huhuh5") {
+		return require("../channels/channel_huhuh5");
+	} else if (channel === "51h5") {
+		return require("../channels/channel_51h5");
+	} else {
+		return require("../channels/channel_egret");
+	}
+};
+
+module.exports.succeed = function(res, data) {
+	data = data || {};
+	if (_.isNumber(data)) {
+		data = data.toString();
+	}
+	res.status(200).send(data);
+};
+
+module.exports.failed = function(res, error, status) {
+	status = status || 500;
+	res.status(status).send(error.message);
+};
+
 module.exports.redirect = function (restfulName, req, res) {
 	_redirect(restfulName, req, res);
 };
