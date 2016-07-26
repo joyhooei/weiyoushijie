10b06a2d5b95bcd813e1b269d17b1c209d2d1938
liweihai <liweihai@huawei.com>
2016-07-26 09:56:29 +0800

    Update Channel51H5.ts

--- public/headline/src/channels/Channel51H5.ts
+++ public/headline/src/channels/Channel51H5.ts
@@ -1,10 +1,31 @@
 class Channel51H5 extends Channel{
+	public appId: string = "";
+	
 	constructor(standalone:boolean) {
         super(standalone);
     }
     
+    public loginQuietly(): Q.Promise<any> {
+        let self = this;
+
+		if (egret.getOption("code")) {
+			application.dao.rest("login",{ token: egret.getOption("code"), wysj_channel: "51h5" }).then(function(account){
+            	self.resolve(account);
+            }, function(error) {
+                self.reject("登录失败");
+            });
+	                            
+			return self.promise();
+		} else {
+			return self.rejectedPromise();
+		}
+	}
+    
     public login(): Q.Promise<any> {
         let self = this;
+        
+        location.href = 'http://web.51h5.com/sso.html?appid=' + this.appId 
+        				+ '&redirect=' + encodeURIComponent(application.baseUrl + "?wysj_channel=51h5");
 		
         return self.promise();
 	}
