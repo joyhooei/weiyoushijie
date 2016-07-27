a0244acee747400c215accc5d0cace83c5306faa
liweihai <liweihai@huawei.com>
2016-07-27 11:48:23 +0800

    Update channel_51h5.js

--- channels/channel_51h5.js
+++ channels/channel_51h5.js
@@ -4,9 +4,9 @@ var Customer = require('../models/customer');
 
 function _post(url, data) {
 	return Q.Promise(function(resolve, reject, notify) {
-		data.appid = 'fg40249b';
+		data.appid = 'y6k9mjsn';
 
-		var sign = Helper.join(data, "&") + "dwdse2tsz70go8dq62pzmj10bpkqh08j";
+		var sign = Helper.join(data, "&") + "1m0ukliipkkjcq0ocuc5nvux7y322zah";
 		var buf = new Buffer(sign);
 		sign = buf.toString("binary");
 		data.sign = Helper.crypto(sign);
@@ -27,8 +27,8 @@ function _post(url, data) {
 
 module.exports.login = function(game, options) {
 	return Q.Promise(function(resolve, reject, notify) {
-		_post("http://api.web.51h5.com/auth/token", {code:options.token}).then(function(tokens){
-			_post("http://api.web.51h5.com/auth/info", {token:tokens.data.access_token}).then(function(body){
+		_post("http://dev.api.web.51h5.com/auth/token", {code:options.token}).then(function(tokens){
+			_post("http://dev.api.web.51h5.com/auth/info", {token:tokens.data.access_token}).then(function(body){
 				var user = {
 					name: body.data.nick, 
 					uid:body.data.openid, 
@@ -55,10 +55,10 @@ module.exports.login = function(game, options) {
 module.exports.payUrl = function(options) {
 	return Q.Promise(function(resolve, reject, notify) {
 		dao.get("Customer", options.customerId).then(function(customer){
-			_post("http://api.web.51h5.com/auth/refresh", {refresh:customer.get('channel_data')}).then(function(tokens){
+			_post("http://dev.api.web.51h5.com/auth/refresh", {refresh:customer.get('channel_data')}).then(function(tokens){
 				customer.set("channel_data", tokens.data.refresh_token);
 				dao.save("Customer", customer).then(function(c){
-					_post("http://api.web.51h5.com/pay/order", {token:tokens.data.access_token, total_fee:options.money, subject:options.goodsId, body:options.goodsName, exten:options.orderId}).then(function(body){
+					_post("http://dev.api.web.51h5.com/pay/order", {token:tokens.data.access_token, total_fee:options.money, subject:options.goodsId, body:options.goodsName, exten:options.orderId}).then(function(body){
 						resolve(body.data.pay_url);
 					}, function(error){
 						reject(error);
