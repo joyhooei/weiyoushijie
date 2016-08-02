66e2d0c5832036052f9042f0649983d8e952103e
liweihai <liweihai@huawei.com>
2016-08-02 15:53:52 +0800

    Create Map01.ts

---
+++ public/tower/src/sprites/maps/Map01.ts
@@ -0,0 +1,17 @@
+class Map01 extends Map {
+    public constructor() {
+        super();
+    }
+    
+    //增加塔基放置点
+    protected addTowerBase() {
+    }
+
+    //增加英雄
+    protected addHero() {
+    }
+    
+    //增加敌人
+    protected addStandbys() {
+    }
+}
