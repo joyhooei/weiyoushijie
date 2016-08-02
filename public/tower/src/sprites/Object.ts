6be009b16d3fb3924ad73e68f271b6a35d99c443
liweihai <liweihai@huawei.com>
2016-08-02 19:11:09 +0800

    Update Object.ts

--- public/tower/src/sprites/Object.ts
+++ public/tower/src/sprites/Object.ts
@@ -1,7 +1,6 @@
 enum ObjectState {
     idle,
     moving,
-    stand,
     fighting,
     dying,
     dead
@@ -9,16 +8,23 @@ enum ObjectState {
 
 class Object extends egret.Sprite {
 	private _state: ObjectState;
+	private _ticks: number;
 
     public constructor() {
         super();
 	}
 	
 	public intersect(x: number, y: number, radius: number):boolean {
-		return !(x - radius > this.x + this.width || 
-           x + radius < this.x || 
-           y - radius > this.y + this.height ||
-           y + radius < this.y);
+		let dx = this.x - x;
+		let dy = this.y - y;
+		return (dx * dx + dy * dy <= radius * radius);
+	}
+	
+	public collide(obj: Object) {
+		return !(obj.x > this.x + this.width || 
+           obj.x + obj.width < this.x || 
+           obj.y > this.y + this.height ||
+           obj.y + obj.height < this.y);		
 	}
 	
 	public dead(): boolean {
@@ -35,6 +41,39 @@ class Object extends egret.Sprite {
     
     /**更新状态*/
     public update(ticks:number):void {
+    	switch(this._state) {
+		    case ObjectState.idle:
+		    	this._idle();
+		    	break;
+		    	
+   	    	case ObjectState.moving,
+		    	this._moving();
+		    	break;
+
+   	    	case ObjectState.fighting,
+		    	this._fighting();
+		    	break;
+		    	
+   	    	case ObjectState.dying,
+		    	this._dying();
+		    	break;
+    	}
+    }
+    
+    protected _idle() {
+    	
+    }
+
+    protected _moving() {
+    	
+    }
+    
+    protected _fighting() {
+    	
+    }
+    
+    protected _dying() {
+    	
     }
     
     /**显示*/
