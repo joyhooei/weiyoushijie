3d749641c97632292fc2f002ddde6b8b2612f5f8
liweihai <liweihai@huawei.com>
2016-08-02 20:03:49 +0800

    Update Tower.ts

--- public/tower/src/sprites/towers/Tower.ts
+++ public/tower/src/sprites/towers/Tower.ts
@@ -3,4 +3,8 @@ class Tower extends Object
     public constructor() {
         super();
     }
+    
+    protected _idle(ticks:number) {
+        this._changeState(ObjectState.building);
+    }    
 }
