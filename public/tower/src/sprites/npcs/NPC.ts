2d565c9b7890f4d95e46295e49d019d9a80f9028
liweihai <liweihai@huawei.com>
2016-08-02 17:47:36 +0800

    Update NPC.ts

--- public/tower/src/sprites/npcs/NPC.ts
+++ public/tower/src/sprites/npcs/NPC.ts
@@ -1,5 +1,20 @@
 class NPC extends Object {
+    private _blood: number;
+    
+    private _damage: number;
+    
     public constructor() {
         super();
+    }
+    
+    public hitBy(damage:number) {
+        this._blood -= damage;
+        if (this._blood < 0) {
+            this._state = ObjectState.dying;
+        }
+    }
+    
+    private _hit(npc: NPC) {
+        npc.hitBy(this._damage);
     }
 }
