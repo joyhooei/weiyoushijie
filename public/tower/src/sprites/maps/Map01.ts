315af42332e159f5f5212d4eb7c3b526612d18e8
liweihai <liweihai@huawei.com>
2016-08-02 16:18:31 +0800

    Update Map01.ts

--- public/tower/src/sprites/maps/Map01.ts
+++ public/tower/src/sprites/maps/Map01.ts
@@ -4,14 +4,23 @@ class Map01 extends Map {
     }
     
     //增加塔基放置点
-    protected addTowerBases() {
+    protected addBases() {
+        let x = [100];
+        let y = [100];
+        
+        for(let i = 0; i < x.length; i++) {
+            this._addBase(x[i], y[i], new Base());
+        }
     }
 
     //增加英雄
     protected addHero() {
+        this._setHero(100, 200, new Hero());
     }
     
     //增加敌人
     protected addStandbys() {
+        let pah = [];
+        this._addStandbys(new Enemy(), path, 10);
     }
 }
