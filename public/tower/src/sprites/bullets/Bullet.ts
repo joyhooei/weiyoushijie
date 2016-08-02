7253318fc4c8700e41c3ad9ec8acbc8cce8c728c
liweihai <liweihai@huawei.com>
2016-08-02 19:03:51 +0800

    Update Bullet.ts

--- public/tower/src/sprites/bullets/Bullet.ts
+++ public/tower/src/sprites/bullets/Bullet.ts
@@ -1,5 +1,30 @@
 class Bullet extends Object {
+    private _target: NPC;
+    
+    private _speed: number;
+    
     public constructor() {
         super();
+    }
+    
+    protected _moving(ticks: number) {
+        let dx = this._target.x - this.x;
+        let dy = this._target.y - this.y;
+        
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        
+        this.x += this._speed * dx / distance;
+        this.y += this._speed * dy / distance;
+        
+        if (this.collide(_target)) {
+            this._state = ObjectState.dying;
+            this._ticks = ticks;
+        }
+    }
+    
+    protected _dying(ticks: number) {
+        if (ticks -  this._ticks > 3) {
+            this._state = ObjectState.dead;
+        }
     }
 }
