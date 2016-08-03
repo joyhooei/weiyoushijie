d2bf1a4f75262fcff56b93b20d946e3f7e698db5
liweihai <liweihai@huawei.com>
2016-08-03 10:36:03 +0800

    Update Base.ts

--- public/tower/src/sprites/bases/Base.ts
+++ public/tower/src/sprites/bases/Base.ts
@@ -1,9 +1,21 @@
-class Base extends Object {
+class Base extends SelectableObject {
     private _tower: Tower;
     
     public constructor() {
         super();
         
         this._tower = null;
+    }
+    
+    public setTower(tower: Tower) {
+        this._tower = tower;
+    }
+    
+    protected _select(again:boolean) {
+        if (this._tower) {
+            this._tower._select(again);
+        } else {
+            application.map.showTool(new TowerMenuUI(), this.x, this.y);
+        }
     }
 }
