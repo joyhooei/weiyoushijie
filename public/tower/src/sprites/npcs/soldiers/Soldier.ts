4f9878ddb1dbf340088dd716ecce05783da6b6b7
liweihai <liweihai@huawei.com>
2016-08-03 11:25:23 +0800

    Update Soldier.ts

--- public/tower/src/sprites/npcs/soldiers/Soldier.ts
+++ public/tower/src/sprites/npcs/soldiers/Soldier.ts
@@ -1,2 +1,21 @@
 class Soldier extends NPC {
+    private _guardX: number;
+    private _gradeY: number;
+    
+    private _enemy: Enemy;
+    
+    public constructor() {
+        super();
+    }
+    
+    protected _moving(ticks: number) {
+        if (this.x == this._guardX && this.y == this._guardY) {
+            this._changeState(ObjectState.fighting, ticks);
+        } else {
+            this._moveOneStep(this._guradX, this._guardY);
+        }
+    }
+    
+    protected _fighting(ticks: number) {
+    }
 }
