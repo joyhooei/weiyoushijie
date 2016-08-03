84fae55ad7b728b40ede330f054897317d8881d1
liweihai <liweihai@huawei.com>
2016-08-03 11:13:47 +0800

    Update Tower.ts

--- public/tower/src/sprites/towers/Tower.ts
+++ public/tower/src/sprites/towers/Tower.ts
@@ -10,10 +10,17 @@ class Tower extends Object {
     /**将圆沿y轴压扁变为椭圆时候的比例*/
     private _ratioY: number;
     
+    private _speed: number;
+    
+    private _lastFireTicks: number;
+    
     public constructor() {
         super();
         
         this._enemy = null;
+        
+        this._speed = 5;
+        this._lastFireTicks = 0;
     }
     
     protected _idle(ticks:number) {
@@ -22,21 +29,25 @@ class Tower extends Object {
     
     protected _building(ticks:number) {
         if (ticks > 100) {
-            this._changeState(ObjectState.firing, ticks);
+            this._changeState(ObjectState.fighting, ticks);
         }
     }
     
-    protected _firing(ticks:number) {
+    protected _fighting(ticks:number) {
         if (!this._enemy || this._enemy.dead() || !this._enemy.intersect(this.x, this.y, this._maxRadius)) {
             this._enemy = application.map.searchEnemy(this.x, this.y, this._maxRadius);
         }
         
         if (this._enemy) {
-            this._fire(this._enemy, ticks);
+            if (ticks - this._lastFireTicks >= this._speed) {
+                this._fire(this._enemy);
+                
+                this._lastFireTicks = ticks;
+            }
         }
     }
     
-    protected _fire(enemy: Enemy, ticks:number) {
+    protected _fire(enemy: Enemy) {
         
     }
 }
