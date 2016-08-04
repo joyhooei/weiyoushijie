9ef15538d704b3646c571e12b9efe7e81a540802
liweihai <liweihai@huawei.com>
2016-08-04 09:10:44 +0800

    Update Channel1758.ts

--- public/headline/src/channels/Channel1758.ts
+++ public/headline/src/channels/Channel1758.ts
@@ -7,16 +7,20 @@ window['onShareTimeline'] = function(){
 }
 
 class Channel1758 extends ChannelEgret {
+	private url:string;
+	
 	public constructor(standalone:boolean) {
         super(standalone);
         
-        this.loadjs("http://wx.1758.com/static/common/js/hlmy1758.js");
+        this.url = "http://wx.1758.com/static/common/js/hlmy1758.js";
 	}
     
     public share(options: any): Q.Promise<any> {
     	var self = this;
     	
-    	hlmy.setShareInfo();
+    	Utility.require(self.url).then(function(){
+    		hlmy.setShareInfo();
+    	});
     	
     	return self.promise();
     }
