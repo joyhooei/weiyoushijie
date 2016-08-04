127919d4fb001d0b884ececa6bad2d53d21b810c
liweihai <liweihai@huawei.com>
2016-08-04 08:59:54 +0800

    Update Utility.ts

--- public/headline/src/Utility.ts
+++ public/headline/src/Utility.ts
@@ -88,6 +88,44 @@ class Utility {
             } catch (error){
             }
     	}
-	}	
+	}
+	
+    public static require(file, callback) {
+    	callback = callback || function () {};
+    	
+    	var filenode;
+    	var jsfile_extension  = /(.js)$/i;
+    	var cssfile_extension = /(.css)$/i;
+
+    	if (jsfile_extension.test(file)) {
+        	filenode = document.createElement('script');
+        	filenode.src = file;
+        	
+        	// IE
+        	filenode.onreadystatechange = function () {
+            	if (filenode.readyState === 'loaded' || filenode.readyState === 'complete') {
+                	filenode.onreadystatechange = null;
+                	callback();
+            	}
+        	};
+        	
+        	// others
+        	filenode.onload = function () {
+            	callback();
+        	};
+        	
+        	document.head.appendChild(filenode);
+    	} else if (cssfile_extension.test(file)) {
+        	filenode = document.createElement('link');
+        	filenode.rel = 'stylesheet';
+        	filenode.type = 'text/css';
+        	filenode.href = file;
+        	document.head.appendChild(filenode);
+        	
+        	callback();
+    	} else {
+        	console.log("Unknown file type to load.")
+    	}
+	}
 }
         
