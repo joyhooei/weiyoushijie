1d2e5c97de18432549ce7c54390e530ad36156d8
liweihai <liweihai@huawei.com>
2016-08-05 15:19:48 +0800

    Update MovableEntity.ts

--- public/tower/src/sprites/MovableEntity.ts
+++ public/tower/src/sprites/MovableEntity.ts
@@ -5,38 +5,10 @@ class MovableEntity extends Entity {
     //一步走的距离    
     private _steps: number[];
     
-    //所有路径
-    private _paths: number[][][];
-    //当前路径
-    private _path: number;
-    
     public constructor() {
         super();
     }
-    
-    //走一步
-    private _moveOneStep(): bool {
-        var path = this._paths[this.path];
-        if (Math.abs(this.x - path[0]) < this._step && Math.abs(this.y - path[1]) < this._step) {
-            if (this._path >= this._paths.length - 1) {
-                //到达终点
-                return true;
-            }
-            
-            this._path ++;
-            
-            path = this._paths[this._path];
-            this._turn(this._direction8(path[0], path[1]));
-            
-            this._steps(path[0], path[1]);
-        }
-        
-        this.x += this._steps[0];
-        this.y += this._steps[1];
-        
-        return false;
-    }
-    
+
     //转向
     private _turn(direction: EntityDirection) {
     	if (direction != this._direction) {
@@ -72,7 +44,7 @@ class MovableEntity extends Entity {
 	        stepX = dx / (dy / stepY);
 	        if (x < this.x) {
 	            stepX = 0 - stepX;
-	        }                   
+	        }
 	    }
 	    
 	   	this._steps = [stepX, stepY];
