4f8e36b4cffcec989a956a2435c6d192e613a6b1
liweihai <liweihai@huawei.com>
2016-08-05 10:26:07 +0800

    Update Object.ts

--- public/tower/src/sprites/Object.ts
+++ public/tower/src/sprites/Object.ts
@@ -149,4 +149,35 @@ class Object extends egret.Sprite {
         	}
         }
     }
+    
+    private _steps(x:number, y:number, step:number):number[] {
+	    let stepX = 0;
+	    let stepY = 0;
+	    
+	    let dx = Math.abs(this.x - x);
+	    let dy = Math.abs(this.y - y);
+	    if (dx >= dy) {
+	        stepX = step;
+	        if (x < this.x) {
+	            stepX = 0 - stepX;
+	        }
+	        
+	        stepY = dy / (dx / stepX);
+	        if (y < this.y) {
+	            stepY = 0 - stepY;
+	        }
+	    } else {
+	         stepY = step;
+	        if (y < this.y) {
+	            stepY = 0 - stepY;
+	        }
+	        
+	        stepX = dx / (dy / stepY);
+	        if (x < this.x) {
+	            stepX = 0 - stepX;
+	        }                   
+	    }
+	    
+	   	return [stepX, stepY];
+    }
 }
