8f9a77fac372ecebdab9c68c6d8a1ce09c013227
liweihai <liweihai@huawei.com>
2016-08-05 14:57:11 +0800

    Update and rename ObjectPool.ts to EntityPool.ts

--- public/tower/src/sprites/EntityPool.ts
+++ public/tower/src/sprites/EntityPool.ts
@@ -1,11 +1,11 @@
-class ObjectPool {
-    private _objs : Object[];
+class EntityPool {
+    private _objs : Entity[];
 
     public constructor() {
-        this._objs = new Object[];
+        this._objs = new Entity[];
     }
     
-    public get(className:string): Object {
+    public get(className:string): Entity {
         for(let i = 0; i < this._objs; i++) {
             let obj = this._objs[i];
             if (className == egret.getQualifiedClassName(obj)) {
@@ -14,12 +14,12 @@ class ObjectPool {
             }
         }
         
-        let obj = Object.create(window[className].prototype);
+        let obj = Entity.create(window[className].prototype);
         obj.constructor.apply(obj);
         return obj;
     }
     
-    public set(obj:Object) {
+    public set(obj:Entity) {
         this._objs.push(obj);
     }
 }
