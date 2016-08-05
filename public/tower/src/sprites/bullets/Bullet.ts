dd8c9e90da03924479473b53d62d02ca58acc527
liweihai <liweihai@huawei.com>
2016-08-05 15:13:23 +0800

    Update Bullet.ts

--- public/tower/src/sprites/bullets/Bullet.ts
+++ public/tower/src/sprites/bullets/Bullet.ts
@@ -1,19 +1,12 @@
-class Bullet extends Entity {
+class Bullet extends MovableEntity {
     private _target: NPC;
-    
-    private _step: number;
-    
-    private _steps: number[];
-    
+
     private _missing: boolean;
     
     public constructor() {
         super();
         
         this._target = null;
-        
-        this._step = 1;
-        
         this._missing = false;
     }
     
@@ -22,7 +15,7 @@ class Bullet extends Entity {
     }
     
     protected _idle() {
-        this._do(ObjectState.moving);
+        this._do(EntityState.moving);
     }
     
     protected _moving() {
@@ -32,14 +25,14 @@ class Bullet extends Entity {
         } else {
             this._turn(this._direction8(this._target.x, this._target.y));
             
-            this._steps = this._steps(this._target.x, this._target.y, this._step);
+            this._steps(this._target.x, this._target.y);
 
             this.x += this._steps[0];
             this.y += this._steps[1];
             
             if (this.collide(this._target)) {
                 this._missing = false;
-                this._do(ObjectState.dying);
+                this._do(EntityState.dying);
     
                 this._target.hitBy(this._damage);
             }
@@ -48,7 +41,7 @@ class Bullet extends Entity {
     
     protected _dying(ticks: number) {
         if (ticks -  this._ticks > 3) {
-            this._do(ObjectState.dead);
+            this._do(EntityState.dead);
         }
     }
 }
