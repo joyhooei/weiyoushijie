2af35575aa263642574a0e35a100fae8b0a7edce
liweihai <liweihai@huawei.com>
2016-08-05 15:19:08 +0800

    Update NPC.ts

--- public/tower/src/sprites/npcs/NPC.ts
+++ public/tower/src/sprites/npcs/NPC.ts
@@ -4,6 +4,11 @@ class NPC extends MovableEntity {
     private _damage: number;
     
     private _hitSpeed: number;
+    
+    //所有路径
+    private _paths: number[][][];
+    //当前路径
+    private _path: number;
 
     public constructor() {
         super();
@@ -47,4 +52,27 @@ class NPC extends MovableEntity {
     private _hit(npc: NPC) {
         npc.hitBy(this._damage);
     }
+    
+    //走一步
+    private _moveOneStep(): bool {
+        var path = this._paths[this.path];
+        if (Math.abs(this.x - path[0]) < this._step && Math.abs(this.y - path[1]) < this._step) {
+            if (this._path >= this._paths.length - 1) {
+                //到达终点
+                return true;
+            }
+            
+            this._path ++;
+            
+            path = this._paths[this._path];
+            this._turn(this._direction8(path[0], path[1]));
+            
+            this._steps(path[0], path[1]);
+        }
+        
+        this.x += this._steps[0];
+        this.y += this._steps[1];
+        
+        return false;
+    }    
 }
