c2a5a503b9b2f81c9a9f7848dfe866b7a253f511
liweihai <liweihai@huawei.com>
2016-08-05 15:38:46 +0800

    Update Entity.ts

--- public/tower/src/sprites/Entity.ts
+++ public/tower/src/sprites/Entity.ts
@@ -25,31 +25,22 @@ class Entity extends egret.Sprite {
     
     //面向
     private _direction: EntityDirection;
+    
+    private _repaint: boolean;
 
     public constructor() {
         super();
-        
-        this._direction = EntityDirection.east;
-        this._do(EntityState.idle);
+
+		this.initialize();
 	}
 	
 	/**初始化*/
-    public initialize(options: any):void {
+    public initialize(options?: any):void {
+    	this._repaint = true;
+        this._direction = EntityDirection.east;
+        this._do(EntityState.idle);
     }
-	
-	public intersect(x: number, y: number, radius: number):boolean {
-		let dx = this.x - x;
-		let dy = this.y - y;
-		return (dx * dx + dy * dy <= radius * radius);
-	}
-	
-	public collide(obj: Entity) {
-		return !(obj.x > this.x + this.width || 
-           obj.x + obj.width < this.x || 
-           obj.y > this.y + this.height ||
-           obj.y + obj.height < this.y);		
-	}
-	
+
 	public dead(): boolean {
 		return this._state == ObjectState.dead;
 	}
@@ -95,6 +86,21 @@ class Entity extends egret.Sprite {
     	}
     }
     
+    //根据状态、面向修改重新渲染
+    public parint() {
+    	if (this._repaint) {
+	    	egret.MovieClip mc = application.characters[egret.getQualifiedClassName(this)].getMC(this._direction, this._state);
+	    	if (mc) {
+		    	this.removeChildren();
+		    	this.addChild(mc);
+		    	
+		    	mc.start();
+	    	}
+	    	
+	    	this._repaint = false;
+    	}
+    }
+    
     private _do(state:EntityState) {
     	if (state != this._state) {
 	    	this._stateChanged( this._state, state);
@@ -102,22 +108,11 @@ class Entity extends egret.Sprite {
 	    	this._ticks = 0;
 	    	this._state = state;
 	    	
-	    	this._render();
+	    	this._repaint = true;
     	}
     }
 
     protected _stateChanged(oldState: EntityState, newState: EntityState) {
-    }
-    
-    //根据状态、面向修改重新渲染
-    protected _render() {
-    	egret.MovieClip mc = application.characters[egret.getQualifiedClassName(this)].getMC(this._direction, this._state);
-    	if (mc) {
-	    	this.removeChildren();
-	    	this.addChild(mc);
-	    	
-	    	mc.start();
-    	}
     }
     
     protected _idle()) {
@@ -137,7 +132,20 @@ class Entity extends egret.Sprite {
     
     protected _dying() {
     }
-    
+	
+	public intersect(x: number, y: number, radius: number):boolean {
+		let dx = this.x - x;
+		let dy = this.y - y;
+		return (dx * dx + dy * dy <= radius * radius);
+	}
+	
+	public collide(obj: Entity) {
+		return !(obj.x > this.x + this.width || 
+           obj.x + obj.width < this.x || 
+           obj.y > this.y + this.height ||
+           obj.y + obj.height < this.y);		
+	}
+	
     private _direction8(x:number, y:number):EntityDirection {
         let angels = [22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5, 360];
         let directions = [EntityDirection.east, EntityDirection.northeast, EntityDirection.north, EntityDirection.northwest, EntityDirection.west, EntityDirection.southwest, EntityDirection.south, EntityDirection.southeast, EntityDirection.east ];
