d6a0d586afdea30aaffb5c2bc7393e0aca1c46b7
liweihai <liweihai@huawei.com>
2016-08-05 09:40:26 +0800

    Update Enemy.ts

--- public/tower/src/sprites/npcs/enemies/Enemy.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy.ts
@@ -1,2 +1,36 @@
 class Enemy extends NPC {
+    private _soliders: Solider[];
+    
+    public constructor() {
+        super();
+        
+        this._soliders = [];
+    }
+    
+    public addSolider(solider:Solider) {
+        this._soliders.push(solider);
+    }
+    
+    protected _moving(ticks: number) {
+        if (this._moveOneStep()) {
+            application.incLifes(-1);
+            
+            this._changeState(ObjectState.dead);
+        }
+    }
+    
+    protected _guarding(ticks: number) {
+    }
+    
+    protected _fighting(ticks: number) {
+        if (ticks % this._hitSpeed == 0) {
+            this._soliders[0].hitBy(this._damage);
+            if (this._soliders[0].dying()) {
+                this._soliders.splice(0, 1);
+                if (this._soliders.length == 0) {
+                    this._changeState(ObjectState.moving);
+                }
+            }
+        }
+    }
 }
