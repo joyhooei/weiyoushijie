b056903b2e89e65dfec6f8642da14eac529b36d7
liweihai <liweihai@huawei.com>
2016-08-09 14:41:33 +0800

    Update TiledMap.ts

--- public/tower/src/sprites/TiledMap.ts
+++ public/tower/src/sprites/TiledMap.ts
@@ -1,14 +1,21 @@
+//https://github.com/bgrins/javascript-astar
+declare Graph;
+declare astar;
+
 class TiledMap extends Entity {
     private _map: number[][];
-    
+
     private _spriteWidth: number;
-    
     private _spriteHeight: number;
     
-    public constructor(spriteWidth: number, spriteHeight: number) {
-        this._spriteHeight = spriteHeight;
-        this._spriteWidth  = spriteWidth;
-        
+    private _entranceTileId: number,
+    private _exitTileId: number,
+    private _pathId: number,
+    private _baseTileId: number,
+    private _baseGuardTileId: number,
+    private _heroGuardTileID: number,
+    
+    public constructor() {
         this._map = [][];
     }
     
@@ -36,6 +43,41 @@ class TiledMap extends Entity {
     }
     
     private _parseTMXTilemap(tmxTileMap:tiled.TMXTilemap) {
+        this._spriteWidth  = tmxTileMap.tilewidth();
+        this._spriteHeight = tmxTileMap.tileheight();
+        
+        let layers: Array<any> = this.getLayers();
+        
+        let pathLayer:tiled.TMXLayer = <tiled.TMXLayer>layers[1];
+        for(let i = 0; i < pathLayer.rows(); i++) {
+            for(let j = 0; j < pathLayer.cols(); j++) {
+                let tileId = pathLayer.getTileId(i, j);
+                if (tileId == this._entranceTileId) {
+                    this._map[i][j] = 5;
+                } else if (tileId == this._exitTileId) {
+                    this._map[i][j] = 6;
+                } else if (tileId == this._pathId) {
+                    this._map[i][j] = 1;
+                } else {
+                    this._map[i][j] = 0;
+                }
+            }
+        }
+        
+        let baseLayer:tiled.TMXLayer = <tiled.TMXLayer>layers[2];
+        for(let i = 0; i < baseLayer.rows(); i++) {
+            for(let j = 0; j < baseLayer.cols(); j++) {
+                let tileId = baseLayer.getTileId(i, j);
+                if (tileId == this._baseTileId) {
+                    this._map[i][j] = 2;
+                } else if (tileId == this._baseGuardTileId) {
+                    this._map[i][j] = 3;
+                } else if (tileId == this._heroGuardTileID) {
+                    this._map[i][j] = 4;
+                }
+            }
+        }
+        
         return tmxTileMap;
     }
     
@@ -90,9 +132,10 @@ class TiledMap extends Entity {
     private _getEnemyPath(entrance:number[], exit: number[]): number[][] {
         let path = number[][];
         
-        
-        
-        return path;
+        var graph = new Graph(this.map, { diagonal: true });
+        var start = graph.grid[entrance[0]][entrance[1]];
+        var end   = graph.grid[exit[0]][exit[1]];
+        return astar.search(graph, start, end, { heuristic: astar.heuristics.diagonal });
     }
     
     private _getPositionsByType(type:number) : number[][] {
