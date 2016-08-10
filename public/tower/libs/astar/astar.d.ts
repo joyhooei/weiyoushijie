bb4171849d8632421c67593423eddde400cf0726
liweihai <liweihai@huawei.com>
2016-08-10 08:49:13 +0800

    Update astar.d.ts

--- public/tower/libs/astar/astar.d.ts
+++ public/tower/libs/astar/astar.d.ts
@@ -1,60 +1,36 @@
+// Type definitions for javascript-astar
+// Project: https://github.com/bgrins/javascript-astar
+// Definitions by: brian ridley <https://github.com/ptlis/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare class Graph {
+    grid: Array<Array<GridNode>>;
+    constructor(grid: Array<Array<number>>, options?: {diagonal?: boolean});
+}
+
+declare class GridNode {
+    x: number;
+    y: number;
+}
+
+interface Heuristic {
+    (pos0: {x: number, y: number}, pos1: {x: number, y: number}): number;
+}
+
+interface Heuristics {
+    manhatten: Heuristic;
+    diagonal: Heuristic;
+}
+
 declare module astar {
-    interface Position {
-        x: number;
-        y: number;
-    }
-    enum GraphNodeType {
-        WALL,
-        OPEN,
-    }
-    class Graph {
-        public nodes: GraphNode[][];
-        public elements: number[][];
-        constructor(grid: number[][]);
-        public toString(): string;
-    }
-    class GraphNode {
-        public x: number;
-        public y: number;
-        public type: GraphNodeType;
-        public data: any;
-        public pos: Position;
-        constructor(x: number, y: number, type: GraphNodeType);
-        public toString(): string;
-        public isWall(): boolean;
-    }
-    class BinaryHeap {
-        public content: any[];
-        public scoreFunction: (node: any) => number;
-        constructor(scoreFunction: (node: any) => number);
-        public push(node: any): void;
-        public pop();
-        public remove(node: any): void;
-        public size(): number;
-        public rescoreElement(node: any): void;
-        public sinkDown(n: number): void;
-        public bubbleUp(n: number): void;
-    }
-    interface AStarData {
-        f: number;
-        g: number;
-        h: number;
-        cost: number;
-        visited: boolean;
-        closed: boolean;
-        parent: AStarData;
-        pos: Position;
-        org: any;
-    }
-    class AStar {
-        static NO_CHECK_START_POINT: boolean;
-        public grid: AStarData[][];
-        constructor(grid: any[][], disablePoints?: Position[], enableCost?: boolean);
-        public heap(): BinaryHeap;
-        public _find(org: any): AStarData;
-        public _search(start: any, end: any, diagonal?: boolean, heuristic?: Function): any[];
-        static search(grid: any[][], start: any, end: any, disablePoints?: Position[], diagonal?: boolean, heuristic?: Function): any[];
-        public manhattan(pos0: Position, pos1: Position): number;
-        public neighbors(node: AStarData, diagonals?: boolean): AStarData[];
-    }
+    function search(
+        graph: Graph,
+        start: {x: number, y: number},
+        end: {x: number, y: number},
+        options?: {
+            closest?: boolean,
+            heuristic?: Heuristic
+        }
+    ): Array<GridNode>;
+    var heuristics: Heuristics;
 }
