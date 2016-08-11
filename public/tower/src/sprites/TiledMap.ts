4c436cbf7114dbe9a97f182704c7a7ea6d18135a
liweihai <liweihai@huawei.com>
2016-08-11 10:12:56 +0800

    Update TiledMap.ts

--- public/tower/src/sprites/TiledMap.ts
+++ public/tower/src/sprites/TiledMap.ts
@@ -1,40 +1,23 @@
-enum TileType {
-    wall = 0,
-    path,
-    base,
-    solider,
-    hero,
-    entrance,
-    exit
-}
-
-class TiledMap extends Entity {
+class TiledMap extends egret.Sprite {
     private _map: tiled.TMXTilemap;
     
     private _grid: number[][];
 
     private _tileWidth: number;
     private _tileHeight: number;
+
+    private _width: number;
+    private _height: number;
     
-    private _entranceTileId: number;
-    private _exitTileId: number;
-    private _pathTileId: number;
-    private _baseTileId: number;
-    private _soliderTileId: number;
-    private _heroTileId: number;
-    
-    public constructor(map:tiled.TMXTilemap, entrance:number, exit:number, path:number, base:number, solider:number, hero:number) {
+    private _bases: number[][];
+    private _heros: number[][];
+    private _paths: number[][][];
+
+    public constructor(map:tiled.TMXTilemap) {
         super();
         
         this._map = map;
-        
-        this._entranceTileId    = entrance;
-        this._exitTileId        = exit;
-        this._pathTileId        = path;
-        this._baseTileId        = base;
-        this._soliderTileId     = solider;
-        this._heroTileId        = hero;
-        
+
         this._parse(map);
         
         this.addChild(map);
@@ -64,93 +47,140 @@ class TiledMap extends Entity {
     }
     
     private _parse(tmxTileMap:tiled.TMXTilemap) {
-        this._grid = [];
-                
         this._tileWidth  = tmxTileMap.tilewidth;
         this._tileHeight = tmxTileMap.tileheight;
         
-        let layers: Array<any> = tmxTileMap.getLayers();
+        this._width  = tmxTileMap.width;
+        this._height = tmxTileMap.height;
         
-        let pathLayer:tiled.TMXLayer = <tiled.TMXLayer>layers[1];
-        for(let i = 0; i < pathLayer.rows; i++) {
-            for(let j = 0; j < pathLayer.cols; j++) {
-                let tileId = pathLayer.getTileId(i, j);
-                if (tileId == this._entranceTileId) {
-                    this._grid[i][j] = TileType.entrance;
-                } else if (tileId == this._exitTileId) {
-                    this._grid[i][j] = TileType.exit;
-                } else if (tileId == this._pathTileId) {
-                    this._grid[i][j] = TileType.path;
-                } else {
-                    this._grid[i][j] = TileType.wall;
+        this._grid = [];
+        for(let i = 0; i <= this._width; i++) {
+            for(let j = 0; j < this._height; j++) {
+                this._grid[i][j] = 0;
+            }
+        }
+
+        let ogs = tmxTileMap.getObjects();
+        for(let i = 0; i < ogs.length; i++) {
+            let og = ogs[i];
+            let name = og.name();
+            if (name.startsWith('path')) {
+                this._paths.push(_parsePath(og));
+            } else if (name.startsWith('base')) {
+                this._bases = _parseBases(og);
+            } else if (name.startsWith('hero')) {
+                this._heros = _parseHeros(og);
+            }
+        }
+    }
+    
+    private _parsePath(og:tiled.TMXObjectGroup):number[][]{
+        let path = [];
+        
+        for(let i = 0; i < og.getObjectCount(); i++) {
+            let o = og.getObjectByIndex(i);
+            let name = o.name();
+            
+            if (name == 'start') {
+                path[0] = [o.x, o.y];
+            } else if (name = 'end') {
+                path[og.getObjectCount() - 1] = [o.x, o.y];
+            } else {
+                let arrayOfStrings = name.split("-");
+                if (arrayOfStrings.length == 2 && arrayOfStrings[0] == 'waypoint') {
+                    let idx = + arrayOfStrings[1];
+                    path[idx] = [o.x, o.y];
                 }
             }
         }
         
-        let baseLayer:tiled.TMXLayer = <tiled.TMXLayer>layers[2];
-        for(let i = 0; i < baseLayer.rows; i++) {
-            for(let j = 0; j < baseLayer.cols; j++) {
-                let tileId = baseLayer.getTileId(i, j);
-                if (tileId == this._baseTileId) {
-                    this._grid[i][j] = TileType.base;
-                } else if (tileId == this._soliderTileId) {
-                    this._grid[i][j] = TileType.solider;
-                } else if (tileId == this._heroTileId) {
-                    this._grid[i][j] = TileType.hero;
+        for(let i = 0; i < path.length - 1; i++) {
+            let xFrom = Math.round(path[i][0] / this._tileWidth);
+            let yFrom = Math.round(path[i][1] / this._tileHeight);
+            
+            let xTo = Math.round(path[i + 1][0] / this._tileWidth);
+            let yTo = Math.round(path[i + 1][1] / this._tileHeight);
+            
+            for(let k = xFrom; k <= xTo; k++) {
+                for(let j = yFrom; j <= yTo; j++) {
+                    this._markArea(k, j, 1, 1);
                 }
             }
         }
+        
+        return path;
+    }
+    
+    private _markArea(x:number, y:number, delta:number, value:number) {
+        for(let i = x - delta; i <= x + delta; i++) {
+            for(let j = y - delta; j <= y + delta; j++) {
+                if (!this._outOfBounds(i, j)) {
+                    this._grid[i][j] = value;
+                }
+            }
+        }
+    }
+    
+    private _parsBases(og:tiled.TMXObjectGroup):number[][]{
+        let bases = [];
+
+        for(let i = 0; i < og.getObjectCount(); i++) {
+            let o = og.getObjectByIndex(i);
+            let name = o.name();
+            let arrayOfStrings = name.split("-");
+            
+            if (arrayOfStrings.length == 2) {
+                let idx = + arrayOfStrings[1];
+                if (arrayOfStrings[0] == 'base') {
+                    if (!bases[idx]) {
+                        bases[idx] = [];
+                    }
+                    bases[idx][0] = o.x;
+                    bases[idx][1] = o.y;
+                } else if (arrayOfStrings[0] = 'guarder') {
+                    bases[idx][2] = o.x;
+                    bases[idx][3] = o.y;
+                }
+            }
+        }
+        
+        return bases;
+    }
+    
+    private _parseHeros(og:tiled.TMXObjectGroup):number[][]{
+        let heros = [];
+        
+        for(let i = 0; i < og.getObjectCount(); i++) {
+            let o = og.getObjectByIndex(i);
+            let name = o.name();
+            
+            if (name.startsWith('start')) {
+                heros[i] = [o.x, o.y];
+            }
+        }
+        
+        return heros;
     }
     
     public walkable(x: number, y: number): boolean {
         x = Math.round(x / this._tileWidth);
         y = Math.round(y / this._tileHeight);
-        return (x < this._grid.length && y < this._grid[x].length 
-                && (this._grid[x][y] == TileType.path || this._grid[x][y] == TileType.solider || this._grid[x][y] == TileType.hero)
-            );
+        return (!this._outofBounds(x, y) && (this._grid[x][y] == 1));
     }
     
-    public getBasePositions(): number[][] {
-        return this._getPositionsByType(TileType.base);    
+    private _outOfBounds(x: number, y: number): boolean {
+        return (x >= 0 && x <= this._width && y >= 0 && y <= this._height);
     }
     
-    public getBaseGuardPosition(): number[][] {
-        return this._getPositionsByType(TileType.solider);            
-    }
-    
-    public getHeroGuardPositions(): number[][] {
-        return this._getPositionsByType(TileType.hero);          
-    }
-    
-    public getEnemyEntrances(): number[][] {
-        return this._getPositionsByType(TileType.entrance);   
-    }
-    
-    public getEnemyExits(): number[][] {
-        return this._getPositionsByType(TileType.exit);         
-    }
-    
-    public getEnemyPaths(): number[][][] {
-        let paths = [];
-        
-        return paths;
+    public getBases(): number[][] {
+        return this._bases;
     }
 
-    public getEnemyPath(x:number, y:number): number[][] {
-        return [];
+    public getHeros(): number[][] {
+        return this._heros;          
     }
-    
-    private _getPositionsByType(type:number) : number[][] {
-        let positions = [];
-        
-        for(let i = 0; i < this._grid.length; i++) {
-            for(let j = 0; j < this._grid[i].length; j++) {
-                if (this._grid[i][j] == type) {
-                    positions.push([i * this._tileWidth, j * this._tileHeight]);
-                }
-            }
-        }
-        
-        return positions;           
+
+    public getPaths(): number[][][] {
+        return this.paths;
     }
 }
