d2d3dde1ec2fe1b7cdf63235498606b47b2306e3
liweihai <liweihai@huawei.com>
2016-08-11 20:18:21 +0800

    Update Arrow.ts

--- public/tower/src/sprites/bullets/Arrow.ts
+++ public/tower/src/sprites/bullets/Arrow.ts
@@ -1,5 +1,21 @@
-class Arrow extends Bullet {
+class Arrow extends Bomb {
+    protected _missing: boolean;
+    
     public constructor() {
         super();
     }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+
+        this._missing = false;
+    } 
+    
+    protected _hitTarget() {
+        if (this._target.dying() || this._target.dead()) {
+            this._missing = true;
+        } else {
+            this._target.hitBy(this._damage);
+        }
+    }    
 }
