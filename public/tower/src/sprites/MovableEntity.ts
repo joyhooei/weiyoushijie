ade48105ba4b2d27424409f0c0e0df827c440fc4
liweihai <liweihai@huawei.com>
2016-08-11 12:12:04 +0800

    Update MovableEntity.ts

--- public/tower/src/sprites/MovableEntity.ts
+++ public/tower/src/sprites/MovableEntity.ts
@@ -5,6 +5,12 @@ class MovableEntity extends Entity {
     //一步走的距离    
     protected _steps: number[];
     
+    //所有路径
+    protected _paths: number[][];
+    
+    //当前路径
+    protected _path: number;
+    
     protected _idleTicks: number;
     
     public constructor() {
@@ -16,8 +22,21 @@ class MovableEntity extends Entity {
         
         this._step     = this.get(properties, "step", 10);
         this._steps    = [this._step, 0];
+
+        this.setPaths(this._get(properties, "paths", []);
         
         this._idleTicks = 0;
+    }
+    
+    public setPaths(paths: number[][]) {
+   		this.x = paths[0][0];
+   		this.y = paths[0][1];
+   		
+    	this._turn(this._direction8(paths[1][0], paths[1][1]));
+    	this._computeSteps(paths[1][0], paths[1][1]);
+    	
+    	this._path = 1;
+    	this._paths = paths;
     }
     
     protected _idle() {
@@ -25,8 +44,24 @@ class MovableEntity extends Entity {
         	this._do(EntityState.moving);
     	}
     }
-
+    
+    //走一步
     protected _moveOneStep(): boolean {
+        var path = this._paths[this._path];
+        if (Math.abs(this.x - path[0]) < this._step && Math.abs(this.y - path[1]) < this._step) {
+            if (this._path >= this._paths.length - 1) {
+                //到达终点
+                return true;
+            }
+            
+            this._path ++;
+            
+            path = this._paths[this._path];
+            this._turn(this._direction8(path[0], path[1]));
+            
+            this._computeSteps(path[0], path[1]);
+        }
+        
         this.x += this._steps[0];
         this.y += this._steps[1];
         
