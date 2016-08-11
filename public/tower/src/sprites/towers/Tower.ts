dc893411228a90f2e8bce3e4595ecd6e9c609637
liweihai <liweihai@huawei.com>
2016-08-11 16:01:26 +0800

    Update Tower.ts

--- public/tower/src/sprites/towers/Tower.ts
+++ public/tower/src/sprites/towers/Tower.ts
@@ -1,25 +1,10 @@
 class Tower extends Entity {
-    private _enemy: Enemy;
-    
-    /**射程范围最大半径*/
-    private _maxRadius: number = 140;
-
-    private _fireSpeed: number;
-    
-    private _lastFireTicks: number;
-    
     public constructor() {
         super();
     }
     
-    public initialize(properties?:any) {
+    public initialize(properties:any) {
         super.initialize(properties);
-        
-        this._enemy = null;
-        this._lastFireTicks = 0;
-        
-        this._fireSpeed = properties.fireSpeed;
-        this._maxRadius = properties.maxRadius;
     }
     
     protected _idle() {
@@ -28,25 +13,7 @@ class Tower extends Entity {
     
     protected _building() {
         if (this._ticks > 100) {
-            this._do(EntityState.fighting);
+            this._do(EntityState.guarding);
         }
-    }
-    
-    protected _fighting() {
-        if (!this._enemy || this._enemy.dead() || !this._enemy.intersect(this.x, this.y, this._maxRadius)) {
-            this._enemy = application.battle.findEnemy(this.x, this.y, this._maxRadius);
-        }
-        
-        if (this._enemy) {
-            if (this._ticks - this._lastFireTicks >= this._fireSpeed) {
-                this._fire(this._enemy);
-                
-                this._lastFireTicks = this._ticks;
-            }
-        }
-    }
-    
-    protected _fire(enemy: Enemy) {
-        
     }
 }
