53281dcf94456074a7f03105d60c319d5b2a7a97
liweihai <liweihai@huawei.com>
2016-08-11 17:22:56 +0800

    Update Tower.ts

--- public/tower/src/sprites/towers/Tower.ts
+++ public/tower/src/sprites/towers/Tower.ts
@@ -1,6 +1,8 @@
 class Tower extends Entity {
     protected _hitSpeed: number;
     
+    protected _buildTicks: number;
+    
     public constructor() {
         super();
     }
@@ -8,7 +10,8 @@ class Tower extends Entity {
     public initialize(properties:any) {
         super.initialize(properties);
         
-        this._hitSpeed = this._get(properties, "hitSpeed", 10);
+        this._hitSpeed   = this._get(properties, "hitSpeed", 60);
+        this._buildTicks = this._get(properties, "buildTicks", 100);
     }
     
     protected _idle() {
@@ -16,7 +19,7 @@ class Tower extends Entity {
     }
     
     protected _building() {
-        if (this._ticks > 100) {
+        if (this._ticks > this._buildTicks) {
             this._do(EntityState.guarding);
         }
     }
