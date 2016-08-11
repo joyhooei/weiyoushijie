e8a812d1b4118c358f1a7985a0cdbe018e173d5c
liweihai <liweihai@huawei.com>
2016-08-11 11:06:03 +0800

    Update Battle1.ts

--- public/tower/src/sprites/battles/Battle1.ts
+++ public/tower/src/sprites/battles/Battle1.ts
@@ -1,18 +1,33 @@
 class Battle1 extends Battle {
     public constructor() {
         super();
-
-        this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
-        this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
-        this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
-        this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
+        
+        this._url = "level1.tmx";
     }
-    
+
     //增加英雄
     protected _addHeros() {
         let heros = this._map.getHeros();
         for(let i = 0; i < heros.length; i ++) {
             this._addHero(heros[i][0], heros[i][1], <MonkeyKing>application.pool.get("MonkeyKing"));
     	}
+    }
+    
+    protected _addStandbys() {
+        let paths = this._map.getPaths();
+        
+        let waves = [
+            [0, 0, "Enemy", 10],
+            
+            [1, 1, "Enemy", 10],
+            
+            [2, 0, "Enemy", 10],
+            [2, 1, "Enemy", 10],
+        ];
+        
+        for(let i = 0; i < waves.length; i++) {
+            let wave = waves[i];
+            this._addWaveStandbys(wave[0], paths[wave[1]], wave[2], wave[3]);
+        }
     }
 }
