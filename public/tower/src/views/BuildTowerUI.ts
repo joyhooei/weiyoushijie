540fcf7309816ff9b95f82ca77ae540925c6d00e
liweihai <liweihai@huawei.com>
2016-08-11 15:44:17 +0800

    Update BuildTowerUI.ts

--- public/tower/src/views/BuildTowerUI.ts
+++ public/tower/src/views/BuildTowerUI.ts
@@ -1,7 +1,30 @@
 class BuildTowerUI extends AbstractUI{
+    public imgArrowTower;
+    public imgBombTower;
+    public imgSoliderTower,
+    public imgMagicTower;
+    
     constructor(base:Base) {
         super("buildTowerUISkin");
         
-        
+        this.imgArrowTower.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function(ev){
+            this._addTower("ArrowTower");
+        },this);
+
+        this.imgBombTower.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function(ev){
+            this._addTower("BombTower");
+        },this);
+
+        this.imgSoliderTower.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function(ev){
+            this._addTower("SoliderTower");
+        },this);
+
+        this.imgMagicTower.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function(ev){
+            this._addTower("MagicTower");
+        },this);        
+    }
+    
+    private _addTower(name) {
+        application.map.addTower(application.pool.get(name));
     }
 }
