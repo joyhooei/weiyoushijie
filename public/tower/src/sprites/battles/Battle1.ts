5e2890041f7fe2a257781139f458e11b6b7f6ff4
liweihai <liweihai@huawei.com>
2016-08-11 10:41:10 +0800

    Update Battle1.ts

--- public/tower/src/sprites/battles/Battle1.ts
+++ public/tower/src/sprites/battles/Battle1.ts
@@ -1,12 +1,18 @@
 class Battle1 extends Battle {
     public constructor() {
         super();
-        
-        this._hero = <Hero>application.pool.get("Hero");
-        
+
         this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
         this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
         this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
         this._addWaveEnemy(0, 0, 0, "Enemy", 100, 10, 50);
+    }
+    
+    //增加英雄
+    protected _addHeros() {
+        let heros = this._map.getHeros();
+        for(let i = 0; i < heros.length; i ++) {
+            this._addHero(heros[i][0], heros[i][1], <MonkeyKing>application.pool.get("MonkeyKing"));
+    	}
     }
 }
