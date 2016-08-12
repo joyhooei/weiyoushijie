f06cd9900b8dbf8dd3a31bf8b31b970affb61fc4
liweihai <liweihai@huawei.com>
2016-08-12 09:18:27 +0800

    Update Magic.ts

--- public/tower/src/sprites/bullets/Magic.ts
+++ public/tower/src/sprites/bullets/Magic.ts
@@ -1,5 +1,28 @@
 class Magic extends Bullet {
+    private _targetX: number;
+    private _targetY: number;
+    
     public constructor() {
         super();
+    }
+    
+    public setTarget(target: NPC) {
+        super.setTarget(target);
+        
+        this._targetX = target.x;
+        this._targetY = target.y;
+        
+        this._computeSteps(target.x, target.y);
+    }
+    
+    protected _moving() {
+        if (this._moveOneStep()) {
+            this._do(EntityState.dying);
+        } else {
+            //如果目标移动，重新调整方向和路径
+            if (this._targetX != this._target.x || this._targetY != this._target.y) {
+                this.setTarget(this._target);
+            }
+        }
     }
 }
