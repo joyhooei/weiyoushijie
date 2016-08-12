a7935f02304af984d6e3a55b8817d6cac3006dc0
liweihai <liweihai@huawei.com>
2016-08-12 09:32:48 +0800

    Update Magic.ts

--- public/tower/src/sprites/bullets/Magic.ts
+++ public/tower/src/sprites/bullets/Magic.ts
@@ -2,10 +2,4 @@ class Magic extends Bullet {
     public constructor() {
         super();
     }
-    
-    public setTarget(target: NPC) {
-        super.setTarget(target);
-
-        this._computeSteps(target.x, target.y);
-    }
 }
