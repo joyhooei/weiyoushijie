98e59710c72fec2f150d7c17f4d07b340471961a
liweihai <liweihai@huawei.com>
2016-08-16 14:39:35 +0800

    Update Hp.ts

--- public/tower/src/sprites/Hp.ts
+++ public/tower/src/sprites/Hp.ts
@@ -8,12 +8,14 @@ class Hp extends Entity {
 	}
 	
 	public initialize(properties:any) {
+		super.initialize(properties);
+		
 	    this._maxHp = this._get(properties, "hp", 100);
 	    this._hp = this._maxHp;
 	}
 	
-	public kill(): number {
-		return this.hitBy(this._hp);
+	public kill() {
+		this.hitBy(this._hp);
 	}
 
 	public hitBy(damage:number): number{
@@ -21,30 +23,27 @@ class Hp extends Entity {
 	    
 	    if (hp != this._hp) {
 	        this._hp = hp;
+	        
 	        this._repaint = true;
 	    }
 	    
 	    return hp;
 	}
 	
-	protected _paint() {
-		if (this._repaint) {
-	        this.graphics.clear();
-	        
-	        let percent = this._hp / this._maxHp;
-	        if (percent >= 0.5) {
-	            this.graphics.beginFill(0x00EC00);
-	        } else if (percent >= 0.1) {
-	            this.graphics.beginFill(0xFFED97);
-	        } else {
-	            this.graphics.beginFill(0xff0000);
-	        }
-	        
-	        this.graphics.drawRect(0, 0, percent * this.width, 20);
-	        
-	        this.graphics.endFill();
-	        
-	        this._repaint = false;
-		}
+	public paint() {
+        this.graphics.clear();
+        
+        let percent = this._hp / this._maxHp;
+        if (percent >= 0.5) {
+            this.graphics.beginFill(0x00EC00);
+        } else if (percent >= 0.1) {
+            this.graphics.beginFill(0xFFED97);
+        } else {
+            this.graphics.beginFill(0xff0000);
+        }
+        
+        this.graphics.drawRect(0, 0, percent * this.width, 20);
+        
+        this.graphics.endFill();
 	}
 }
