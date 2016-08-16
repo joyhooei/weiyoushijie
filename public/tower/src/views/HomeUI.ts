a9b78d2a26d96effed25471405674cce16d77587
liweihai <liweihai@huawei.com>
2016-08-16 10:12:12 +0800

    Update HomeUI.ts

--- public/tower/src/views/HomeUI.ts
+++ public/tower/src/views/HomeUI.ts
@@ -3,12 +3,11 @@ class HomeUI extends AbstractUI{
 
     constructor() {
         super("homeUISkin");
-
     }
 
     protected onRefresh():void {
         this.imgBattle1.addEventListener( egret.TouchEvent.TOUCH_TAP, ()=>{
             application.showUI(new BattleLoadingUI(1, 1));
-        }, this ); 
+        }, this); 
     }
 }
