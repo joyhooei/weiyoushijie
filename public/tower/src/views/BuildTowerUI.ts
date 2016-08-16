07909d116f343c5f5853ffb97f64fbcab193fb86
liweihai <liweihai@huawei.com>
2016-08-16 11:07:27 +0800

    Update BuildTowerUI.ts

--- public/tower/src/views/BuildTowerUI.ts
+++ public/tower/src/views/BuildTowerUI.ts
@@ -1,13 +1,17 @@
 class BuildTowerUI extends AbstractUI{
-    public imgArrowTower;
-    public imgBombTower;
-    public imgSoliderTower;
-    public imgMagicTower;
+    public imgArrowTower: eui.Image;
+    public imgBombTower: eui.Image;
+    public imgSoliderTower: eui.Image;
+    public imgMagicTower: eui.Image;
+    
+    private _imgTowers: eui.Image[]: 
     
     private _base: Base;
     
     constructor(base:Base) {
         super("buildTowerUISkin");
+        
+        this._imgTowers = [this.imgArrowTower, this.imgBombTower, this.imgSoliderTower, this.imgMagicTower];
         
         this.imgArrowTower.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function(ev){
             this._addTower("ArrowTower");
@@ -23,13 +27,32 @@ class BuildTowerUI extends AbstractUI{
 
         this.imgMagicTower.addEventListener(egret.TouchEvent.TOUCH_BEGIN, function(ev){
             this._addTower("MagicTower");
-        },this);        
+        },this);  
+        
+        application.dao.addEventListener("Battle",function(evt: egret.Event) {
+            this.refresh();
+        },this);
+    }
+    
+    protected _onRefresh() {
+        let prices = [100, 200, 300, 400];
+        let golds = application.battle.getGolds();
+        for (let i = 0; i < this._imgTowers.length; i++) {
+            if (golds >= prices[i]) {
+                this._imgTowers.source = "";
+            } else {
+                this._imgTowers.source = "";
+            }
+        }
     }
     
     private _addTower(name:string, options?:any) {
-        let tower = <Tower>application.pool.get(name, options);
-        tower.x = this._base.x;
-        tower.y = this._base.y;
-        this._base.buildTower(tower);
+        let golds = application.battle.getGolds();
+        if (golds >= tower.getPrice()) {
+            let tower = <Tower>application.pool.get(name, options);
+            tower.x = this._base.x;
+            tower.y = this._base.y;
+            this._base.buildTower(tower);
+        }
     }
 }
