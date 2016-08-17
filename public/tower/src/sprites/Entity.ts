e963f0520ca19245d109bb4c053fe08b192fa746
liweihai <liweihai@huawei.com>
2016-08-17 09:46:11 +0800

    Update Entity.ts

--- public/tower/src/sprites/Entity.ts
+++ public/tower/src/sprites/Entity.ts
@@ -35,12 +35,19 @@ class Entity extends egret.Sprite {
         super();
 	}
 	
-	/**初始化*/
     public initialize(properties: any):void {
         this._direction = this._get(properties, "direction", EntityDirection.east);
         this._state 	= this._get(properties, "state", EntityState.idle);
         this._ticks 	= 0;
         this._repaint   = true;
+    }
+    
+    protected _get(properties: any, name:string, defaultVal:any): any {
+    	if (properties && properties[name]) {
+    		return properties[name];
+    	} else {
+    		return defaultVal;
+    	}
     }
 	
 	public getClassName() {
@@ -64,18 +71,6 @@ class Entity extends egret.Sprite {
     		return (<Entity>this.parent).getMapY() + this.y;
     	} else {
     		return this.y;
-    	}
-    }
-    
-    public setMCs(mcs: egret.MovieClip[]) {
-    	this._mcs = mcs;
-    }
-    
-    protected _get(properties: any, name:string, defaultVal:any): any {
-    	if (properties && properties[name]) {
-    		return properties[name];
-    	} else {
-    		return defaultVal;
     	}
     }
 	
@@ -103,12 +98,8 @@ class Entity extends egret.Sprite {
     	this._do(EntityState.dead);
     }
 
-	public dead(): boolean {
-		return this._state == EntityState.dead;
-	}
-	
-	public dying(): boolean {
-		return this._state == EntityState.dying;
+	public inactive(): boolean {
+		return this._state == EntityState.dead || this._state == EntityState.dying;
 	}
 
     public select(again:boolean) {
@@ -147,11 +138,15 @@ class Entity extends egret.Sprite {
 		    	break;
     	}
 
-    	if (this._repaint && this._state != EntityState.idle && this._state != EntityState.dead) {
+    	if (this._repaint && !this.inactive()) {
         	this.paint();
         	
         	this._repaint = false;
     	}
+    }
+    
+    public setMCs(mcs: egret.MovieClip[]) {
+    	this._mcs = mcs;
     }
     
     //根据状态、面向修改重新渲染
@@ -201,9 +196,6 @@ class Entity extends egret.Sprite {
     }
     
     protected _stateChanged(oldState: EntityState, newState: EntityState) {
-    	if (this._parent && newState == EntityState.dead) {
-    		this._parent.childDead(this);
-    	}
     }
 
     protected _idle() {
@@ -231,10 +223,8 @@ class Entity extends egret.Sprite {
 	}
 	
 	public collide(obj: Entity) {
-		return !(obj.x > this.x + this.width || 
-           obj.x + obj.width < this.x || 
-           obj.y > this.y + this.height ||
-           obj.y + obj.height < this.y);		
+		return !(obj.x > this.x + this.width ||  obj.x + obj.width < this.x || 
+           obj.y > this.y + this.height || obj.y + obj.height < this.y);		
 	}
 	
     protected _direction8(x:number, y:number):EntityDirection {
