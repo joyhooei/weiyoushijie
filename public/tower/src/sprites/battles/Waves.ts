4d3ee14c82f0a4d17955a42648dce2a4b8b0adb0
liweihai <liweihai@huawei.com>
2016-08-17 17:09:19 +0800

    Update Waves.ts

--- public/tower/src/sprites/battles/Waves.ts
+++ public/tower/src/sprites/battles/Waves.ts
@@ -1,18 +1,19 @@
 class Waves {
-    protected _enemies: Enemy[][];
+    private _enemies: Enemy[][];
     
     //当前一波敌人
-    protected _currentWave: number;
+    private _currentWave: number;
     
     //下一波敌人发动攻击时间
-    protected _timeToNextWave: number;
+    private _timeToNextWave: number;
     
     //两波敌人发动攻击的时间间隔
-    protected _timeBetweenWaves: number; 
+    private _timeBetweenWaves: number; 
     
     public constructor() {
         this._enemies = [];
         this._currentWave = 0;
+        
         this._timeToNextWave = 1000;
         this._timeBetweenWaves = 1000;
     }
@@ -21,8 +22,57 @@ class Waves {
         this._enemies[wave] = this._enemies[wave] || [];
 
         for(let i = 0; i < count; i++) {
-            this._enemies[wave].push(<Enemy>application.pool.get(className, {"paths": paths}));
+            this._enemies[wave].push(<Enemy>application.pool.get(className, {"paths": this._randomPaths(i, paths)}));
         }
+    }
+    
+    private _randomPaths(idx: number, paths: number[][]): number[][]{
+        let pathWidth = 60;
+        let enemyInterval = 20;
+        
+        let deltaY = Math.random() * pathWidth - pathWidth / 2;
+        let deltaX = Math.random() * pathWidth - pathWidth / 2;
+        
+        let newPaths = [];
+
+        let direction = Entity.direction4(paths[0][0], paths[0][1], paths[1][0], paths[1][1]);
+        switch(direction) {
+            case EntityDirection.east:
+                newPaths.push([paths[0][0] - idx * Math.random() * enemyInterval, paths[0][1] + deltaY]);
+                break;
+                
+            case EntityDirection.west:
+                newPaths.push([paths[0][0] + idx * Math.random() * enemyInterval, paths[0][1] + deltaY]);
+                break;
+                
+            case EntityDirection.north:
+                newPaths.push([paths[0][0] + deltaX, paths[0][1] + idx * Math.random() * enemyInterval]);
+                break;
+                
+            case EntityDirection.south:
+                newPaths.push([paths[0][0] + deltaX, paths[0][1] - idx * Math.random() * enemyInterval]);
+                break;
+        }
+
+        for(let j = 1; j < paths.length - 1; j++) {
+            newPaths.push([paths[j][0] + deltaX, paths[j][1] + deltaY]);
+        }
+        
+        direction = Entity.direction4(paths[paths.length - 2][0], paths[paths.length - 2][1], paths[paths.length - 1][0], paths[paths.length - 1][1]);
+        switch(direction) {
+            case EntityDirection.east:
+            case EntityDirection.west:
+                deltaX = 0；
+                break;
+                
+            case EntityDirection.north:
+            case EntityDirection.south:
+                deltaY = 0；
+                break;
+        }
+        newPaths.push([paths[paths.length - 1][0] + deltaX, paths[paths.length - 1][1] + deltaY]);        
+        
+        return newPaths;
     }
     
     public erase() {
