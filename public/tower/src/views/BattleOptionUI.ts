82047aefd0d8a7359c248f322dbcf589103f90cc
liweihai <liweihai@huawei.com>
2016-08-17 15:07:52 +0800

    Update BattleOptionUI.ts

--- public/tower/src/views/BattleOptionUI.ts
+++ public/tower/src/views/BattleOptionUI.ts
@@ -2,8 +2,16 @@ class BattleOptionUI extends AbstractUI {
     public imgStart:  eui.Image;
     public imgQuit: eui.Image;
     
+    public imgResult: eui.Image;
+    
     constructor(cbStart:Function, cbQuit:Function) {
         super("battleOptionUISkin");
+        
+        if (application.battle.getLives() > 0) {
+        	this.imgResult.source = "win_png";
+        } else {
+        	this.imgResult.source = "lose_png";
+        }
         
 		this.imgStart.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
 		    cbStart();
