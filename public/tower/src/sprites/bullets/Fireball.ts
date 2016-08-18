ae583a8da72a4319882ef59a2377d6685811054f
liweihai <liweihai@huawei.com>
2016-08-18 17:41:52 +0800

    Create Fireball.ts

---
+++ public/tower/src/sprites/bullets/Fireball.ts
@@ -0,0 +1,24 @@
+class Fireball extends Bullet {
+    protected _hitRadius: number;
+    
+    public constructor() {
+        super();
+    }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+
+        this._hitRadius = this._get(properties, 'hitRadius', 20);
+    }
+    
+    protected _hitTarget() {
+        let enemies = application.battle.findEnemies(this.x, this.y, this._hitRadius, [0]);
+        for (let i = 0; i < enemies.length; i++) {
+            if (enemies[1] != this._target) {
+                enemies[i].hitBy(this._damage);
+            }
+        }
+        
+        this._target.hitBy(this._damage);
+    }
+}
