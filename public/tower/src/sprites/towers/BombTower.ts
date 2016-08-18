8c0901fd20c9735121c47ceda97644e9eb23fc5d
liweihai <liweihai@huawei.com>
2016-08-18 11:55:45 +0800

    Update BombTower.ts

--- public/tower/src/sprites/towers/BombTower.ts
+++ public/tower/src/sprites/towers/BombTower.ts
@@ -2,8 +2,4 @@ class BombTower extends RemoteHitTower {
     public constructor() {
         super();
     }
-
-    protected _createBullet():Bullet {
-        return <Bullet>application.pool.get("Bomb");
-    }
 }
