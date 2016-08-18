a48367a7936fdc339748fc503fb8e1e08493925d
liweihai <liweihai@huawei.com>
2016-08-18 17:29:55 +0800

    Update CastBullet.ts

--- public/tower/src/sprites/bullets/CastBullet.ts
+++ public/tower/src/sprites/bullets/CastBullet.ts
@@ -3,9 +3,6 @@ class CastBullet extends Bullet {
     protected _initX: number;
     protected _initY: number;
 
-    /**飞行角度*/
-    protected _angle: number;
-    
     protected _flyHeight: number;
     
     protected _gravity: number;
@@ -21,8 +18,6 @@ class CastBullet extends Bullet {
 
         this._flyHeight = this._get(properties, 'flyHeight', 25);
         this._gravity   = this._get(properties, 'gravity', 1);
-        
-        this._angle = 0;
 
         this._flyTicks = 0;
     }
@@ -41,17 +36,6 @@ class CastBullet extends Bullet {
 	    this._delta = [stepX, stepY];
     }
     
-    protected _moving() {
-        if (this._moveOneStep()) {
-            this.kill();
-        } else {
-            //如果目标移动，重新调整方向和路径
-            if (this._targetX != this._target.x || this._targetY != this._target.y) {
-                this.setTarget(this._target);
-            }
-        }
-    }
-    
     protected _moveOneStep():boolean {
         this._flyTicks += 0.5;
         
@@ -64,28 +48,19 @@ class CastBullet extends Bullet {
         var sy: number = this._initY + this._flyTicks * this._delta[1] + this._gravity * this._flyTicks * this._flyTicks / 2;
         var dx: number = sx - this.x;
         var dy: number = sy - this.y;
-        this._angle = Math.atan2(dy, dx) * 180 / Math.PI + 180;
         
         this._flyTicks -= 0.5;
-
-        return this._hitTest();
-    }
-    
-    protected _hitTest(): boolean {
-        if(this._angle > 180 && this._angle < 360) {
+        
+        let angle = Math.atan2(dy, dx) * 180 / Math.PI + 180;
+        if(angle > 180 && angle < 360) {
             var disx: number = this.x - this._target.x < 0 ? this._target.x - this.x : this.x - this._target.x;
             var disy: number = this.y - this._target.y < 0 ? this._target.y - this.y : this.y - this._target.y;
     
             if(disx <= this._delta[0] && disy < this._delta[1]) {
-                this._hitTarget();
-                
                 return true;
-            } 
+            }
         }
         
         return false;
-    }
-    
-    protected _hitTarget() {
     }
 }
