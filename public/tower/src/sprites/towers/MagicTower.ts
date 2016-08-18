cb9ee67f0437eeee2ee4a0270f12f79818f13e3f
liweihai <liweihai@huawei.com>
2016-08-18 11:56:03 +0800

    Update MagicTower.ts

--- public/tower/src/sprites/towers/MagicTower.ts
+++ public/tower/src/sprites/towers/MagicTower.ts
@@ -2,8 +2,4 @@ class MagicTower extends RemoteHitTower {
     public constructor() {
         super();
     }
-    
-    protected _createBullet():Bullet {
-        return <Bullet>application.pool.get("Magic");
-    }
 }
