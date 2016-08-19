8da52c56d393423d3cecc996b8f71b0d4242c94d
liweihai <liweihai@huawei.com>
2016-08-19 12:29:04 +0800

    Update NotMoveableTip.ts

--- public/tower/src/sprites/NotMoveableTip.ts
+++ public/tower/src/sprites/NotMoveableTip.ts
@@ -1,12 +1,20 @@
 class NotMoveableTip extends Entity {
+    private _dyingTicks: number;
+    
     public constructor() {
         super();
         
         this.kill();
     }
     
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._dyingTicks = this._get(properties, "dyingTicks", 1000);
+    }
+    
     protected _dying() {
-        if (this._ticks > 20) {
+        if (this._ticks > _dyingTicks) {
             this.erase();
         }
     }
