a651897cf09c9cc0d9619d38e81b702f91a1b83f
liweihai <liweihai@huawei.com>
2016-08-19 10:31:24 +0800

    Update Character.ts

--- public/tower/src/models/Character.ts
+++ public/tower/src/models/Character.ts
@@ -1,53 +1,27 @@
 class Character {
-    private _properties: any;
-    
-    private _factory: egret.MovieClipDataFactory;
-    
-    private _mcs: string[];
+    private _properties: any[];
 
-    constructor(properties: any, mcs:string[], factory: egret.MovieClipDataFactory) {
+    constructor(properties: any) {
         this._properties = properties;
-        
-        this._mcs = mcs;
-        
-        this._factory = factory;
     }
 
     public getProperties(): any {
         return this._properties;
     }
 
-    public getMCs(): egret.MovieClip[] {
-        let mcs = [];
-
-        for(let j = 0; j < this._mcs.length; j++) {
-            let mc = new egret.MovieClip(this._factory.generateMovieClipData(this._mcs[j]));
-            
-            mc.frameRate = application.frameRate;
-            
-            mcs.push(mc);
-        }
-        
-        return mcs;
-    }
-    
     static createAll(): Character[] {
         let characters = new Array<Character>();
         
         let config = [
-            //name, properties [ mc_name]
+            //name, properties
             /*
-            {name: 'MonkeyKing', properties:{hp:1000}, mcs:['test', 'test1']},
+            {name: 'MonkeyKing', properties:{hp:1000}},
             */
         ];   
-        
-        let data = RES.getRes("animation.json");
-        let txtr = RES.getRes("animation.png");
-        let factory:egret.MovieClipDataFactory = new egret.MovieClipDataFactory( data, txtr );
-        
+
         for(let i = 0; i < config.length; i++) {
             let d = config[i];
-            characters[d.name] = new Character(d.properties, d.mcs, factory);
+            characters[d.name] = new Character(d.properties);
         }
         
         return characters;
