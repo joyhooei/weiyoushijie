98b1db5291971195c3f371cbee310aae965ec659
liweihai <liweihai@huawei.com>
2016-08-23 15:01:30 +0800

    Update SoliderTower.ts

--- public/tower/src/sprites/towers/SoliderTower.ts
+++ public/tower/src/sprites/towers/SoliderTower.ts
@@ -12,11 +12,20 @@ class SoliderTower extends Tower {
         this._guardX = this._get(properties, "guardX", 10);
         this._guardY = this._get(properties, "guardY", 10);
     }
-
+    
+    public create(soldier: Soldier) {
+        let s = soldier.relive(100);
+        s.x = this.getCenterX();
+        s.y = this.getCenterY();
+        application.battle.addSoldier(s);
+    }
+    
     public guard() {
         for(let i = 0; i < 3; i++) {
             let soldier = <Soldier>application.pool.get(claz, {"guardX": this._guardX, "guardY": this._guardY});
             soldier.setCreator(this);
+            soldier.x = this.getCenterX();
+            soldier.y = this.getCenterY();            
             application.battle.addSoldier(soldier);
         }
         
