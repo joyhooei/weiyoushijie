a0557404ae40d213fccc07a32674d0c659e12871
liweihai <liweihai@huawei.com>
2016-08-23 14:38:38 +0800

    Update Hero.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
@@ -10,13 +10,4 @@ class Hero extends Soldier {
     
     public deselect() {
     }
-    
-    public erase() {
-        super.erase();
-        
-        let hero = <Hero>application.pool.get(this.getClassName(), {guardX: this._guardX, guardY: this._guardY, idleTicks: 30});
-        hero.x = this._guardX;
-        hero.y = this._guardY;
-        application.battle.addHero(hero);
-    }
 }
