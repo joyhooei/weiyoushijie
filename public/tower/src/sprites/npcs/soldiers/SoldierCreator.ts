05b3c9567efa3ee3ab3589e2e2d16c9dcd2133b7
liweihai <liweihai@huawei.com>
2016-08-23 15:06:06 +0800

    Update SoldierCreator.ts

--- public/tower/src/sprites/npcs/soldiers/SoldierCreator.ts
+++ public/tower/src/sprites/npcs/soldiers/SoldierCreator.ts
@@ -1,7 +1,15 @@
 class SoldierCreator enxtends Entity {
+    private _createSpeed: number;
+    
     public constructor() {
         super();
     }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._createSpeed = this._get(properties, "createSpeed", 100);
+    }    
     
     public create(soldier: Soldier):Soldier {
         return null;
