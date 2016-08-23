e2e0a7a3adc16bc12481eeac22eb3abd4b9b9695
liweihai <liweihai@huawei.com>
2016-08-23 16:22:23 +0800

    Update Hero.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
@@ -1,13 +1,5 @@
 class Hero extends Soldier {
     public constructor() {
         super();
-        
-        application.battle.enableSelect(this);
-    }
-    
-    public select(again:boolean) {
-    }
-    
-    public deselect() {
     }
 }
