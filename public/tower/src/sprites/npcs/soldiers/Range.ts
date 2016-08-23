dd107fc0d45a0fc3f41dad02c65155642175c682
liweihai <liweihai@huawei.com>
2016-08-23 15:33:26 +0800

    Create Range.ts

---
+++ public/tower/src/sprites/npcs/soldiers/Range.ts
@@ -0,0 +1,22 @@
+class Range extends Entity {
+    private _guardRadius: number;
+    
+	public constructor() {
+        super();
+	}
+	
+    public initialize(properties:any) {
+		super.initialize(properties);
+		
+	    this._guardRadius = this._get(properties, "guardRadius", 100);
+	}
+	
+	public paint() {
+        this.graphics.clear();
+        
+        this.graphics.lineStyle(1, 0x009900);
+        this.graphics.drawEllipse(0, 0, this._guardRadius, this._guardRadius >> 1);
+
+        this.graphics.endFill();
+	}	
+}
