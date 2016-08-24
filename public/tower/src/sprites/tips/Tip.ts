324337fe0a8c97ba8a2193893a55c7d017555e84
liweihai <liweihai@huawei.com>
2016-08-24 17:30:39 +0800

    Update Tip.ts

---
+++ public/tower/src/sprites/tips/Tip.ts
@@ -0,0 +1,21 @@
+class Tip extends Entity {
+    private _dyingTicks: number;
+    
+    public constructor() {
+        super();
+    }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._dyingTicks = this._get(properties, "dyingTicks", 1000);
+        
+        this.kill();
+    }
+    
+    protected _dying() {
+        if (this._ticks > this._dyingTicks) {
+            this.erase();
+        }
+    }
+}
