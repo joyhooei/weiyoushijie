653e9784161659c290a2987e057e177037b3675c
liweihai <liweihai@huawei.com>
2016-08-25 11:10:54 +0800

    Update ArrowTower31.ts

--- public/tower/src/sprites/towers/ArrowTower31.ts
+++ public/tower/src/sprites/towers/ArrowTower31.ts
@@ -2,6 +2,6 @@ class ArrowTower31 extends ArrowTower {
     public constructor() {
         super();
         
-        this._displays.addBitmap("arrowtower31").addClip("building");
+        this._displays.addBitmap("arrowtower31");
     }
 }
