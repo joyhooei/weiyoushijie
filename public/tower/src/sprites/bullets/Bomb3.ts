936dc62c396dc756f031a9c384264bc9551a9a6e
liweihai <liweihai@huawei.com>
2016-08-25 10:52:09 +0800

    Update and rename Bomb3.ts to Bomb31.ts

--- public/tower/src/sprites/bullets/Bomb3.ts
+++
@@ -1,7 +0,0 @@
-class Bomb3 extends Bomb {
-    public constructor() {
-        super();
-        
-        this._displays.addClip("bomb3");
-    }
-}
---
+++ public/tower/src/sprites/bullets/Bomb31.ts
@@ -0,0 +1,7 @@
+class Bomb31 extends Bomb {
+    public constructor() {
+        super();
+        
+        this._displays.addClip("bomb31");
+    }
+}
