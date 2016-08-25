b0ca1362bfb98840a9a515fe649844cdccc52d88
liweihai <liweihai@huawei.com>
2016-08-25 12:17:22 +0800

    Update EntitySounds.ts

--- public/tower/src/sprites/EntitySounds.ts
+++ public/tower/src/sprites/EntitySounds.ts
@@ -14,7 +14,7 @@ class EntitySounds {
         if (name) {
             var sound_eff: egret.Sound = RES.getRes(name);
             sound_eff.type = egret.Sound.EFFECT;
-            sound_eff.volume = value;
+            sound_eff.volume = 1;
             sound_eff.play();
         }
     }
