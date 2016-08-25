0b75449ece9ca968279980fd4e434b64e1cc01b6
liweihai <liweihai@huawei.com>
2016-08-25 11:54:57 +0800

    Create EntitySounds.ts

---
+++ public/tower/src/sprites/EntitySounds.ts
@@ -0,0 +1,21 @@
+class EntitySounds {
+    private _sounds: string[];
+
+    public constructor() {
+        this._sounds = [];
+    }
+    
+    public add(sound:string, state:number) {
+        this._sounds[state] = sound;
+    }
+    
+    public play(state:number) {
+        let name = this._sounds[state];
+        if (name) {
+            var sound_eff: egret.Sound = RES.getRes(name);
+            sound_eff.type = egret.Sound.EFFECT;
+            sound_eff.volume = value;
+            sound_eff.play();
+        }
+    }
+}
