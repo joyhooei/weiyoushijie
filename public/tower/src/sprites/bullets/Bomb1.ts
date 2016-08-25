5c1eb2d7a64bea9e4cc335c08951448dccd8c865
liweihai <liweihai@huawei.com>
2016-08-25 10:06:27 +0800

    Update Bomb1.ts

--- public/tower/src/sprites/bullets/Bomb1.ts
+++ public/tower/src/sprites/bullets/Bomb1.ts
@@ -2,7 +2,6 @@ class Bomb1 extends Bomb {
     public constructor() {
         super();
         
-        this._displays.addClip("bomb1_json", "bomb1_png", "moving")
-                      .addClip("bomb1_json", "bomb1_png", "dying", {state: EntityState.dying});
+        this._displays.addClip("bomb1");
     }
 }
