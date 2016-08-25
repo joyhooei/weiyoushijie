d3320cb648fa34d07cb3a99d61dab70348e40a23
liweihai <liweihai@huawei.com>
2016-08-25 10:06:53 +0800

    Update EntityDisplays.ts

--- public/tower/src/sprites/EntityDisplays.ts
+++ public/tower/src/sprites/EntityDisplays.ts
@@ -1,107 +1,114 @@
 class EntityDisplays {
     private _displays: egret.DisplayObject[];
-    private _keys: any[];
-    
+
     private _currentDisplay: egret.DisplayObject;
     
     private _defaultDisplay: egret.DisplayObject;
     
     public constructor() {
-        this._displays    = [];
-        this._keys = [];
+        this._displays = [];
         
         this._currentDisplay = null;
         this._defaultDisplay = null;
     }
     
-    private _add(display:egret.DisplayObject, options?:any) {
-        if (options) {
-            this._displays.push(display);
-            this._keys.push(options);
+    private _add(display:egret.DisplayObject, action?:string) {
+        if (action) {
+            this._displays[action] = display;
         } else {
             this._defaultDisplay = display;
         }
     }
     
-    public addBitmap(bitmapRes:string, options?:any): EntityDisplays {
+    public addBitmap(name:string, action?:string): EntityDisplays {
         let bm:egret.Bitmap = new egret.Bitmap();
         bm.texture = RES.getRes(name);
         
-        this._add(bm, options);
+        this._add(bm, action);
         
         return this;
     }
     
-    public addClip(dataRes:string, textureRes:string, clipName:string, options?:any): EntityDisplays {
-        let data    = RES.getRes(dataRes);
-        let texture = RES.getRes(textureRes);
+    public addClip(name:string, action?:string): EntityDisplays {
+        let data    = RES.getRes(name + "_json");
+        let texture = RES.getRes(name + "_png");
         let mcf: egret.MovieClipDataFactory = new egret.MovieClipDataFactory(data, texture);
         
         let clip = new egret.MovieClip();
-        clip.movieClipData = mcf.generateMovieClipData(clipName);
+        let mcd: egret.MovieClipData = mcf.generateMovieClipData(name);
+        clip.movieClipData = mcd;
         
-        this._add(clip, options);
+        if (action) {
+            this._add(clip, action);
+        } else {
+            for(let i = 0; i < mcd.labels.length; i++) {
+                let label = mcd.labels[i];
+                this._add(clip, label);
+            }
+        }
         
         return this;
     }
 
-    public render(container: egret.DisplayObjectContainer, options:any): boolean {
-        let display = this._getDisplay(options);
-        if (display && display != this._currentDisplay) {
-            if (this._currentDisplay) {
-                container.removeChild(this._currentDisplay);
+    public render(container: egret.DisplayObjectContainer, action:string): boolean {
+        let display = this._getDisplay(action);
+        if (display) {
+            if (display != this._currentDisplay) {
+                if (this._currentDisplay) {
+                    container.removeChild(this._currentDisplay);
+                }
+                
+                if (display) {
+                    container.addChild(display);
+                }
+            
+                this._currentDisplay = display;
             }
-            
-            container.addChild(display);
-            
-            if (egret.getQualifiedClassName(display) == "MovieClip") {
-                (<egret.MovieClip>display).play();
-            }
-            
-            this._currentDisplay = display;
-            
+
             return true;
         } else {
             return false;
         }
     }
     
-    private _getDisplay(options:any): egret.DisplayObject {
+    /*
+    east_moving
+    moving
+    */
+    private _getDisplay(action:string): egret.DisplayObject {
         let display  = null;
-        let suitable = 0;
         
-        for(let i = 0; i < this._keys.length; i++) {
-            let s = this._suitable(options, this._keys[i]);
-            if (s > suitable) {
-                display  = this._displays[i];
-                suitable = s;
-            }
-        }
-        
-        if (display) {
-            return display;
-        } else {
-            return this._defaultDisplay;
-        }
-    }
-    
-    private _suitable(options:any, keys:any): number {
-        let suitable:number = 0;
-        
-        for (var key in options) {
-            if (keys.hasOwnProperty(key)) {
-                if (keys[key] == options[key]) {
-                    if (key == "direction") {
-                        suitable += 2;
-                    } else {
-                        suitable += 1;
-                    }
-                } else {
-                    return 0;
+        for (let key in this._displays) {
+            if (key == action) {
+                display = this._displays[key];
+
+                if (egret.getQualifiedClassName(display) == "MovieClip") {
+                    (<egret.MovieClip>display).gotoAndPlay(action);
                 }
+                
+                break;
+            }
+        }
+    
+        if (display == null) {
+            let actions = action.split("_");
+            if (actions.length <= 1) {
+                if (this._defaultDisplay) {
+                    display = this._defaultDisplay;
+                } else if (this._displays.length > 0) {
+                    display = this._displays[0];
+                }
+                
+                if (egret.getQualifiedClassName(display) == "MovieClip") {
+                    (<egret.MovieClip>display).play();
+                }                
+            } else {
+                actions.splice(0, 1);
+                let action = actions.join("_");
+                display = this._getDisplay(action);
             }
         }
         
-        return suitable;
+        return display;    
     }
 }
