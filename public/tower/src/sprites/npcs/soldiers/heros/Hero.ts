df1a2826bc980126104c97db6db30837890499ef
liweihai <liweihai@huawei.com>
2016-08-29 16:31:18 +0800

    Update Hero.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
@@ -1,9 +1,20 @@
 class Hero extends Soldier implements {
-    protected _warriors: Soldier[];
-    
     public constructor() {
         super();
         
         this._warriors = [];
+    }
+    
+    public addWarrior(warrior: Soldier) {
+        warrior.setCreator(this);
+    }
+    
+    public createSoldier(soldier: Soldier): Soldier {
+        let s = soldier.relive(ticks);
+        s.x = this.getCenterX();
+        s.y = this.getCenterY();
+        application.battle.addSoldier(s);
+
+        return s;        
     }
 }
