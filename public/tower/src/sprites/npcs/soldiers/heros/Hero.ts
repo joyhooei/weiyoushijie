f45fd3e4d6d2cc4645909f45d05526e2ae70dd24
liweihai <liweihai@huawei.com>
2016-08-29 16:13:44 +0800

    Update Hero.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
@@ -1,5 +1,9 @@
-class Hero extends Soldier {
+class Hero extends Soldier implements {
+    protected _warriors: Soldier[];
+    
     public constructor() {
         super();
+        
+        this._warriors = [];
     }
 }
