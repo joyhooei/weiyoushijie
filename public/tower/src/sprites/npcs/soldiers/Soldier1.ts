d14dcd5719e42a2220ed1b53992ed278036af74c
liweihai <liweihai@huawei.com>
2016-08-31 11:58:36 +0800

    Update Soldier1.ts

--- public/tower/src/sprites/npcs/soldiers/Soldier1.ts
+++ public/tower/src/sprites/npcs/soldiers/Soldier1.ts
@@ -6,11 +6,6 @@ class Soldier1 extends Soldier {
     }
 
     public paint() {
-    	let display: egret.DisplayObject = this._displays.render(this, this._direction, this._state);
-        if (display && this._hp) {
-        	display.y = -10;
-            display.x = -26;
-            display.height = this.height - 3;
-        }
+        this._paintDisplay(-10, -26);
     }
 }
