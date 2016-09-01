099929cfe159367b1a080bc3e1e1f9959187adde
liweihai <liweihai@huawei.com>
2016-09-01 20:21:18 +0800

    Update Application.ts

--- public/headline/src/Application.ts
+++ public/headline/src/Application.ts
@@ -20,7 +20,7 @@ module application {
     export var ticks: number = 0;
     export var stopwatch: egret.EventDispatcher;
     
-    export var version: string = '2.6.2';
+    export var version: string = '2.6.3';
     
     export var game: string = 'headline';
     
