25423a931573d7bf5ab655f0635a907eed2fa399
liweihai <liweihai@huawei.com>
2016-09-08 16:39:59 +0800

    Create Reinforce.ts

---
+++ public/tower/src/sprites/npcs/soldiers/Reinforce.ts
@@ -0,0 +1,14 @@
+class Reinforce extends Soldier {
+    public constructor() {
+        super();
+        
+        this._displays.addClip("reinforce_east_fighting", "east-fighting")
+                .addClip("reinforce_east_moving", "east-moving")
+                .addClip("reinforce_east_guarding", "east-guarding")
+                .addClip("reinforce_dying", "dying");
+    }
+
+    public paint() {
+        this._display(-10, -26, this.width, this.height);
+    }
+}
