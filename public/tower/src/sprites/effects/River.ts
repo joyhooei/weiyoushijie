301ad6ba033e51dbf2e4f268bfec28c27ead2ebb
liweihai <liweihai@huawei.com>
2016-09-12 16:39:15 +0800

    Update River.ts

--- public/tower/src/sprites/effects/River.ts
+++ public/tower/src/sprites/effects/River.ts
@@ -1,13 +1,7 @@
-class River extends Entity {
+class River extends Effect {
     public constructor() {
         super();
         
         this._displays.addClip("river_east_moving", "east-moving");
-    }
-    
-    public initialize(properties: any) {
-        super.initialize(properties);
-        
-        this.move();
     }
 }
