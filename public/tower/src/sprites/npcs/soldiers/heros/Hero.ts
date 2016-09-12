91540984b04373105f9e8118afda4c818623b56b
liweihai <liweihai@huawei.com>
2016-09-12 09:40:37 +0800

    Update Hero.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
@@ -1,6 +1,14 @@
 class Hero extends Soldier implements SoldierCreator {
+    protected _skill : number;
+    
     public constructor() {
         super();
+    }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._skill = 0;
     }
 
     public createSoldier(soldier: Soldier): Soldier {
