0e4939c430f3a6f0c9e3cf06322b94fd850d7d54
li weihai <liweihai2009@gmail.com>
2016-09-12 19:26:54 +0800

    huhu

--- channels/channel_huhuh5.js
+++ channels/channel_huhuh5.js
@@ -6,8 +6,8 @@ module.exports.login = function(game, options) {
 	var self = this;
 	
 	return Q.Promise(function(resolve, reject, notify) {
-		//var url = "http://server.huhuh5.com:8082/dreamspay/appUser/checkToken?token=";
-		var url = "http://test.sh.1251228860.clb.myqcloud.com:8082/dreamspay/appUser/checkToken?token=";
+		var url = "http://server.huhuh5.com:8082/dreamspay/appUser/checkToken?token=";
+		//var url = "http://test.sh.1251228860.clb.myqcloud.com:8082/dreamspay/appUser/checkToken?token=";
 		url += encodeURIComponent(options.token);
 
 		Helper.get(url).then(function(body){
@@ -42,7 +42,7 @@ module.exports.login = function(game, options) {
 module.exports.pay = function(options) {
 	return Q.Promise(function(resolve, reject, notify) {
 		Helper.pay("huhuh5", options.orderId, options.money, true).then(function(message){
-			resolve('succeed');
+			resolve('success');
 		}, function(message){
 			reject("fail");
 		});
