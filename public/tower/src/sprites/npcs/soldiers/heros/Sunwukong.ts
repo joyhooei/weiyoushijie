b54a6313f19d7d5b7ea01179bc9c1df4abd45b09
liweihai <liweihai@huawei.com>
2016-09-14 12:23:10 +0800

    Update Sunwukong.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Sunwukong.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Sunwukong.ts
@@ -9,6 +9,29 @@ class Sunwukong extends Hero {
                         .addClip("sunwukong_east_fighting_3", "east-fighting");
     }
     
+    public setLegend(legend: Legend) {
+        let level = legend.level - 1;
+        
+        this._hp.setMaxHp(level * 15 + this._hp.getMaxHp()); 
+        this._armor += level * 5;
+        this._forceHigh += level * 2;
+        this._forceLow += level;
+        
+        for (let i = 0; i < legend.skills.length; i++) {
+            if (legend.skills[i].name == "重击") {
+                this._force = 2 * legend.skills[i].level;
+            } else if (legend.skills[i].name == "荆棘甲") {
+                this._resistance = 10 +  (legend.skills[i].level - 1) * 20;
+            } else if (legend.skills[i].name == "金刚不坏") {
+                this._hp.setMaxHp(legend.skills[i].level * 30 + this._hp.getMaxHp()); 
+            } else if (legend.skills[i].name == "腾云突击") {
+                this._skill1 = 2 * legend.skills[i].level; 
+            } else if (legend.skills[i].name == "猴毛") {
+                this._skill2 = legend.skills[i].level; 
+            }
+        }
+    }
+    
     protected _useSkill() {
         let random = Math.round(Math.random() * 10);
         
@@ -19,7 +42,9 @@ class Sunwukong extends Hero {
         } else {
             this._skill = 2;
             
-            application.battle.addWarriorsByName("Warrior", this);
+            for(let i = 0; i < this._skill2; i++) {
+                application.battle.addWarriorsByName("Warrior", this, {maxHp: 60 + (this._skill2 - 1) * 40});
+            }
         }
     }
 }
