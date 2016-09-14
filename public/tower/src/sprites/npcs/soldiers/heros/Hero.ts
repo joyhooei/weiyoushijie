9e293ac53d12a1cd6413a49ec99a2ada04d744e7
liweihai <liweihai@huawei.com>
2016-09-14 15:27:35 +0800

    Update Hero.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Hero.ts
@@ -1,6 +1,4 @@
 class Hero extends Soldier implements SoldierCreator {
-    protected _skill : number;
-    
     protected _forceHigh: number;
     
     protected _forceLow: number;
@@ -17,8 +15,6 @@ class Hero extends Soldier implements SoldierCreator {
         this._forceHigh   = this._get(properties, "forceHigh", 10);
         this._forceLow    = this._get(properties, "forceLow", 6);
         this._force = 0;
-        
-        this._skill = 0;
     }
     
     public setLegend(legend: Legend) {
@@ -37,14 +33,4 @@ class Hero extends Soldier implements SoldierCreator {
     public getForce(): number {
         return this._forceLow + Math.round(Math.random() * (this._forceHigh - this._forceLow)) + this._force;
     }
-    
-    public paint() {
-        if (EntityState.fighting == this._state) {
-            this._display(0, this._hp.height + 2, this._skill);
-            
-            this._centerHp();
-        } else {
-            super.paint();
-        }
-    }      
 }
