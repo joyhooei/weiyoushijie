8d5a1da1c43efa4ab6c2a861688085aa6d926f32
liweihai <liweihai@huawei.com>
2016-09-14 15:46:49 +0800

    Update Sunwukong.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Sunwukong.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Sunwukong.ts
@@ -1,4 +1,9 @@
 class Sunwukong extends Hero {
+    private _skill1 : number;
+    
+    private _skill2 : number;
+    private _skill1Times: number;
+    
     public constructor() {
         super();
         
@@ -12,38 +17,64 @@ class Sunwukong extends Hero {
     public setLegend(legend: Legend) {
         let level = legend.level - 1;
         
-        this._hp.setMaxHp(level * 15 + this._hp.getMaxHp()); 
+        this._hp.setMaxHp(level * 15 + 30 + this._hp.getMaxHp()); 
         this._armor += level * 5;
         this._forceHigh += level * 2;
         this._forceLow += level;
         
+        this._force = 0;
+        this._resistance = 10;
+        this._skill1 = 2;
+        this._skill2 = 1;
         for (let i = 0; i < legend.skills.length; i++) {
+            let skill = legend.skills[i];
+            
             if (legend.skills[i].name == "重击") {
-                this._force = 2 * legend.skills[i].level;
+                this._force = 2 * skill.level;
             } else if (legend.skills[i].name == "荆棘甲") {
-                this._resistance = 10 +  (legend.skills[i].level - 1) * 20;
+                this._resistance = 10 +  (skill - 1) * 20;
             } else if (legend.skills[i].name == "金刚不坏") {
-                this._hp.setMaxHp(legend.skills[i].level * 30 + this._hp.getMaxHp()); 
+                this._hp.setMaxHp((skill.level - 1) * 30 + this._hp.getMaxHp()); 
             } else if (legend.skills[i].name == "腾云突击") {
-                this._skill1 = 2 * legend.skills[i].level; 
+                this._skill1 = 2 * skill.level;
             } else if (legend.skills[i].name == "猴毛") {
-                this._skill2 = legend.skills[i].level; 
+                this._skill2 = skill.level;
             }
         }
     }
-    
-    protected _useSkill() {
+
+    protected _nextSkill() {
         let random = Math.round(Math.random() * 10);
         
         if (random <= 5) {
             this._skill = 0;
         } else if (random <= 8) {
             this._skill = 1;
+            this._skill1Times = 0;
         } else {
             this._skill = 2;
+        }        
+    }
+    
+    protected _fightOpponent() {
+        if (this._skill == 0) {
+            super._fightOpponents();
             
+            this._nextSkill();
+        } else if (this._skill == 2) {
             for(let i = 0; i < this._skill2; i++) {
-                application.battle.addWarriorsByName("Warrior", this, {maxHp: 60 + (this._skill2 - 1) * 40});
+                application.battle.addWarriorsByName("Warrior", this, {maxHp: 60 + (this._skill2 - 1) * 40, liveTicks:8 * application.frameRate});
+            }
+            
+            this._nextSkill();
+        } else {
+            super._fightOpponents();
+            
+            this._skill1Times ++;
+            if (this._skill1Times < this._skill1) {
+                this._playFightMovieClip();
+            } else {
+                this._nextSkill();
             }
         }
     }
