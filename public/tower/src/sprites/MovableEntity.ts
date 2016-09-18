ed00f8e4304b63a05be4dc857dc0bad87f68282f
liweihai <liweihai@huawei.com>
2016-09-18 09:07:12 +0800

    Update MovableEntity.ts

--- public/tower/src/sprites/MovableEntity.ts
+++ public/tower/src/sprites/MovableEntity.ts
@@ -7,8 +7,9 @@ class MovableEntity extends Entity {
     
     protected _dyingTicks:number;
     
-    //当前路径每步走的距离，0表示x方向，1表示y方向
-    protected _delta: number[];
+    //当前路径每步走的距离
+    protected _stepX: number;
+    protected _stepY: number;
     //当前路径一共需要走多少步
     protected _totalSteps: number;
     //当前路径已经走了多少步
@@ -25,7 +26,8 @@ class MovableEntity extends Entity {
         this._idleTicks  = this._get(properties, "idleTicks", 0);
         this._dyingTicks = this._get(properties, "idleTicks", 5);
 
-        this._delta = [];
+        this._stepX = 0;
+        this._stepY = 0;
         this._totalSteps = 0;
         this._steps = 0;
     }
@@ -33,11 +35,11 @@ class MovableEntity extends Entity {
     //走一步，true表示已经到了终点
     protected _moveOneStep(): boolean {
     	this._steps ++;
-        if (this._steps >= this._totalSteps) {
+        if (this._steps > this._totalSteps) {
             return true;
         } else {
-	        this.x += this._delta[0];
-	        this.y += this._delta[1];
+	        this.x += this._stepX;
+	        this.y += this._stepY;
 	        
 	        return false;
         }
@@ -45,9 +47,8 @@ class MovableEntity extends Entity {
 
     //计一步走的距离
     protected _computeSteps(x1:number, y1:number, x2:number, y2:number): boolean {
-	    let stepX = 0;
-	    let stepY = 0;
-	    
+		this._steps = 0;
+		
 	    let dx = Math.abs(x1 - x2);
 	    let dy = Math.abs(y1 - y2);
 	    if (dx >= dy) {
@@ -57,19 +58,16 @@ class MovableEntity extends Entity {
 	    }
 	    
 	    if (this._totalSteps > 0) {
-	        stepX = dx / this._totalSteps;
+	        this._stepX = dx / this._totalSteps;
 	        if (x2 < x1) {
-	            stepX = 0 - stepX;
+	            this._stepX = 0 - this._stepX;
 	        }
 	        
-	        stepY = dy / this._totalSteps;
+	        this._stepY = dy / this._totalSteps;
 	        if (y2 < y1) {
-	            stepY = 0 - stepY;
+	            this._stepY = 0 - this._stepY;
 	        }
 	
-		   	this._delta = [stepX, stepY];
-			this._steps = 0;
-			
 			return true;
 	    } else {
 	    	return false;
