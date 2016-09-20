b148a5611da5e991cded6a68fbe196e26962e8d7
liweihai <liweihai@huawei.com>
2016-09-20 11:37:05 +0800

    Update Rhino.ts

--- public/tower/src/sprites/npcs/enemies/Rhino.ts
+++ public/tower/src/sprites/npcs/enemies/Rhino.ts
@@ -2,9 +2,9 @@ class Rhino extends Enemy {
     public constructor() {
         super();
         
-        this.addClip("rhino_east_moving", "east-moving")
-                    .addClip("rhino_dying", "dying")
-                    .addClip("rhino_east_moving", "guarding")
-                    .addClip("rhino_east_fighting", "east-fighting");
+        this.addClip("rhino_east_moving", "east-moving", 8)
+            .addClip("rhino_dying", "dying", 8)
+            .addClip("rhino_east_moving", "guarding", 8)
+            .addClip("rhino_east_fighting", "east-fighting", 8);
     }
 }
