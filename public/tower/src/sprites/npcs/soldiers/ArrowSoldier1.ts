5a8bbc20c27f8704122366c3bed6490ede0be88c
liweihai <liweihai@huawei.com>
2016-09-20 11:40:53 +0800

    Create ArrowSoldier1.ts

---
+++ public/tower/src/sprites/npcs/soldiers/ArrowSoldier1.ts
@@ -0,0 +1,8 @@
+class ArrowSoldier1 extends ArrowSoldier {
+    public constructor() {
+        super();
+        
+        this.addClip("arrowsoldier1_fighting", "fighting")
+            .addClip("arrowsoldier1_guarding", "guarding");
+    }
+}
