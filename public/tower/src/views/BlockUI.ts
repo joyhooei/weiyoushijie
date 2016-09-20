5afd689dbab170005f41f8b3cd9ba5af058309a4
liweihai <liweihai@huawei.com>
2016-09-20 11:11:11 +0800

    Update BlockUI.ts

--- public/tower/src/views/BlockUI.ts
+++ public/tower/src/views/BlockUI.ts
@@ -1,11 +1,14 @@
 class BlockUI extends AbstractUI {
+	public rcBlock: eui.Rect;
+	
     public constructor() {
         super('blockUISkin');
+        
         this.addEventListener(egret.TouchEvent.TOUCH_TAP,(event:egret.TouchEvent) => {
             for(let i = 0; i < this.numChildren; i++) {
                 let child = this.getChildAt(i);
 
-                if (egret.getQualifiedClassName(child) != "eui.Rect" && child.hitTestPoint(event.stageX, event.stageY)) {
+                if (child != this.rcBlock && child.hitTestPoint(event.stageX, event.stageY)) {
                     return;
                 }
             }
