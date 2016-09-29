b5200b68514263d1f31adfb4673c0318c0e5a8e4
liweihai <liweihai@huawei.com>
2016-09-29 20:29:08 +0800

    Update Cock.ts

--- public/tower/src/sprites/effects/Cock.ts
+++ public/tower/src/sprites/effects/Cock.ts
@@ -2,6 +2,6 @@ class Cock extends Effect {
     public constructor() {
         super();
         
-        this.addClip("cock_east_moving", "east-moving");
+        this.addClip("cock_east_moving");
     }
 }
