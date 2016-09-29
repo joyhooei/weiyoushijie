05e96a954643a167190afa3c49e2b99a68d08eab
liweihai <liweihai@huawei.com>
2016-09-29 14:29:37 +0800

    Update and rename SoldierTower31.ts to SoldierTower3.ts

---
+++ public/tower/src/sprites/towers/SoldierTower3.ts
@@ -0,0 +1,10 @@
+class SoldierTower3 extends SoldierTower {
+    public constructor() {
+        super();
+        
+        this.addBitmap("soldiertower3_png");
+        
+        this._soldierClaz = "Soldier3";
+    }
+}
+
