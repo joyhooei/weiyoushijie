126b9b7b657f3c8a4f96cf3aaa8e6228569f5fda
liweihai <liweihai@huawei.com>
2016-09-29 09:03:10 +0800

    Update MagicTower1.ts

--- public/tower/src/sprites/towers/MagicTower1.ts
+++ public/tower/src/sprites/towers/MagicTower1.ts
@@ -2,6 +2,8 @@ class MagicTower1 extends MagicTower {
     public constructor() {
         super();
         
+        this._soldierClaz = "MagicSoldier1";
+        
         this.addBitmap("magictower1_png");
     }
 }
