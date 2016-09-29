e8ddbf4106072f9c661323a08a0f050f85adb35e
liweihai <liweihai@huawei.com>
2016-09-29 14:35:50 +0800

    Create ArrowTower5.ts

---
+++ public/tower/src/sprites/towers/ArrowTower5.ts
@@ -0,0 +1,9 @@
+class ArrowTower5 extends ArrowTower {
+    public constructor() {
+        super();
+        
+        this.addBitmap("arrowtower5_png");
+        
+        this._soldierClaz = "ArrowSoldier5";
+    }
+}
