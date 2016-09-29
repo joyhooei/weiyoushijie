14e5a8a41f1bd1e88fb14c98674f9eca9f4d0096
liweihai <liweihai@huawei.com>
2016-09-29 11:01:30 +0800

    Update Warrior.ts

--- public/tower/src/sprites/npcs/soldiers/Warrior.ts
+++ public/tower/src/sprites/npcs/soldiers/Warrior.ts
@@ -2,7 +2,7 @@ class Warrior extends Soldier {
     public constructor() {
         super();
         
-        this.addClip("warrior_east_fighting", "east-fighting").addClip("warrior_east_fighting", "moving").addClip("warrior_east_fighting", "guarding")
-                .addClip("warrior_dying", "dying");
+        this.addClip("warrior_east_fighting", ["east-fighting", "south-fighting","north-fighting", "east-moving", "south-moving","north-moving", "east-guarding", "west-guarding", "south-guarding", "north-guarding"])
+            .addClip("warrior_dying", "dying");
     }
 }
