2039c6687835a0ed602b2eb03f3bcd6b1ad22cb0
liweihai <liweihai@huawei.com>
2016-09-29 20:20:52 +0800

    Update BombSoldier1.ts

--- public/tower/src/sprites/npcs/soldiers/BombSoldier1.ts
+++ public/tower/src/sprites/npcs/soldiers/BombSoldier1.ts
@@ -2,7 +2,7 @@ class BombSoldier1 extends ShootSoldier {
     public constructor() {
         super();
         
-        this._bulletClaz = "Bomb";
+        this._bulletClaz = "Bomb1";
         
         this.addClip("bombsoldier1_fighting", "fighting")
             .addClip("bombsoldier1_east_guarding_png", ["east-guarding", "south-guarding"])
