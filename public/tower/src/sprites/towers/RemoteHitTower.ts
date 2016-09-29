d8bb69eb9e90598b76d9ccbbf47749a9b6ef302e
liweihai <liweihai@huawei.com>
2016-09-29 08:59:44 +0800

    Update RemoteHitTower.ts

--- public/tower/src/sprites/towers/RemoteHitTower.ts
+++ public/tower/src/sprites/towers/RemoteHitTower.ts
@@ -1,7 +1,7 @@
-class RemoteHitTower extends Tower {
-    protected _enemy: Enemy;
+class ShootTower extends Tower {
+    protected _soldier: ShootSoldier;
     
-    protected _bulletName: string;
+    protected _soldierClaz: string;
 
     public constructor() {
         super();
@@ -9,23 +9,27 @@ class RemoteHitTower extends Tower {
     
     public initialize(properties:any) {
         super.initialize(properties);
-
-        this._bulletName = this._get(properties, "bulletName", "Bomb");
         
-        this._enemy = null;
+        this._soldiers = [];
     }
 
-    protected _guarding() {
-        if (this._ticks % this._hitSpeed == 0) {
-            if (this._enemy == null 
-                    || !this._enemy.active() 
-                    || !this._enemy.within(this.parent.x, this.parent.y, this._guardRadius)) {
-                this._enemy = application.battle.findEnemy(this.parent.x, this.parent.y, this._guardRadius, [0]);
-            }
-
-            if (this._enemy) {
-                Bullet.shootAtNPC(this.getCenterX(), this.getCenterY(), this._enemy, this._bulletName);
-            }
+    public erase() {
+        super.erase();
+        
+        if (this._soldier) {
+            this._soldier.erase();
         }
+        this._soldier = null;
+    }
+
+    public guard(){
+        super.guard();
+
+        this._soldier =  <ShootSoldier>application.pool.get(claz, {guardX: this.getCenterX(), guardY: this.getCenterY(), guardRadius: this._guardRadius});
+                
+        this._soldier.setCenterX(this.getCenterX());
+        this._soldier.setBottomY(this.getCenterY());
+        
+        application.battle.addEntity(this._soldier);
     }
 }
