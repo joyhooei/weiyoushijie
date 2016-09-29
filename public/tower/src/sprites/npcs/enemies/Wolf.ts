8cbf9fcf8c2b7fe722094eeb56f0184c9d04c623
liweihai <liweihai@huawei.com>
2016-09-29 11:05:33 +0800

    Update Wolf.ts

--- public/tower/src/sprites/npcs/enemies/Wolf.ts
+++ public/tower/src/sprites/npcs/enemies/Wolf.ts
@@ -2,9 +2,8 @@ class Wolf extends Enemy {
     public constructor() {
         super();
         
-        this.addClip("wolf_east_moving", "east-moving")
-            .addClip("wolf_dying", "dying")
-            .addClip("wolf_east_moving", "guarding")
-            .addClip("wolf_east_fighting", "east-fighting");
+        this.addClip("wolf_dying", "dying")
+            .addClip("wolf_east_moving", ["east-moving", "south-moving","north-moving", "guarding"])
+            .addClip("wolf_east_fighting", ["east-fighting", "south-fighting","north-fighting"]);
     }
 }
