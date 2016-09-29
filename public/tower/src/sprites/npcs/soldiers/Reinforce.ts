8fdb104cbafb05ed4d1766c51e84f890f46e8d6e
liweihai <liweihai@huawei.com>
2016-09-29 11:00:35 +0800

    Update Reinforce.ts

--- public/tower/src/sprites/npcs/soldiers/Reinforce.ts
+++ public/tower/src/sprites/npcs/soldiers/Reinforce.ts
@@ -2,7 +2,7 @@ class Reinforce extends Soldier {
     public constructor() {
         super();
         
-        this.addClip("reinforce_east_fighting", "east-fighting")
+        this.addClip("reinforce_east_fighting", ["east-fighting", "south-fighting","north-fighting"])
             .addClip("reinforce_east_moving", ["east-moving", "south-moving","north-moving", "east-guarding", "west-guarding", "south-guarding", "north-guarding"])
             .addClip("reinforce_dying", "dying");
     }
