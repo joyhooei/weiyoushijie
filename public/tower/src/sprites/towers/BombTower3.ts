b5c6f67724f3f3ea88b8ef5f68caefc9a2b9b005
liweihai <liweihai@huawei.com>
2016-09-29 14:18:50 +0800

    Update and rename BombTower31.ts to BombTower3.ts

--- public/tower/src/sprites/towers/BombTower3.ts
+++ public/tower/src/sprites/towers/BombTower3.ts
@@ -1,6 +1,8 @@
-class BombTower31 extends BombTower {
+class BombTower3 extends BombTower {
     public constructor() {
         super();
+        
+        this._soldierClaz = "BombSoldier3";
         
         this.addBitmap("bombtower31_png");
     }
