f13baeac71aad21521852db766fdd59b36e1e319
liweihai <liweihai@huawei.com>
2016-09-29 09:02:34 +0800

    Update BombTower1.ts

--- public/tower/src/sprites/towers/BombTower1.ts
+++ public/tower/src/sprites/towers/BombTower1.ts
@@ -2,6 +2,8 @@ class BombTower1 extends BombTower {
     public constructor() {
         super();
         
+        this._soldierClaz = "BombSoldier1";
+        
         this.addBitmap("bombtower1_png");
     }
 }
