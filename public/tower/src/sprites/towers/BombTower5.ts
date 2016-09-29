e5e41d6a94594e14689459fca47e03803eb0d389
liweihai <liweihai@huawei.com>
2016-09-29 14:20:00 +0800

    Create BombTower5.ts

---
+++ public/tower/src/sprites/towers/BombTower5.ts
@@ -0,0 +1,9 @@
+class BombTower5 extends BombTower {
+    public constructor() {
+        super();
+        
+        this._soldierClaz = "BombSoldier5";
+        
+        this.addBitmap("bombtower5_png");
+    }
+}
