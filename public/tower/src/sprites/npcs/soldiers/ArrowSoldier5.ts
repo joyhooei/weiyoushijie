7b975a06c70daafa3b140ca175abf1cc5d07ce8b
liweihai <liweihai@huawei.com>
2016-09-29 20:20:32 +0800

    Update ArrowSoldier5.ts

--- public/tower/src/sprites/npcs/soldiers/ArrowSoldier5.ts
+++ public/tower/src/sprites/npcs/soldiers/ArrowSoldier5.ts
@@ -2,7 +2,7 @@ class ArrowSoldier5 extends ShootSoldier {
     public constructor() {
         super();
         
-        this._bulletClaz = "Arrow";
+        this._bulletClaz = "Arrow5";
         
         this.addClip("arrowsoldier5_east_fighting", ["east-fighting", "south-fighting"])
             .addClip("arrowsoldier5_west_fighting", ["west-fighting", "north-fighting"])
