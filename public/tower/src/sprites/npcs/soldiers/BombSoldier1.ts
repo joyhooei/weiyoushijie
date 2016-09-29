087591ce9e6ea24546d7785e90bbdd9109715e9b
liweihai <liweihai@huawei.com>
2016-09-29 14:17:27 +0800

    Update BombSoldier1.ts

--- public/tower/src/sprites/npcs/soldiers/BombSoldier1.ts
+++ public/tower/src/sprites/npcs/soldiers/BombSoldier1.ts
@@ -4,6 +4,8 @@ class BombSoldier1 extends ShootSoldier {
         
         this._bulletClaz = "Bomb";
         
-        this.addClip("bombsoldier1_fighting", "fighting").addClip("bombsoldier1_guarding", "guarding");
+        this.addClip("bombsoldier1_fighting", "fighting")
+            .addClip("bombsoldier1_east_guarding_png", ["east-guarding", "south-guarding"])
+            .addClip("bombsoldier1_west_guarding_png", ["west-guarding", "north-guarding"]);
     }
 }
