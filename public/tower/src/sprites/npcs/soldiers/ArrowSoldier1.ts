0152764ec658f078f1043af1d6e547558e0d970a
liweihai <liweihai@huawei.com>
2016-09-29 08:53:13 +0800

    Update ArrowSoldier1.ts

--- public/tower/src/sprites/npcs/soldiers/ArrowSoldier1.ts
+++ public/tower/src/sprites/npcs/soldiers/ArrowSoldier1.ts
@@ -1,6 +1,8 @@
-class ArrowSoldier1 extends ArrowSoldier {
+class ArrowSoldier1 extends ShootSoldier {
     public constructor() {
         super();
+        
+        this._bulletClaz = "Arrow";
         
         this.addClip("arrowsoldier1_fighting", "fighting")
             .addClip("arrowsoldier1_guarding", "guarding");
