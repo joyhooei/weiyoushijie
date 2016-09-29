b9644a3cacefce68fbc2ca49a273c1b6b4a1cc92
liweihai <liweihai@huawei.com>
2016-09-29 14:45:25 +0800

    Create ArrowSoldier3.ts

---
+++ public/tower/src/sprites/npcs/soldiers/ArrowSoldier3.ts
@@ -0,0 +1,11 @@
+class ArrowSoldier3 extends ShootSoldier {
+    public constructor() {
+        super();
+        
+        this._bulletClaz = "Arrow";
+        
+        this.addClip("arrowsoldier3_east_fighting", ["east-fighting", "south-fighting"])
+            .addClip("arrowsoldier3_west_fighting", ["west-fighting", "north-fighting"])
+            .addBitmap("arrowsoldier3_guarding_png");
+    }
+}
