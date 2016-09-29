60eb135886d455372f5d499c5ff17cb469fa5132
liweihai <liweihai@huawei.com>
2016-09-29 20:15:53 +0800

    Update and rename Arrow32.ts to Arrow2.ts

---
+++ public/tower/src/sprites/bullets/Arrow2.ts
@@ -0,0 +1,7 @@
+class Arrow2 extends Arrow {
+    public constructor() {
+        super();
+        
+        this.addClip("arrow2");
+    }
+}
--- public/tower/src/sprites/bullets/Arrow32.ts
+++
@@ -1,7 +0,0 @@
-class Arrow32 extends Arrow {
-    public constructor() {
-        super();
-        
-        this.addClip("arrow32");
-    }
-}
