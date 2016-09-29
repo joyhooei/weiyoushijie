84e68c85c084fd50c438273695a987b3ae4591b6
liweihai <liweihai@huawei.com>
2016-09-29 19:52:06 +0800

    Update Character.ts

--- public/tower/src/models/Character.ts
+++ public/tower/src/models/Character.ts
@@ -14,28 +14,28 @@ class Character {
         
         let config = [
             //heros
-            {name: 'Sunwukong', properties:{hp:245, forceHigh:10, forceLow:6, armor: 15, cureSpeed:1, moveSpeed:1, idleTicks: 20 * application.frameRate, guardRadius: 50}},
+            {name: 'Sunwukong', properties:{hp:245, forceHigh:10, forceLow:6, armor: 15, cureSpeed:1, moveSpeed:1, idleTicks: 20 * application.frameRate, guardRadius: 50, guardAltitude:[-1, 0]}},
 
             //soldiers
-            {name: 'Reinforce', properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, liveTicks: 20 * application.frameRate, guardRadius: 40}},
-            {name: 'Warrior',   properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, liveTicks: 8 * application.frameRate, guardRadius: 40}},
+            {name: 'Reinforce', properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, liveTicks: 20 * application.frameRate, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'Warrior',   properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, liveTicks: 8 * application.frameRate, guardRadius: 40, guardAltitude:[-1, 0]}},
 
             //tower soldiers
-            {name: 'Soldier1',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'Soldier2',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'Soldier3',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'Soldier4',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'Soldier5',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'BombSoldier1',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'BombSoldier2',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'BombSoldier3',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'BombSoldier4',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'BombSoldier5',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'ArrowSoldier1',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'ArrowSoldier2',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'ArrowSoldier3',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'ArrowSoldier4',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},
-            {name: 'ArrowSoldier5',  properties:{hp:245, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40}},            
+            {name: 'Soldier1',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'Soldier2',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'Soldier3',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'Soldier4',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'Soldier5',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'BombSoldier1',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'BombSoldier2',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'BombSoldier3',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'BombSoldier4',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'BombSoldier5',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0]}},
+            {name: 'ArrowSoldier1',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0, 1]}},
+            {name: 'ArrowSoldier2',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0, 1]}},
+            {name: 'ArrowSoldier3',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0, 1]}},
+            {name: 'ArrowSoldier4',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0, 1]}},
+            {name: 'ArrowSoldier5',  properties:{hp:100, force:6, armor: 0, cureSpeed:1, moveSpeed:1, guardRadius: 40, guardAltitude:[-1, 0, 1]}},            
 
             //enemies
             {name: 'Hogs',  properties:{bonus:10, hp:100, force:6, cureSpeed:0, moveSpeed:1, idleTick:0, dyingTicks:10}},
@@ -44,9 +44,9 @@ class Character {
 
             //bullets
             {name: 'Fireball', properties:{force:6, hitRadius:10, moveSpeed:5, idleTick:0, dyingTicks:10}},
-            {name: 'Bomb', properties:{force:6, hitRadius:10, moveSpeed:5, idleTick:0, dyingTicks:10}},
-            {name: 'Magic', properties:{force:6, moveSpeed:5, idleTick:0, dyingTicks:10}},
-            {name: 'Arrow', properties:{force:6, moveSpeed:5, idleTick:0, dyingTicks:10}},
+            {name: 'Bomb',     properties:{force:6, hitRadius:10, moveSpeed:5, idleTick:0, dyingTicks:10}},
+            {name: 'Magic',    properties:{force:6, moveSpeed:5, idleTick:0, dyingTicks:10}},
+            {name: 'Arrow',    properties:{force:6, moveSpeed:5, idleTick:0, dyingTicks:10}},
         ]; 
 
         for(let i = 0; i < config.length; i++) {
