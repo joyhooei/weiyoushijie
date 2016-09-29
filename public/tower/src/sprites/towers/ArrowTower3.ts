760791dd01dd5a8aae740737d73e1a45054670ea
liweihai <liweihai@huawei.com>
2016-09-29 14:33:42 +0800

    Update and rename ArrowTower32.ts to ArrowTower3.ts

---
+++ public/tower/src/sprites/towers/ArrowTower3.ts
@@ -0,0 +1,9 @@
+class ArrowTower3 extends ArrowTower {
+    public constructor() {
+        super();
+        
+        this.addBitmap("arrowtower3_png");
+        
+        this._soldierClaz = "ArrowSoldier3";
+    }
+}
--- public/tower/src/sprites/towers/ArrowTower32.ts
+++
@@ -1,7 +0,0 @@
-class ArrowTower32 extends ArrowTower {
-    public constructor() {
-        super();
-        
-        this.addBitmap("arrowtower32_png");
-    }
-}
