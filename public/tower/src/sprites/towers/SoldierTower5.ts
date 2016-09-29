e1abcb3c089c5714f0e35b0b8f9744f22eb260f1
liweihai <liweihai@huawei.com>
2016-09-29 14:30:45 +0800

    Create SoldierTower5.ts

---
+++ public/tower/src/sprites/towers/SoldierTower5.ts
@@ -0,0 +1,9 @@
+class SoldierTower5 extends SoldierTower {
+    public constructor() {
+        super();
+        
+        this.addBitmap("soldiertower5_png");
+        
+        this._soldierClaz = "Soldier5";
+    }
+}
