d9039cd2511de80808cb1752d9da38c5511e2283
liweihai <liweihai@huawei.com>
2016-09-30 08:51:10 +0800

    Update Battle.ts

--- public/tower/src/sprites/battles/Battle.ts
+++ public/tower/src/sprites/battles/Battle.ts
@@ -269,12 +269,17 @@ class Battle extends Entity implements SoldierCreator {
     public erase() {
         super.erase();
         
-        this._eraseEntities(this._soldiers);
-        this._eraseEntities(this._enemies);
         this._eraseEntities(this._bases);
+        
+        for(let i = 0; i < this._soldiers.length; i++) {
+            this._soldiers[i].setCreator(null);
+        }
+        this._eraseEntities(this._soldiers);
+        
+        this._eraseEntities(this._enemies);
         this._eraseEntities(this._entities);
     }
-    
+
     private _eraseEntities(entities: Entity[]){
         let i = entities.length;
         while(i > 0) {
