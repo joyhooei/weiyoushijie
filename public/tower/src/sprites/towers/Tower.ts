bedb337cd713420f3d6d3255cf8745b87266862b
liweihai <liweihai@huawei.com>
2016-09-30 09:27:12 +0800

    Update Tower.ts

--- public/tower/src/sprites/towers/Tower.ts
+++ public/tower/src/sprites/towers/Tower.ts
@@ -24,6 +24,21 @@ class Tower extends Entity {
         this._guardRadius = this._get(properties, "guardRadius", 10);
     }
 
+	public snapshot(): string {
+		return this._displays.snapshot();
+	}
+
+	public getNextLevelTowerClaz(): string {
+		let claz = this.getClassName();
+		
+		let idx = parseInt(claz.charAt(claz.length - 1));
+		if (idx >= 1 && idx <= 4) {
+			return claz.slice(0, claz.length - 2) + (idx + 1).toString();
+		} else {
+			return null;
+		}
+	}
+
 	public getPrice(): number {
 		return this._price;
 	}
