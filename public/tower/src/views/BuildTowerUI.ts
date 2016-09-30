db7684f15ec11c1bf6bc3d6cf40029121d533c72
liweihai <liweihai@huawei.com>
2016-09-30 17:17:14 +0800

    Update BuildTowerUI.ts

--- public/tower/src/views/BuildTowerUI.ts
+++ public/tower/src/views/BuildTowerUI.ts
@@ -1,17 +1,17 @@
 class BuildTowerUI extends AbstractUI{
     private _base: Base;
     
-    private _mcSoldierTower: egret.MovieClip;
-    private _lblSoldierTower: eui.Label;
-    
-    private _mcArrowTower: egret.MovieClip;
-    private _lblArrowTower: eui.Label;
-    
-    private _mcBombTower: egret.MovieClip;
-    private _lblBombTower: eui.Label;
-    
-    private _mcMagicTower: egret.MovieClip;
-    private _lblMagicTower: eui.Label;
+    public lblSoldierTower: eui.Label;
+    public lblArrowTower: eui.Label;
+    public lblBombTower: eui.Label;
+    public lblMagicTower: eui.Label;
+
+	private _mcTowers: egret.MovieClip[];
+
+	private _soldierTower: Tower;
+	private _arrowTower: Tower;
+	private _bombTower: Tower;
+	private _magicTower: Tower;
     
     constructor(base: Base) {
         super("buildTowerUISkin");
@@ -21,42 +21,63 @@ class BuildTowerUI extends AbstractUI{
         application.dao.addEventListener("Battle",function(evt: egret.Event) {
             this.refresh();
         },this);
+		
+        this._mcTowers = [];
+		application.stopwatch.addEventListener("second", function(event:egret.Event){
+			let i = Math.round(Math.random() * this._mcTowers.length * 10);
+			if (i < this._mcTowers.length) {
+				this._mcTowers[i].gotoAndPlay(0, 1);
+			}
+		}, this);
     }
     
     protected onRefresh() {
 		this.touchEnabled = true;
 		this.touchChildren = true;
 
-		this._renderTower(96, 15, "soldier_tower", "SoldierTower1", 300);
-		this._renderTower(32, 80, "arrow_tower", "ArrowTower1", 300);
-		this._renderTower(160, 80, "bomb_tower", "BombTower1", 300);
-		this._renderTower(96, 145, "magic_tower", "MagicTower1", 300);
+		let options = {guardX: this._base.getGuardX(), guardY: this._base.getGuardY()};
+		
+		this._soldierTower = <Tower>application.pool.get("SoldierTower1",options);
+		this.lblSoldierTower.text = this._soldierTower.getPrice();
+		this._mcTowers.push(this._renderTower(96, 15, "soldier_tower", this._soldierTower));
+		
+		this._arrowTower = <Tower>application.pool.get("ArrowTower1", options);
+		this.lblArrowTower.text = this._arrowTower.getPrice();
+		this._mcTowers.push(this._renderTower(32, 80, "arrow_tower", this._arrowTower));
+		
+		this._bombTower = <Tower>application.pool.get("BombTower1", options);
+		this.lblBombTower.text = this._bombTower.getPrice();
+		this._mcTowers.push(this._renderTower(160, 80, "bomb_tower", this._bombTower));
+		
+		this._magicTower = <Tower>application.pool.get("MagicTower1", options);
+		this.lblMagicTower.text = this._magicTower.getPrice();
+		this._mcTowers.push(this._renderTower(96, 145, "magic_tower", this._magicTower));
     }
 
-	private _renderTower(x, y, res:string, claz:string, price:number) {
+	private _renderTower(x, y, res:string, tower:Tower): egret.MovieClip {
 		let data = RES.getRes(res + "_json");
 		let txtr = RES.getRes(res + "_png");
 		let mcFactory:egret.MovieClipDataFactory = new egret.MovieClipDataFactory( data, txtr );	
 
 		let clip = new egret.MovieClip( mcFactory.generateMovieClipData( res ) );	
 		clip.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
-		    if (application.battle.getGolds() > price) {
-		        application.battle.incGolds(-price);
-		        
-		        this._base.setTower(<Tower>application.pool.get(claz, {guardX: this._base.getGuardX(), guardY: this._base.getGuardY()}));
+		    if (application.battle.getGolds() > tower.getPrice()) {
+		        this._base.setTower(tower);
 		        
 		        application.hideUI(this);
-		    }
+		    } else {
+				Toast.launch("需要更多的金币");
+			}
 		}, this);
 		clip.touchEnabled = true;
 
 		clip.x = x + 20;
 		clip.y = y + 20;
 
-		clip.frameRate = 8;
+		clip.frameRate = 6;
 
 		this.addChild(clip);
 
-		clip.gotoAndPlay(0, -1);
+		return clip;
 	}
 }
