f26473840b470c7e94a4a8db0e3300c4d98e1878
liweihai <liweihai@huawei.com>
2016-10-08 09:05:24 +0800

    Update Battle1.ts

--- public/tower/src/sprites/battles/Battle1.ts
+++ public/tower/src/sprites/battles/Battle1.ts
@@ -6,21 +6,6 @@ class Battle1 extends Battle {
         this._url = "resource/art/sprites/battles/battle1.tmx";
     }
     
-    protected _addBases() {
-        this._addBasesByName("Base1");
-    }
-    
-    //增加英雄
-    protected _addHeros() {
-        this._addHerosByName("Sunwukong");
-    }
-    
-    protected _addEffects() {
-        this._addEffectByName("River", 0, 290, EntityDirection.east);
-        this._addEffectByName("Cock", 120, 70, EntityDirection.east);
-        this._addEffectByName("Cock", 540, 400, EntityDirection.west);
-    }
-    
     protected _addStandbys() {
         let paths = this._map.getPaths();
         
