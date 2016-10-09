9d4fc44529be8546cd9bd22203a3b0da46475b7d
liweihai <liweihai@huawei.com>
2016-10-09 09:15:31 +0800

    Update UpgradeTowerUI.ts

--- public/tower/src/views/UpgradeTowerUI.ts
+++ public/tower/src/views/UpgradeTowerUI.ts
@@ -1,5 +1,6 @@
 class UpgradeTowerUI extends AbstractUI{
     private _base: Base;
+	private _tower: Tower;
 
 	private _nextLevelTower: Tower;
 
@@ -8,21 +9,59 @@ class UpgradeTowerUI extends AbstractUI{
     public imgUpgrade: eui.Image;
     public lblUpgraePrice:eui.Label;
 
+	public imgFlag: eui.Image;
+
     constructor(base: Base) {
         super("upgradeTowerUISkin");
         
         this._base = base;
+		this._tower = base.getTower();
 		
-		let claz = this._base.getTower().getNextLevelTowerClaz();
+		let claz = this._tower.getNextLevelTowerClaz();
 		if (claz) {
 			this._nextLevelTower = <Tower>application.pool.get(claz);
 		} else {
 			this._nextLevelTower = null;
 		}
-        
+		
+		if (this._tower.getSuperClaz() == "SoldierTower") {
+			this.imgFlag.visible = true;
+			
+			let soldierTower = <SoldierTower>this._tower;
+			
+			this.imgFlag.x = soldierTower.getGuardX() - this.imgFlag.height;
+			this.imgFlag.y = soldierTower.getGuardY() - this.imgFlag.width / 2;
+		} else {
+			this.imgFlag.visible = false;
+		}
+		        
         application.dao.addEventListener("Battle",function(evt: egret.Event) {
             this.refresh();
         },this);
+		
+		this.addEventListener(egret.TouchEvent.TOUCH_TAP,(e:egret.TouchEvent) => {
+			if (this.imgFlag.visible) {
+    	        let x = Math.round(e.localX);
+                let y = Math.round(e.localY);
+			
+				this.imgFlag.x = x - this.imgFlag.height;
+				this.imgFlag.y = y - this.imgFlag.width / 2;
+				
+				let soldierTower = <SoldierTower>this._tower;
+			
+				if (x != soldierTower.getGuardX() || y != soldierTower.getGuardY()) {
+					soldierTower.setGuardX(x);
+					soldierTower.setGuardY(y);
+				}
+				
+				if (this._nextLevelTower) {
+					this._nextLevelTower.setGuardX(x);
+					this._nextLevelTower.setGuardY(y);
+				}
+			} else {
+				application.hideUI(this);
+			}
+		}, this);
 		
 		this.imgSell.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
 			this._base.setTower(null);
@@ -52,6 +91,6 @@ class UpgradeTowerUI extends AbstractUI{
 			this.lblUpgraePrice.text   = "";		
 		}
 		
-		this.lblUpgraePrice.text = this._base.getTower().getSellPrice().toString();
+		this.lblUpgraePrice.text = this._tower.getSellPrice().toString();
     }
 }
