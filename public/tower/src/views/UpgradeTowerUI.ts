e32d04cd051864b6a001a6fea23891797a1b48fa
liweihai <liweihai@huawei.com>
2016-10-09 10:06:30 +0800

    Update UpgradeTowerUI.ts

--- public/tower/src/views/UpgradeTowerUI.ts
+++ public/tower/src/views/UpgradeTowerUI.ts
@@ -3,11 +3,21 @@ class UpgradeTowerUI extends AbstractUI{
 	private _tower: Tower;
 
 	private _nextLevelTower: Tower;
+	private _nextLevelTower2: Tower;
+
+	private imgBg: eui.Image;
 
     public imgSell: eui.Image;
     public lblSellPrice: eui.Label;
+
     public imgUpgrade: eui.Image;
     public lblUpgraePrice:eui.Label;
+
+    public imgUpgrade1: eui.Image;
+    public lblUpgraePrice1:eui.Label;
+
+    public imgUpgrade2: eui.Image;
+    public lblUpgraePrice2:eui.Label;
 
 	public imgFlag: eui.Image;
 
@@ -17,11 +27,17 @@ class UpgradeTowerUI extends AbstractUI{
         this._base = base;
 		this._tower = base.getTower();
 		
-		let claz = this._tower.getNextLevelTowerClaz();
-		if (claz) {
-			this._nextLevelTower = <Tower>application.pool.get(claz);
+		let claz = this._tower.getClaz();
+		let idx = parseInt(claz.charAt(claz.length - 1));
+		if (idx == 1 || idx == 2) {
+			this._nextLevelTower = <Tower>application.pool.get(claz.slice(0, claz.length - 1) + (idx + 1).toString());
+			this._nextLevelTower2 = null;
+		} else if (idx == 3) {
+			this._nextLevelTower  = <Tower>application.pool.get(claz.slice(0, claz.length - 1) + (idx + 1).toString());
+			this._nextLevelTower2 = <Tower>application.pool.get(claz.slice(0, claz.length - 1) + (idx + 2).toString());
 		} else {
 			this._nextLevelTower = null;
+			this._nextLevelTower2 = null;
 		}
 		
 		if (this._tower.getSuperClaz() == "SoldierTower") {
@@ -84,15 +100,75 @@ class UpgradeTowerUI extends AbstractUI{
 				Toast.launch("已经是最高级了");
 			}				
 		}, this);
+		
+		this.imgUpgrade1.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+			if (this._nextLevelTower) {
+				if (application.battle.getGolds() > this._nextLevelTower.getPrice()) {
+					this._base.setTower(this._nextLevelTower);
+
+					application.hideUI(this);
+				} else {
+					Toast.launch("需要更多的金币");
+				}
+			} else {
+				Toast.launch("已经是最高级了");
+			}				
+		}, this);
+		
+		this.imgUpgrade2.addEventListener(egret.TouchEvent.TOUCH_TAP,() => {
+			if (this._nextLevelTower) {
+				if (application.battle.getGolds() > this._nextLevelTower2.getPrice()) {
+					this._base.setTower(this._nextLevelTower2);
+
+					application.hideUI(this);
+				} else {
+					Toast.launch("需要更多的金币");
+				}
+			} else {
+				Toast.launch("已经是最高级了");
+			}				
+		}, this);		
     }
 
     protected _onRefresh() {
 		if (this._nextLevelTower) {
-			this.lblUpgraePrice.text   = this._nextLevelTower.getPrice().toString();
+			if (this._nextLevelTower2) {
+				this.imgBg.source = "";
+				
+				this.lblUpgraePrice1.text   = this._nextLevelTower.getPrice().toString();
+				this.lblUpgraePrice2.text   = this._nextLevelTower2.getPrice().toString();
+				
+				this.lblUpgraePrice1.visible = true;
+				this.lblUpgraePrice2.visible = true;
+				this.imgUpgrade1.visible = true;
+				this.imgUpgrade2.visible = true;
+				this.lblUpgraePrice.visible = false;
+				this.imgUpgrade.visible = false;
+				
+			} else {
+				this.imgBg.source = "";
+				
+				this.lblUpgraePrice.text   = this._nextLevelTower.getPrice().toString();
+				
+				this.lblUpgraePrice1.visible = false;
+				this.lblUpgraePrice2.visible = false;
+				this.imgUpgrade1.visible = false;
+				this.imgUpgrade2.visible = false;
+				this.lblUpgraePrice.visible = true;
+				this.imgUpgrade.visible = true;
+			}
 		} else {
-			this.lblUpgraePrice.text   = "";		
+			this.imgBg.source = "";
+			
+				
+			this.lblUpgraePrice1.visible = false;
+			this.lblUpgraePrice2.visible = false;
+			this.imgUpgrade1.visible = false;
+			this.imgUpgrade2.visible = false;
+			this.lblUpgraePrice.visible = false;
+			this.imgUpgrade.visible = false;
 		}
 		
-		this.lblUpgraePrice.text = this._tower.getSellPrice().toString();
+		this.lblSellPrice.text = this._tower.getSellPrice().toString();
     }
 }
