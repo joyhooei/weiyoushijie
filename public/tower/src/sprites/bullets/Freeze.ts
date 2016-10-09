a59f7021e34a9d34ccbfd0b2e4253553b354135a
liweihai <liweihai@huawei.com>
2016-10-09 17:25:25 +0800

    Update Freeze.ts

--- public/tower/src/sprites/bullets/Freeze.ts
+++ public/tower/src/sprites/bullets/Freeze.ts
@@ -14,7 +14,7 @@ class Freeze extends Bullet {
     }
     
     protected _hitTarget() {
-        let enemies = application.battle.findEnemies(this.getCenterX(), this.getCenterY(), this._hitRadius, [-1, 0, 1]);
+        let enemies = application.battle.getEnemies();
         for (let i = 0; i < enemies.length; i++) {
             enemies[i].frozen();
         }
