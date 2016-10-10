65c3dd812107b07d4d3cdddc65b0e6913c63fb2e
liweihai <liweihai@huawei.com>
2016-10-10 08:51:52 +0800

    Update MagicTower.ts

--- public/tower/src/sprites/towers/MagicTower.ts
+++ public/tower/src/sprites/towers/MagicTower.ts
@@ -1,5 +1,34 @@
 class MagicTower extends Tower {
+    protected _enemy : Enemy;
+
+    protected _bulletClaz: string;
+
     public constructor() {
         super();
     }
+    
+    protected _guarding() {
+        this._enemy = application.battle.findEnemy(this.getCenterX(), this.getCenterY(), this._guardRadius, [0, 1]);
+        if (this._enemy) {
+            this.fight();
+        }
+    }
+
+    protected _fighting() {
+        if (this._ticks % this._hitSpeed == 0) {
+             if (this._enemy == null 
+                    || !this._enemy.active() 
+                    || !this._enemy.within(this.getCenterX(), this.getCenterY(), this._guardRadius)) {
+                this._enemy = application.battle.findEnemy(this.getCenterX(), this.getCenterY(), this._guardRadius, [0, 1]);
+            }
+
+            if (this._enemy) {
+                Bullet.shootByNPC(this, this._enemy, this._bulletClaz);
+                
+                this._ticks++;
+            } else {
+                this.guard();
+            }
+        }
+    }  
 }
