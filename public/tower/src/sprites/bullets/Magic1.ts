5f4466ac263f771a0cfc0c49999a144bcafc8b59
liweihai <liweihai@huawei.com>
2016-10-10 16:11:46 +0800

    Update Magic1.ts

--- public/tower/src/sprites/bullets/Magic1.ts
+++ public/tower/src/sprites/bullets/Magic1.ts
@@ -2,7 +2,7 @@ class Magic1 extends Magic {
     public constructor() {
         super();
         
-        this.addBitmap("magic_moving_png", ["south-moving", "east-moving", "west-moving", "north-moving"])
-            .addBitmap("magic_fighting", ["south-fighting", "east-fighting", "west-fighting", "north-fighting"]);      
+        this.addAllBitmaps("magic_moving_png", "moving")
+            .addAllClips("magic_fighting", "fighting");
     }
 }
