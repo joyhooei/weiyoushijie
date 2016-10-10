96d5940c1f0988baa77762d91502c28bfe8b04e4
liweihai <liweihai@huawei.com>
2016-10-10 16:08:07 +0800

    Update Bomb3.ts

--- public/tower/src/sprites/bullets/Bomb3.ts
+++ public/tower/src/sprites/bullets/Bomb3.ts
@@ -2,8 +2,8 @@ class Bomb3 extends Bomb {
     public constructor() {
         super();
         
-        this.addBitmap("bomb3_moving_png", ["south-moving", "east-moving", "west-moving", "north-moving"])
-            .addClip("bomb_dying", ["south-fighting", "east-fighting", "west-fighting", "north-fighting"])
-            .addBitmap("bomb3_dying_png", "dying");  
+        this.addAllBitmaps("bomb3_moving_png", "moving")
+            .addAllClips("bomb_dying", "fighting")
+            .addAllBitmaps("bomb3_dying_png", "dying");  
     }
 }
