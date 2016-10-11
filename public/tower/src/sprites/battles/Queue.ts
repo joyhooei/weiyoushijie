8ff88f016ee195a691f61a56b9b239d39ac9eabb
liweihai <liweihai@huawei.com>
2016-10-11 10:43:12 +0800

    Update Queue.ts

--- public/tower/src/sprites/battles/Queue.ts
+++ public/tower/src/sprites/battles/Queue.ts
@@ -20,16 +20,16 @@ class Queue {
         this._launched = false;
     }
     
-    public launch(paths: number[][], idleTicks: number) {
+    public launch(path: number[][], idleTicks: number) {
         for(let j = 0; j < this._count; j++) {
-            let enemy = <Enemy>application.pool.get(this._claz, {"paths": this._randomPaths(paths), idleTicks: idleTicks});
+            let enemy = <Enemy>application.pool.get(this._claz, {"paths": this._randomPath(path), idleTicks: idleTicks + Math.random() * (application.frameRate << 2)});
             application.battle.addEnemy(enemy);
         }
         
         this._launched = true;
     }
     
-    private _randomPaths(paths: number[][]): number[][]{
+    private _randomPath(path: number[][]): number[][]{
         let pathWidth = 30;
         let enemyInterval = 20;
         
@@ -38,51 +38,51 @@ class Queue {
         let deltaY = Math.random() * pathWidth - pathWidth / 2;
         let deltaX = Math.random() * pathWidth - pathWidth / 2;
         
-        let newPaths = [];
+        let newPath = [];
         
-        let direction = Entity.direction4(paths[0][0], paths[0][1], paths[1][0], paths[1][1]);
+        let direction = Entity.direction4(path[0][0], path[0][1], path[1][0], path[1][1]);
         switch(direction) {
             case EntityDirection.east:
-                newPaths.push([-entityWidth, paths[0][1] + deltaY]);
+                newPath.push([-entityWidth, path[0][1] + deltaY]);
                 break;
                 
             case EntityDirection.west:
-                newPaths.push([this._mapWidth + entityWidth, paths[0][1] + deltaY]);
+                newPath.push([this._mapWidth + entityWidth, path[0][1] + deltaY]);
                 break;
                 
             case EntityDirection.south:
-                newPaths.push([paths[0][0] + deltaX, -entityWidth]);
+                newPath.push([path[0][0] + deltaX, -entityWidth]);
                 break;
                 
             case EntityDirection.north:
-                newPaths.push([paths[0][0] + deltaX, this._mapHeight + entityWidth]);
+                newPath.push([path[0][0] + deltaX, this._mapHeight + entityWidth]);
                 break;
         }
         
-        for(let j = 1; j < paths.length - 1; j++) {
-            newPaths.push([paths[j][0] + deltaX, paths[j][1] + deltaY]);
+        for(let j = 1; j < path.length - 1; j++) {
+            newPath.push([path[j][0] + deltaX, path[j][1] + deltaY]);
         }
         
-        direction = Entity.direction4(paths[paths.length - 2][0], paths[paths.length - 2][1], paths[paths.length - 1][0], paths[paths.length - 1][1]);
+        direction = Entity.direction4(paths[path.length - 2][0], path[path.length - 2][1], path[path.length - 1][0], path[path.length - 1][1]);
         switch(direction) {
             case EntityDirection.east:
-                newPaths.push([this._mapWidth, paths[paths.length - 1][1] + deltaY]);        
+                newPath.push([this._mapWidth, path[path.length - 1][1] + deltaY]);        
                 break;
                 
             case EntityDirection.west:
-                newPaths.push([0, paths[paths.length - 1][1] + deltaY]);        
+                newPath.push([0, path[path.length - 1][1] + deltaY]);        
                 break;
                 
             case EntityDirection.north:
-                 newPaths.push([paths[paths.length - 1][0] + deltaX, 0]);
+                 newPath.push([path[path.length - 1][0] + deltaX, 0]);
                  break;
                  
             case EntityDirection.south:
-                newPaths.push([paths[paths.length - 1][0] + deltaX, this._mapHeight]);
+                newPath.push([path[path.length - 1][0] + deltaX, this._mapHeight]);
                 break;
         }
         
         
-        return newPaths;
+        return newPath;
     }    
 }
