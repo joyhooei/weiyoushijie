119ab30f3a5c0920455794a40b90efd6cdb36743
liweihai <liweihai@huawei.com>
2016-10-11 16:23:28 +0800

    Create Thunder.ts

---
+++ public/tower/src/sprites/bullets/Thunder.ts
@@ -0,0 +1,23 @@
+class Thunder extends Bullet {
+    protected _hitRadius: number;
+    
+    public constructor() {
+        super();
+        
+        this.addAllClips("thunder_moving", "moving")
+            .addAllClips("thunder_fighting", "fighting");        
+    }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+
+        this._hitRadius = this._get(properties, 'hitRadius', 200);
+    }
+    
+    protected _hitTarget() {
+        let enemies = application.battle.findEnemies(this.x, this.y, this._hitRadius, [0, 1]);
+        for (let i = 0; i < enemies.length; i++) {
+            enemies[i].shootBy(this);
+        }
+    }
+}
