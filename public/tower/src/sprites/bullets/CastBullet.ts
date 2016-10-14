8a1c1c0997cd7cf3385588b6593251892f9663ef
liweihai <liweihai@huawei.com>
2016-10-14 17:25:52 +0800

    Update CastBullet.ts

--- public/tower/src/sprites/bullets/CastBullet.ts
+++ public/tower/src/sprites/bullets/CastBullet.ts
@@ -38,18 +38,15 @@ class CastBullet extends Bullet {
 		this._targetX = targetX;
 		this._targetY = targetY;
 		
-		//运动轨迹最高点，跨度越大，顶点越高
-		var topY = Math.min(this._startY, this._targetY) - Math.abs(this._startX - this._targetX) / 3;
-		if (topY < 30) {
-		  	// 可能出现起点或者终点就是运动曲线顶点的情况
-		  	topY = Math.min(30, Math.min(this._startY, this._targetY));
+		if (this._targetX > this._startX) {
+			this._parabola(this._startX, this._startY, this._targetX, this._targetY);
+		} else if (this._targetX < this._startX) {
+			this._parabola(this._targetX, this._targetY, this._startX, this._startY);
+		} else {
+			this._a = 0;
+			this._b = 0;
+			this._c = 0;
 		}
-
-		var topX = Math.ceil((this._targetX - this._startX) * 2 / 3);
-
-		this._a = ((this._targetY - this._startY) / (this._targetX - this._startX) - (this._topY - this._startY) / (this._topX - this._startX)) / (this._targetX - this._topX);
-		this._b = ((this.targetY - this._startY) - this._a * (this._targetX * this._targetX - this._startX * this._startX) ) / (this._targetX - this._startX);
-		this._c = this._targetY - this._a * this._targetX * this._targetX - this._b * this._targetX;
 		
 		var distance = Math.sqrt(Math.pow(this._startY - this._targetY, 2) + Math.pow(this._startX - this._targetX, 2));
 		this._totalSteps = Math.ceil(distance / this._moveSpeed);		
@@ -57,12 +54,31 @@ class CastBullet extends Bullet {
 		this._steps = 0;
 		return this._totalSteps > 0;
     }
+
+	private  _parabola(x0: number, y0: number, x1: number, y1: number) {
+		//运动轨迹最高点，跨度越大，顶点越高
+		var y2 = Math.min(y0, y1) - Math.abs(x0 - x1) / 3;
+		if (y2 < 30) {
+		  	// 可能出现起点或者终点就是运动曲线顶点的情况
+		  	y2 = Math.min(30, Math.min(y0, y1));
+		}
+
+		var x2 = Math.ceil((x1 - x0) * 2 / 3);
+
+		this._a = ((y1 - y0) / (x1 - x0) - (y2 - y0) / (x2 - x0)) / (x1 - x2);
+		this._b = (y1 - y0) / (x1 - x0) - this._a * (x1 + x0);
+		this._c = y1 - this._a * x1 * x1 - this._b * x1;
+	}
     
     protected _moveOneStep():boolean {
 		if (this._steps < this._totalSteps) {
 			this.x = this._startX + (this._targetX - this._startX) * this._steps / this._totalSteps;
-			this.y = this._a * this.x * this.x + this._b * this.x + this._c;
-
+			if (this._a != 0) {
+				this.y = this._a * this.x * this.x + this._b * this.x + this._c;
+			} else {
+				this.y = this._startY + (this._targetY - this._startY) * this._steps / this._totalSteps;
+			}
+			
 			this._steps++;
 
 			return false;
