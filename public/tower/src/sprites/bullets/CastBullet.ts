ed5950dbe350b0f413633f15ce929215825e5deb
liweihai <liweihai@huawei.com>
2016-10-14 13:11:51 +0800

    Update CastBullet.ts

--- public/tower/src/sprites/bullets/CastBullet.ts
+++ public/tower/src/sprites/bullets/CastBullet.ts
@@ -37,32 +37,24 @@ class CastBullet extends Bullet {
 		this._targetY = targetY;
 		
 		//运动轨迹最高点，跨度越大，顶点越高
-		let vertexY = Math.min(this._startY, this._targetY) - Math.abs(this._startX - this._targetX) / 3;
-		if (vertexY < 10) {
+		this._b = Math.min(this._startY, this._targetY) - Math.abs(this._startX - this._targetX) / 3;
+		if (this._b < 10) {
         	// 可能出现起点或者终点就是运动曲线顶点的情况
-        	vertexY = Math.min(10, Math.min(this._startY, this._targetY));
+        	this._b = Math.min(10, Math.min(this._startY, this._targetY));
       	}
-		
-		let ratio = 0;
-		if (this._startY != vertexY) {
-        	ratio = (this._targetY - vertexY) / (this._startY - vertexY);
-		}
-        let vertexX = (ratio * this._targetX * this._targetX - this._startX * this._startX) / (ratio - 1);
 
-        // 特殊情况，出现顶点left==终点left，将曲率设置为0，做直线运动。
-        let curvature = 0;
-		if (this._targetX != vertexX) {
+        // 特殊情况，出现顶点x==终点x，将曲率设置为0，做直线运动。
+        this._a = 0;
+		if (this._targetX != this._startX) {
 			// a = (y - b ) / (x * x)
-			curvature = (this._targetY - vertexY) / Math.pow(this._targetX, 2);
+			this._a = (this._targetY - this._b) / Math.pow(this._targetX, 2);
 		}
 		
-		this._a = curvature;
-		this._b = vertexY;
 		this._steps = -1;
 		
         //移动距离和移动步数
-		let distance = Math.sqrt(Math.pow(this._startY - this._targetY, 2) + Math.pow(this._startX - this._targetX, 2));
-        this._totalSteps = Math.ceil(Math.min(Math.max(Math.log(distance) / 0.05 - 75, 30), 100) / this._moveSpeed);
+		let distance      = Math.sqrt(Math.pow(this._startY - this._targetY, 2) + Math.pow(this._startX - this._targetX, 2));
+        this._totalSteps = Math.ceil(distance / this._moveSpeed);
 		
 		return this._totalSteps > 0;
     }
@@ -74,7 +66,7 @@ class CastBullet extends Bullet {
 			if (this._a == 0) {
 				this.y = this._startY + (this._targetY - this._startY) * this._steps / this._totalSteps;
 			} else {
-				this.y = this._a * Math.pow(this.x, 2) + this._b;
+				this.y = this._a * this.x * this.x + this._b;
 			}
 
 			this._steps++;
