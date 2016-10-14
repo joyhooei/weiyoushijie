72c82de28ab3537572818ec8b8011867582041af
liweihai <liweihai@huawei.com>
2016-10-14 15:41:52 +0800

    Update CastBullet.ts

--- public/tower/src/sprites/bullets/CastBullet.ts
+++ public/tower/src/sprites/bullets/CastBullet.ts
@@ -2,11 +2,12 @@
 class CastBullet extends Bullet {
 	/**
 	 * ======================================================
-	 * 运动轨迹在页面中的y值可以抽象成函数 y = a * x * x + b;
+	 * 运动轨迹在页面中的y值可以抽象成函数 y = a * x * x + b * x + c;
 	 * ======================================================
 	*/
     protected _a: number;
 	protected _b: number;
+	protected _c: number;
 
 	protected _startX: number;
 	protected _startY: number;
@@ -23,6 +24,7 @@ class CastBullet extends Bullet {
 		
         this._a = 0;
 		this._b = 0;
+		this._c = 0;
     }
 
 	protected _idle() {
@@ -37,37 +39,29 @@ class CastBullet extends Bullet {
 		this._targetY = targetY;
 		
 		//运动轨迹最高点，跨度越大，顶点越高
-		this._b = Math.min(this._startY, this._targetY) - Math.abs(this._startX - this._targetX) / 3;
-		if (this._b < 10) {
-        	// 可能出现起点或者终点就是运动曲线顶点的情况
-        	this._b = Math.min(10, Math.min(this._startY, this._targetY));
-      	}
-
-        // 特殊情况，出现顶点x==终点x，将曲率设置为0，做直线运动。
-        this._a = 0;
-		if (this._targetX != this._startX) {
-			// a = (y - b ) / (x * x)
-			this._a = (this._targetY - this._b) / Math.pow(this._targetX, 2);
+		var topY = Math.min(this._startY, this._targetY) - Math.abs(this._startX - this._targetX) / 3;
+		if (topY < 20) {
+		  	// 可能出现起点或者终点就是运动曲线顶点的情况
+		  	topY = Math.min(20, Math.min(this._startY, this._targetY));
 		}
+
+		var topX = Math.ceil((this._targetX - this._startX) * 2 / 3);
+
+		this._a = ((this._targetY - this._startY) / (this._targetX - this._startX) - (this._topY - this._startY) / (this._topX - this._startX)) / (this._targetX - this._topX);
+		this._b = ((this.targetY - this._startY) - this._a * (this._targetX * this._targetX - this._startX * this._startX) ) / (this._targetX - this._startX);
+		this._c = this._targetY - this._a * this._targetX * this._targetX - this._b * this._targetX;
+		
+		var distance = Math.sqrt(Math.pow(this._startY - this._targetY, 2) + Math.pow(this._startX - this._targetX, 2));
+		this._totalSteps = Math.ceil(distance / 5);		
 		
 		this._steps = 0;
-		
-        //移动距离和移动步数
-		let distance      = Math.sqrt(Math.pow(this._startY - this._targetY, 2) + Math.pow(this._startX - this._targetX, 2));
-        this._totalSteps = Math.ceil(distance / this._moveSpeed);
-		
 		return this._totalSteps > 0;
     }
     
     protected _moveOneStep():boolean {
-		if (this._steps < this._totalSteps - 1) {
+		if (this._steps <= this._totalSteps) {
 			this.x = this._startX + (this._targetX - this._startX) * this._steps / this._totalSteps;
-			
-			if (this._a == 0) {
-				this.y = this._startY + (this._targetY - this._startY) * this._steps / this._totalSteps;
-			} else {
-				this.y = this._a * this.x * this.x + this._b;
-			}
+			this.y = this._a * this.x * this.x + this._b * this.x + this._c;
 
 			this._steps++;
 
