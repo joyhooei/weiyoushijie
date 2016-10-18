321522c7131d2b8f5f3db269774f016d041fb812
liweihai <liweihai@huawei.com>
2016-10-18 08:43:46 +0800

    Update ArrowTower2.ts

--- public/tower/src/sprites/towers/ArrowTower2.ts
+++ public/tower/src/sprites/towers/ArrowTower2.ts
@@ -7,11 +7,11 @@ class ArrowTower2 extends ArrowTower {
         this._bulletClaz = "Arrow1";
     }
 
-    protected getMuzzleX(): number {
+    public getMuzzleX(): number {
         return this.x + 25;
     }
 
-    protected getMuzzleY(): number {
+    public getMuzzleY(): number {
         return this.y + 7;
     }
 }
