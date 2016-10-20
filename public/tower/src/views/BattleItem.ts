99821327c3474454d96306c36684cde1aefc5668
liweihai <liweihai@huawei.com>
2016-10-20 10:23:03 +0800

    Update BattleItem.ts

--- public/tower/src/views/BattleItem.ts
+++ public/tower/src/views/BattleItem.ts
@@ -5,6 +5,7 @@ class BattleItem extends AbstractUI {
     
     public imgLock: eui.Image;
 	public imgBg: eui.Image;
+	public rcCover: eui.Rect;
 
     public constructor(stage:number) {
         super("battleItemSkin");
@@ -21,7 +22,18 @@ class BattleItem extends AbstractUI {
 
 	protected onRefresh() {
 		this.imgBg.source = "map_" + this._stage.toString() +  "_png";
+		
 		this.imgLock.visible = this._locked;
+		this.imgLock.x = this.imgBg.x + (this.imgBg.width - this.imgLock.width) / 2;
+		this.imgLock.y = this.imgBg.y + (this.imgBg.height - this.imgLock.height) / 2l
+		
+		if (this._locked) {
+			this.rcCover.width  = this.imgBg.width;
+			this.rcCover.height = this.imgBg.height;
+			this.rcCover.visible = true;
+		} else {
+			this.rcCover.visible = false;
+		}
 	}
     
     public unlock() {
