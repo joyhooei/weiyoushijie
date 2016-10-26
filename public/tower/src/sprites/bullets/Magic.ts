8bb64ff4eab836d03341d556165461199eea8b8f
liweihai <liweihai@huawei.com>
2016-10-26 11:51:30 +0800

    Update Magic.ts

--- public/tower/src/sprites/bullets/Magic.ts
+++ public/tower/src/sprites/bullets/Magic.ts
@@ -3,6 +3,23 @@ class Magic extends Bullet {
         super();
     }
     
+    protected _idle() {
+    	this._ticks ++;
+    	if (this._ticks >= this._idleTicks) {
+        	this.build();
+    	}
+    }
+    
+    protected _act() {
+        if (this._state == EntityState.building) {
+            this.move();
+            
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     protected _hitTarget() {
         if (this._target.active()) {
             this._target.shootBy(this);
