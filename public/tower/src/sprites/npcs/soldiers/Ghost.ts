8c0bae677a544a5cf07c34b8693f6a66dbcc796b
liweihai <liweihai@huawei.com>
2016-10-26 11:05:57 +0800

    Create Ghost.ts

---
+++ public/tower/src/sprites/npcs/soldiers/Ghost.ts
@@ -0,0 +1,10 @@
+class Ghost extends Soldier {
+    public constructor() {
+        super();
+        
+        this.addClip("ghost_east_fighting", ["east-fighting", "north-fighting", "south-fighting"])
+                .addClip("ghost_east_moving", ["east-moving", "north-moving", "south-moving"])
+                .addAllClips("ghost_guarding", "guarding")
+                .addAllClips("ghost_dying", "dying");
+    }
+}
