10f71e7a5864661bf8a41be834874f15a8e25292
liweihai <liweihai@huawei.com>
2016-10-26 11:59:59 +0800

    Update Magic2.ts

--- public/tower/src/sprites/bullets/Magic2.ts
+++ public/tower/src/sprites/bullets/Magic2.ts
@@ -3,7 +3,7 @@ class Magic2 extends Magic {
         super();
         
         this.addAllBitmaps("magic_moving_png", "moving")
-            .addAllClips("magic2_fighting_png", "fighting")
-            .addAllClips("magic2_building_png", "building");
+            .addAllBitmaps("magic2_fighting_png", "fighting")
+            .addAllClips("magic2_building", "building");
     }
 }
