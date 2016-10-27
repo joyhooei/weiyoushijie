f6c661d3405bb9f0a19b2c4adc3f093dd5f29a8c
liweihai <liweihai@huawei.com>
2016-10-27 11:46:47 +0800

    Update MagicTower4.ts

--- public/tower/src/sprites/towers/MagicTower4.ts
+++ public/tower/src/sprites/towers/MagicTower4.ts
@@ -1,4 +1,10 @@
-class MagicTower4 extends MagicTower {
+class MagicTower4 extends MagicTower implements SoldierCreator {
+    private _soldier : Soldier;
+    
+    private _skill1Level: number;
+    
+    private _skill2Level: number;
+    
     public constructor() {
         super();
         
@@ -6,7 +12,37 @@ class MagicTower4 extends MagicTower {
 
         this._bulletClaz = "Magic4";
     }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._skill1Level = 1;
+        this._skill2Level = 1;
+        
+        this._soldier = null;
+    }
+    
+    public guard() {
+        super.guard();
+        
+        if (!this._soldier) {
+            this._soldier = <Soldier>application.pool.get("BlackImpermanence", {guardX: this._guardX, guardY: this._guardY});
+            this._soldier.setCenterX(this.getMuzzleX());
+            this._soldier.setCenterY(this.getMuzzleY());
 
+            this._soldier.setCreator(this);
+
+            application.battle.addSoldier(this._soldier);
+        }
+    }
+    
+    public createSoldier(soldier: Soldier): Soldier {
+        let s = soldier.relive(application.frameRate * 12);
+        this._addSoldier(s);
+
+        return s;
+    }
+    
     public getMuzzleX(): number {
         return this.x + 40;
     }
