248e4b1215dc06d542b4bc33ce349a6673df430a
liweihai <liweihai@huawei.com>
2016-10-27 15:21:02 +0800

    Update Magic4.ts

--- public/tower/src/sprites/bullets/Magic4.ts
+++ public/tower/src/sprites/bullets/Magic4.ts
@@ -1,6 +1,4 @@
 class Magic4 extends Magic {
-   protected _hitRadius: number;
-
     public constructor() {
         super();
         
@@ -8,16 +6,10 @@ class Magic4 extends Magic {
             .addAllClips("magic4_fighting", "fighting")
             .addAllClips("magic4_building", "building");
     }
-     
-    public initialize(properties:any) {
-        super.initialize(properties);
-
-        this._hitRadius = this._get(properties, 'hitRadius', 50);
-    }
     
     protected _act():boolean {
         if (this._state == EntityState.fighting) {
-            let enemies = application.battle.findEnemies(this.getCenterX(), this.getBottomY(), this._hitRadius, [-1, 0, 1]);
+            let enemies = application.battle.getEnemies();
             for (let i = 0; i < enemies.length; i++) {
                 enemies[i].shootBy(this);
             }
