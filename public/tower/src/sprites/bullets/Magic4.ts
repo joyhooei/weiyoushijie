45a202d7ed80be575c358831423b7e5e66ae5f1e
liweihai <liweihai@huawei.com>
2016-10-27 09:15:00 +0800

    Update Magic4.ts

--- public/tower/src/sprites/bullets/Magic4.ts
+++ public/tower/src/sprites/bullets/Magic4.ts
@@ -6,4 +6,16 @@ class Magic4 extends Magic {
             .addAllClips("magic4_fighting", "fighting")
             .addAllClips("magic4_building", "building");
     }
+
+    protected _hitTarget() {
+        if (this._target.active()) {
+            if (this._target.shootBy(this)) {
+                //target is dead
+                let ghost:Ghost = <Ghost>application.pools.get("Ghost");
+                ghost.setCenterX(this._target.getCenterX());
+                ghost.setBottomY(this._target.getBottomY());
+                application.battle.addSoldier(ghost);
+            }
+        }
+    }   
 }
