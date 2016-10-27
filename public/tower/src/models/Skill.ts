92fd44f58e2cebed9da382c63f6e541098a60301
liweihai <liweihai@huawei.com>
2016-10-27 09:00:31 +0800

    Update Skill.ts

--- public/tower/src/models/Skill.ts
+++ public/tower/src/models/Skill.ts
@@ -8,4 +8,14 @@ class Skill {
     public save() {
         application.dao.save("Skill", this.attrs);
     }
+
+    public static getByClaz(skills: Skill[], claz: string): Skill {
+    	for(let i = 0; i < skills.length; i++) {
+    		if (skills[i].attrs.claz == claz) {
+    			return skills[i];
+    		}
+    	}
+    	
+    	return new Skill({level: 1});
+    }
 }
