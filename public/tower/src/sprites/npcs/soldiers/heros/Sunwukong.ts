d966648f667fc348e7811f200234fbefd3afd6bc
liweihai <liweihai@huawei.com>
2016-10-27 12:16:43 +0800

    Update Sunwukong.ts

--- public/tower/src/sprites/npcs/soldiers/heros/Sunwukong.ts
+++ public/tower/src/sprites/npcs/soldiers/heros/Sunwukong.ts
@@ -1,12 +1,14 @@
 class Sunwukong extends Hero {
     private _skill0Ticks: number;
 
-    private _skill1 : number;
-    private _skill1Times: number;
-    private _skill1Ticks: number;
+    //"腾云突击"
+    private _skill4 : number;
+    private _skill4Times: number;
+    private _skill4Ticks: number;
 
-    private _skill2 : number;
-    private _skill2Ticks: number;
+    //"猴毛"
+    private _skill5 : number;
+    private _skill5Ticks: number;
     
     public constructor() {
         super();
@@ -18,69 +20,78 @@ class Sunwukong extends Hero {
             .addClip("sunwukong_east_fighting_3", ["east-fighting", "south-fighting","north-fighting"])
             .addClip("sunwukong_dying", ["dying"]);
     }
+
+    public initialize(properties:any) {
+        super.initialize(properties);
+       
+        this._skill1Ticks = this._hitSpeed;
+        
+        this._skill4 = 2;
+        this._skill4Times = 0;
+        this._skill4Ticks = 8 * application.frameRate;
+        
+        this._skill5 = 1;
+        this._skill5Ticks = 10 * application.frameRate;
+    }
     
-    public setLegend(legend: Legend) {
-        let level = legend.attrs.level - 1;
-        
-        this._hp.setMaxHp(level * 15 + 30 + this._hp.getMaxHp()); 
-        this._armor += level * 5;
-        this._forceHigh += level * 2;
-        this._forceLow += level;
-        
-        this._force = 0;
-        this._skill = 0;
-        this._resistance = 10;
-        this._skill1 = 2;
-        this._skill1Times = 0;
-        this._skill2 = 1;
-        this._skill0Ticks = this._hitSpeed;
-        this._skill1Ticks = 8 * application.frameRate;
-        this._skill2Ticks = 10 * application.frameRate;
-        for (let i = 0; i < legend.skills.length; i++) {
-            let skill = legend.skills[i];
+    public setSkill(skills: Skill[]) {
+        for (let i = 0; i < skills.length; i++) {
+            let skill = skills[i];
             
-            if (legend.skills[i].attrs.name == "重击") {
-                this._force = 2 * skill.attrs.level;
-            } else if (legend.skills[i].attrs.name == "荆棘甲") {
+            if (skill.attrs.idx == 0) {
+                let level = skill.attrs.level - 1;
+        
+                this._hp.setMaxHp(level * 15 + 30 + this._hp.getMaxHp()); 
+                this._armor     += level * 5;
+                this._forceHigh += level * 2;
+                this._forceLow  += level;
+            } else if (skill.attrs.idx == 1) {
+                //"重击"
+                this._extraForce = 2 * skill.attrs.level;
+            } else if (skill.attrs.idx == 2) {
+                //"荆棘甲"
                 this._resistance = 10 +  (skill.attrs.level - 1) * 20;
-            } else if (legend.skills[i].attrs.name == "金刚不坏") {
+            } else if (skill.attrs.idx == 3) {
+                //"金刚不坏"
                 this._hp.setMaxHp((skill.attrs.level - 1) * 30 + this._hp.getMaxHp()); 
-            } else if (legend.skills[i].attrs.name == "腾云突击") {
-                this._skill1 = 2 * skill.attrs.level;
-                this._skill1Ticks = (9 - skill.attrs.level) * application.frameRate;
-            } else if (legend.skills[i].attrs.name == "猴毛") {
-                this._skill2 = skill.attrs.level;
+            } else if (skill.attrs.idx == 4) {
+                //"腾云突击"
+                this._skill4 = 2 * skill.attrs.level;
+                this._skill4Ticks = (9 - skill.attrs.level) * application.frameRate;
+            } else if (skill.attrs.idx == 5) {
+                //"猴毛"
+                this._skill5 = skill.attrs.level;
             }
         }
     }
 
     protected _readyFight() {
         this._skill0Ticks --;
-        this._skill1Ticks --;
-        this._skill2Ticks --;
+        this._skill4Ticks --;
+        this._skill5Ticks --;
         
         //正在腾云突击中
-        if (this._skill == 1 && this._skill1Times < this._skill1 && this._enemy && this._enemy.active()) {
+        if (this._curSkill == 4 && this._skill4Times < this._skill4 && this._enemy && this._enemy.active()) {
             return true;
         }
                 
-        if (this._skill2Ticks <= 0) {
+        if (this._skill5Ticks <= 0) {
              //猴毛冷却:10秒
-            this._skill = 2;
+            this._curSkill = 5;
             
-            this._skill2Ticks = 10 * application.frameRate;
+            this._skill5Ticks = 10 * application.frameRate;
             
             return true;
-        } else if (this._skill1Ticks <= 0) {
+        } else if (this._skill4Ticks <= 0) {
             //腾云突击冷却:8秒，7秒，6秒
-            this._skill = 1;
+            this._curSkill = 4;
             
-            this._skill1Ticks = (9 - this._skill1 / 2) * application.frameRate;
-            this._skill1Times = 0;
+            this._skill4Ticks = (9 - this._skill4 / 2) * application.frameRate;
+            this._skill4Times = 0;
             
             return true;
         } else if (this._skill0Ticks <= 0) {
-            this._skill == 0;
+            this._curSkill == 0;
             
             this._skill0Ticks = this._hitSpeed;
             
@@ -91,13 +102,13 @@ class Sunwukong extends Hero {
     }
 
     protected _hitOpponents() {
-        if (this._skill == 2) {
-            for(let i = 0; i < this._skill2; i++) {
-                application.battle.addWarriorsByName("Warrior", this, {maxHp: 60 + (this._skill2 - 1) * 40, guardRadius: this._guardRadius, liveTicks: 8 * application.frameRate});
+        if (this._curSkill == 5) {
+            for(let i = 0; i < this._skill5; i++) {
+                application.battle.addWarriorsByName("Warrior", this, {maxHp: 60 + (this._skill5 - 1) * 40, guardRadius: this._guardRadius, liveTicks: 8 * application.frameRate});
             }
         } else {
-            if (this._skill == 1) {
-                this._skill1Times ++;
+            if (this._curSkil4 == 1) {
+                this._skill4Times ++;
             }
 
             super._hitOpponents();
