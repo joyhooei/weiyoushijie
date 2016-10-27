d0f0390422aa51d68cee3cd2727b96e1f6ab5eea
liweihai <liweihai@huawei.com>
2016-10-27 15:06:10 +0800

    Delete Magic5.ts

--- public/tower/src/sprites/bullets/Magic5.ts
+++
@@ -1,9 +0,0 @@
-class Magic5 extends Magic {
-    public constructor() {
-        super();
-        
-        this.addAllBitmaps("magic5_moving_png", "moving")
-            .addAllClips("magic5_fighting", "fighting")
-            .addAllClips("magic5_building", "building");
-    }
-}
