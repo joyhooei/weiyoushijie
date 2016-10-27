160ac687e886346428bbaa2d60477d93dfc4fc04
liweihai <liweihai@huawei.com>
2016-10-27 15:36:35 +0800

    Update Magic4.ts

--- public/tower/src/sprites/bullets/Magic4.ts
+++ public/tower/src/sprites/bullets/Magic4.ts
@@ -6,15 +6,4 @@ class Magic4 extends Magic {
             .addAllClips("magic4_fighting", "fighting")
             .addAllClips("magic4_building", "building");
     }
-    
-    protected _act():boolean {
-        if (this._state == EntityState.fighting) {
-            let enemies = application.battle.getEnemies();
-            for (let i = 0; i < enemies.length; i++) {
-                enemies[i].shootBy(this);
-            }
-        }
-
-        return super._act();
-    }
 }
