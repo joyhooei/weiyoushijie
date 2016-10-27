c99ae9c732915c5fdb52c02bc342cb03b723d26d
liweihai <liweihai@huawei.com>
2016-10-27 15:38:57 +0800

    Create BlackWater.ts

---
+++ public/tower/src/sprites/bullets/BlackWater.ts
@@ -0,0 +1,27 @@
+class BlackWater extends Magic {
+    protected _hitRadius: number;
+    
+    public constructor() {
+        super();
+        
+        this.addAllClips("blackwater_fighting", "fighting")
+            .addAllClips("blackwater_building", "building");
+    }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+
+        this._hitRadius = this._get(properties, 'hitRadius', 50);
+    }
+    
+    protected _act():boolean {
+        if (this._state == EntityState.fighting) {
+            let enemies = application.battle.findEnemies(this.getCenterX(), this.getCenterY(), this._hitRadius, [0, 1]);
+            for (let i = 0; i < enemies.length; i++) {
+                enemies[i].shootBy(this);
+            }
+        }
+
+        return super._act();
+    }
+}
