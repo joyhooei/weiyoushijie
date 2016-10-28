70807f376264cca305461ea4553acd30075c4adc
liweihai <liweihai@huawei.com>
2016-10-28 12:15:48 +0800

    Update Hogs.ts

--- public/tower/src/sprites/npcs/enemies/Hogs.ts
+++ public/tower/src/sprites/npcs/enemies/Hogs.ts
@@ -2,7 +2,7 @@ class Hogs extends Enemy {
     public constructor() {
         super();
         
-        this.addClip("hogs_east_moving", ["east-moving", "east-guarding"])
+        this.addClip("hogs_east_moving", ["east-moving", "south-moving", "east-guarding", "south-guarding"])
             .addClip("hogs_north_moving", ["north-moving", "north-guarding"])
             .addClip("hogs_dying", "dying")
             .addClip("hogs_east_fighting", ["east-fighting", "south-fighting","north-fighting"]);    
