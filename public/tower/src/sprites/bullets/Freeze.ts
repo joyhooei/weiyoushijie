752186988755fed62e44ca89026ceed67b19d936
liweihai <liweihai@huawei.com>
2016-10-29 17:11:29 +0800

    Update Freeze.ts

--- public/tower/src/sprites/bullets/Freeze.ts
+++ public/tower/src/sprites/bullets/Freeze.ts
@@ -8,7 +8,7 @@ class Freeze extends Bullet {
     protected _hitTarget() {
         let enemies = application.battle.getEnemies();
         for (let i = 0; i < enemies.length; i++) {
-            enemies[i].frozen(this.getForce());
+            enemies[i].frozen(this.getForce(), application.frameRate << 2);
         }
     }
 }
