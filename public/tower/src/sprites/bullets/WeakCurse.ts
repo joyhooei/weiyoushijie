055e59bc02de72c7ff48a0e65a3186d4f0d9c1b5
liweihai <liweihai@huawei.com>
2016-10-31 15:53:53 +0800

    Update WeakCurse.ts

--- public/tower/src/sprites/bullets/WeakCurse.ts
+++ public/tower/src/sprites/bullets/WeakCurse.ts
@@ -1,16 +1,11 @@
-class WeakCurse extends Bullet {
-    protected _hitRadius: number;
-    
-    public initialize(properties:any) {
-        super.initialize(properties);
-
-        this._hitRadius = this._get(properties, 'hitRadius', 50);
+class WeakCurse extends Curse {
+    public constructor() {
+        super();
+        
+        this.addClip("weakcurse_dying");        
     }
     
-    protected _hitTarget() {
-        let enemies = application.battle.findEnemies(this.getCenterX(), this.getCenterY(), this._hitRadius, [-1, 0, 1]);
-        for (let i = 0; i < enemies.length; i++) {
-            enemies[i].curse(this.getForce(), this._fightingTicks);
-        }
+    protected _hitEnemy(enemy: Enemy, force: number, ticks: number) {
+        enemy.weak(force, ticks);
     }
 }
