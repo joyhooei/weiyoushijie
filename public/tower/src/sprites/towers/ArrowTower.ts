1353814d1061f03ffa94ca803ed21481184b1e95
liweihai <liweihai@huawei.com>
2016-10-31 12:03:52 +0800

    Update ArrowTower.ts

--- public/tower/src/sprites/towers/ArrowTower.ts
+++ public/tower/src/sprites/towers/ArrowTower.ts
@@ -1,3 +1,5 @@
 class ArrowTower extends ShootTower {
-
+    protected _findEnemy(): Enemy {
+        return application.battle.findEnemy(this.getCenterX(), this.getCenterY(), this._guardRadius, [0, 1]);
+    }
 }
