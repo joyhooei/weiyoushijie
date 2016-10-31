5c413aef6e1b95a479963ae1efa1b60d9eda3fb8
liweihai <liweihai@huawei.com>
2016-10-31 15:37:44 +0800

    Create MiscastCurse.ts

---
+++ public/tower/src/sprites/bullets/MiscastCurse.ts
@@ -0,0 +1,16 @@
+class MiscastCurse extends Bullet {
+    protected _hitRadius: number;
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+
+        this._hitRadius = this._get(properties, 'hitRadius', 50);
+    }
+    
+    protected _hitTarget() {
+        let enemies = application.battle.findEnemies(this.getCenterX(), this.getCenterY(), this._hitRadius, [-1, 0, 1]);
+        for (let i = 0; i < enemies.length; i++) {
+            enemies[i].miscast(this.getForce(), this._fightingTicks);
+        }
+    }
+}
