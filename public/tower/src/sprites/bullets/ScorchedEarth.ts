dffe7906cf174da26d1dc618250f41d4e6d0f3ac
liweihai <liweihai@huawei.com>
2016-10-31 10:44:05 +0800

    Update ScorchedEarth.ts

--- public/tower/src/sprites/bullets/ScorchedEarth.ts
+++ public/tower/src/sprites/bullets/ScorchedEarth.ts
@@ -1,25 +1,18 @@
 class ScorchedEarth extends Bomb {
-    protected _hitRadius: number;
-    
     public constructor() {
         super();
         
         this.addClip("scorchedearth_fighting");
     }
     
-    public initialize(properties:any) {
-        super.initialize(properties);
-
-        this._hitRadius = this._get(properties, 'hitRadius', 50);
-    }
-    
     protected _hitTarget() {
         let x = this.getCenterX();
         let y = this.getCenterY();
         
-        Bullet.throw(x, y, x, y - this._hitRadius, "ScorchedEarth", {hitRadius: hitRadius});
-        Bullet.throw(x, y, x, y + this._hitRadius, "ScorchedEarth", {hitRadius: hitRadius});
-        Bullet.throw(x, y, x - this._hitRadius, y, "ScorchedEarth", {hitRadius: hitRadius});
-        Bullet.throw(x, y, x + this._hitRadius, y, "ScorchedEarth", {hitRadius: hitRadius});
+        let radius = hitRadius >> 1;
+        Bullet.throw(x, y, x, y - radius, "ScorchedEarth", {hitRadius: radius});
+        Bullet.throw(x, y, x, y + radius, "ScorchedEarth", {hitRadius: radius});
+        Bullet.throw(x, y, x - radius, y, "ScorchedEarth", {hitRadius: radius});
+        Bullet.throw(x, y, x + radius, y, "ScorchedEarth", {hitRadius: radius});
     }
 }
