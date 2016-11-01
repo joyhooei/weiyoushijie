0452d8e46cf103d676e8d9ac94a6b71e47c41744
liweihai <liweihai@huawei.com>
2016-11-01 15:14:31 +0800

    Update SoldierTower4.ts

--- public/tower/src/sprites/towers/SoldierTower4.ts
+++ public/tower/src/sprites/towers/SoldierTower4.ts
@@ -6,6 +6,10 @@ class SoldierTower4 extends SoldierTower {
         
         this._soldierClaz = "Soldier4";
     }
+    
+    public skillUpgradable(skill:number): boolean {
+        return false;
+    }
 
     public upgradable(): boolean {
         return false;
