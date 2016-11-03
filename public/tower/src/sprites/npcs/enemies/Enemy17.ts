78332576d32e025780565e429b26af11e745b0fc
liweihai <liweihai@huawei.com>
2016-11-03 09:39:38 +0800

    Create Enemy17.ts

---
+++ public/tower/src/sprites/npcs/enemies/Enemy17.ts
@@ -0,0 +1,11 @@
+class Enemy17 extends Enemy {
+    public constructor() {
+        super();
+        
+        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy1_dying", "east-dying")
+            .addClip("enemy1_east_fighting", "east-fighting");
+    }
+}
