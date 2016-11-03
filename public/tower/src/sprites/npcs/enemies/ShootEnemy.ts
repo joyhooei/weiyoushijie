8ac2449e098d4b64cd3d4bd3e8c16632bf13803a
liweihai <liweihai@huawei.com>
2016-11-03 10:30:42 +0800

    Update ShootEnemy.ts

--- public/tower/src/sprites/npcs/enemies/ShootEnemy.ts
+++ public/tower/src/sprites/npcs/enemies/ShootEnemy.ts
@@ -2,4 +2,19 @@ class ShootEnemy extends Enemy implements Shooter {
     public constructor() {
         super();
     }
+    
+    public targetKilled(target:NPC) {
+    }
+    
+    public getMuzzleX(): number {
+        it (this._target.x > this.x) {
+            return this.x + this.width;
+        } else {
+            return this.x;
+        }
+    }
+    
+    public getMuzzleY(): number {
+        return this.y;
+    }
 }
