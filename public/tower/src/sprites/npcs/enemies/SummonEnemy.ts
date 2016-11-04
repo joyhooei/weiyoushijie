b21339198c66f3846c18ee3b750e4bc79116f110
liweihai <liweihai@huawei.com>
2016-11-04 17:27:55 +0800

    Create SummonEnemy.ts

---
+++ public/tower/src/sprites/npcs/enemies/SummonEnemy.ts
@@ -0,0 +1,22 @@
+abstract class SummonEnemy extends Enemy {
+    protected _summonTicks: number;
+    protected _summonClaz: string;
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._summonTicks = this._get(properties, "summonTicks", 5 * application.frameRate);
+        this._summonClaz  = this._get(properties, "summonClaz", "Enemy27");
+    }
+    
+    protected _moving() {
+        super._moving();
+        
+        this._ticks++;
+        if (this._ticks % this._summonTicks == 0) {
+            this._summon(this._summonclaz);
+        }
+    }
+    
+    abstract _summon(claz: string): Enemy;
+}
