c24507b29b649922cb3bb32e55919fbe4b84d0e1
liweihai <liweihai@huawei.com>
2016-11-04 16:06:23 +0800

    Update Enemy9.ts

--- public/tower/src/sprites/npcs/enemies/Enemy9.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy9.ts
@@ -8,4 +8,13 @@ class Enemy9 extends Enemy {
             .addClip("enemy1_dying", "east-dying")
             .addClip("enemy1_east_fighting", "east-fighting");
     }
+    
+    protected _hitOpponents() {
+        super._hitOpponents();
+        
+        let s = this.firstSoldier();
+    	if (s) {
+            s.black(5, 2 * application.frameRate, false);
+    	}
+    }    
 }
