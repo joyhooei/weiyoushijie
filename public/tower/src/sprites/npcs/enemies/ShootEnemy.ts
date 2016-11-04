1e95e26863bdc82a17f47f6930570a8efb5df463
liweihai <liweihai@huawei.com>
2016-11-04 15:03:06 +0800

    Update ShootEnemy.ts

--- public/tower/src/sprites/npcs/enemies/ShootEnemy.ts
+++ public/tower/src/sprites/npcs/enemies/ShootEnemy.ts
@@ -1,6 +1,14 @@
 class ShootEnemy extends Enemy implements Shooter {
+    protected _shootRadius: number;
+    
     public constructor() {
         super();
+    }
+     
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+		this._shootRadius = this._get(properties, "shootRadius", 50);
     }
     
     public targetKilled(target:NPC) {
@@ -16,5 +24,28 @@ class ShootEnemy extends Enemy implements Shooter {
     
     public getMuzzleY(): number {
         return this.y;
+    }
+    
+    protected _moving() {
+        let soldier = this._findSoldier();
+        if (soldier) {
+            this.addSoldier(soldier);
+            this.fight();
+        } else {
+            super._moving();
+        }
+    }
+    
+    protected _hitOpponents() {
+        let soldier = this.application.battle.findSuitableSoldier(this.getCenterX(), this.getCenterY(), this._shootRadius);
+        if (soldier && soldier.active()) {
+            this._shoot(soldier);
+        } else {
+            this.rmvSoldier(soldier);
+        }
+    }
+    
+    protected _shoot(soldier: Soldier) {
+        Bullet.shoot(this, soldier, "Arrow1" , {force: this.getForce()});
     }
 }
