c8b584911f1c2911fd961bdeae3920fa9f1c3e91
liweihai <liweihai@huawei.com>
2016-11-07 17:04:49 +0800

    Update Enemy32.ts

--- public/tower/src/sprites/npcs/enemies/Enemy32.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy32.ts
@@ -1,11 +1,48 @@
 class Enemy32 extends Enemy {
+    protected _stealthTicks:number;
+
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy32_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy32_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy32_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy32_dying", "east-dying")
+            .addClip("enemy32_east_fighting", "east-fighting");
+    }
+    
+    public update():boolean {
+        this._stealthTicks --;
+        if (this._stealthTicks > 0) {
+            let lastPath = this._paths[this._paths.length - 1];
+            let distance = (this.x - lastPath[0]) * (this.x - lastPath[0]) + (this.y - lastPath[1]) * (this.y - lastPath[1]);
+            if (distance <= 100) {
+                this._altitude = 0;
+            }
+        } else if (this._stealthTicks == 0) {
+             this._altitude = 0;
+        }
+        
+        return super.update();
+    }
+
+    public damage(d: number): boolean {
+        if (this.active()) {           
+            let oldHp = this._hp.getHp();
+            if (this._hp.damage(d)) {
+                this.kill();
+                
+                return true;
+            } else {
+                if (oldHp > ((this._hp.getMaxHp() << 1)) && this._hp.getHp() <= (this._hp.getMaxHp() << 1)) {
+                    this._stealthTicks = 8 * application.frameRate;
+                    this._altitude = -1;
+                }
+                
+                return false;
+            }
+        } else {
+            return true;
+        }
     }
 }
