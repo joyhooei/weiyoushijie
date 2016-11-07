4822512dd063483c677d9c090fdf9db0ecb0dfb0
liweihai <liweihai@huawei.com>
2016-11-07 10:05:07 +0800

    Update Enemy18.ts

--- public/tower/src/sprites/npcs/enemies/Enemy18.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy18.ts
@@ -1,22 +1,19 @@
-class Enemy18 extends Enemy {
+class Enemy18 extends GroupEnemy {
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy18_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy18_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy18_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy18_dying", "east-dying")
+            .addClip("enemy18_east_fighting", "east-fighting");
     }
-    
-    //攻击接触到的士兵
-    protected _hitOpponents() {
-        let soldiers = application.battle.findSoldiers(this.getCenterX(), this.getCenterY(), 50, [0]);
-        for(let i = 0; i < soldiers.length; i++) {
-            let s = soldiers[i];
-            if (s.hitBy(this)) {
-                this.rmvSoldier(s);
-            }
-        }
-    }    
+
+    protected _enterGroup(enemy: Enemy) {
+        enemy.incArm(80);
+    }
+
+    protected _leaveGroup(enemy: Enemy) {
+        enemy.incArm(-80);
+    }   
 }
