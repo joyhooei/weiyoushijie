9e0527fcb11f7b725ad83a9de0a811495f57e520
liweihai <liweihai@huawei.com>
2016-11-07 16:49:21 +0800

    Update Enemy35.ts

--- public/tower/src/sprites/npcs/enemies/Enemy35.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy35.ts
@@ -1,11 +1,31 @@
 class Enemy35 extends Enemy {
+    protected _summonTicks: number;
+
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy33_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy33_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy33_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy33_dying", "east-dying")
+            .addClip("enemy33_east_fighting", "east-fighting");
     }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._summonTicks = this._get(properties, "summonTicks", 5 * application.frameRate);
+    }
+    
+    protected _moving() {
+        super._moving();
+        
+        this._ticks++;
+        if (this._ticks % this._summonTicks == 0) {
+            let pos = this._ahead(10);
+            this._born("Enemy29", pos[0], pos[1]);
+            this._born("Enemy32", pos[0], pos[1]);
+            this._born("Enemy33", pos[0], pos[1]);
+        }
+    }      
 }
