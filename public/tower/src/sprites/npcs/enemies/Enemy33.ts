9b7802c0fc1c13b9caa5ef2c4143bf71b1b9b831
liweihai <liweihai@huawei.com>
2016-11-07 16:47:06 +0800

    Update Enemy33.ts

--- public/tower/src/sprites/npcs/enemies/Enemy33.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy33.ts
@@ -1,11 +1,32 @@
 class Enemy33 extends Enemy {
+    protected _flashTicks: number;
+
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy33_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy33_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy33_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy33_dying", "east-dying")
+            .addClip("enemy33_east_fighting", "east-fighting");
+    }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._flashTicks = application.frameRate;
+    }
+    
+    protected _moving() {
+        this._flashTicks--;
+        if (this._flashTicks <= 0) {
+            for(i = 0; i < 10; i++) {
+                if (this.active()) {
+                    super._moving();
+                }
+            }
+            
+            this._flashTicks = 5 * application.frameRate;
+        }
     }
 }
