528465a6e06a05977d149fda4496c37f9d1cd43f
liweihai <liweihai@huawei.com>
2016-11-07 10:29:38 +0800

    Update Enemy19.ts

--- public/tower/src/sprites/npcs/enemies/Enemy19.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy19.ts
@@ -1,11 +1,38 @@
-class Enemy19 extends ShootEnemy {
+class Enemy19 extends GroupEnemy {
+    protected _cureTicks: number;
+
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy19_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy19_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy19_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy19_dying", "east-dying")
+            .addClip("enemy19_east_fighting", "east-fighting");
+    }
+
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._cureTicks = application.frameRate;
+    }
+     
+    public update(): boolean {
+        this._cureTicks --;
+        if (this._cureTicks <= 0) {
+            for(let i = 0; i < this._group; i++) {
+                this._group[i].addHp(20);
+            }
+            
+            this._cureTicks = application.frameRate;
+        }
+        
+        return super.update();
+    }
+
+    protected _enterGroup(enemy: Enemy) {
+    }
+
+    protected _leaveGroup(enemy: Enemy) {
     }
 }
