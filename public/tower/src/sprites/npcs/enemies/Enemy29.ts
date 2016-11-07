d2fd2de4774959a06e9177f7798902237a754cc8
liweihai <liweihai@huawei.com>
2016-11-07 12:23:02 +0800

    Update Enemy29.ts

--- public/tower/src/sprites/npcs/enemies/Enemy29.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy29.ts
@@ -1,11 +1,25 @@
 class Enemy29 extends Enemy {
+    protected _initialMoveSpeed: number;
+
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy29_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy29_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy29_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy29_dying", "east-dying")
+            .addClip("enemy29_east_fighting", "east-fighting");
+    }
+    
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._initialMoveSpeed = this._moveSpeed;
+    }    
+    
+    public damage(d: number): boolean {
+        this._moveSpeed = Math.round((1 - this._hp.getHp() / this._hp.getMaxHp()) * 2 * this._initialMoveSpeed);
+        
+        return super.damage(d);
     }
 }
