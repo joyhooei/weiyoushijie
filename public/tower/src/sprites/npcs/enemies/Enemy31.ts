91751e9993c07ee78d242929014531c76da8ba8a
liweihai <liweihai@huawei.com>
2016-11-07 16:38:51 +0800

    Update Enemy31.ts

--- public/tower/src/sprites/npcs/enemies/Enemy31.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy31.ts
@@ -2,10 +2,41 @@ class Enemy31 extends ShootEnemy {
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy31_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy31_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy31_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy31_dying", "east-dying")
+            .addClip("enemy31_east_fighting", "east-fighting");
+    }
+    
+    protected _hitOpponents() {
+        let s = this.firstSoldier();
+    	if (s) {
+            if (Math.random() <= 0.1) {
+                if (s.getParentClaz() == "Hero") {
+                    if (s.damage(100)) {
+                        this.rmvSoldier(s);
+                    }
+                } else {
+                    s.kill();
+                    this.rmvSoldier(s);
+                }
+            } else {
+                if (s.hitBy(this)) {
+                    this.rmvSoldier(s);
+                }
+            }
+    	} else {
+    		this._moveAgain();
+    	}
+    }
+    
+    public kill() {
+        super.kill();
+        
+        let soldiers = application.battle.findSoldiers(this.getCenterX(), this.getCenterY(), 80);
+        for(let i = 0; i < soldiers; i++) {
+            soldiers[i].damage(50);
+        }
     }
 }
