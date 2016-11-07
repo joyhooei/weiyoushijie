4d87ca9f079f98f47fe266b5865fbf312d6220e9
liweihai <liweihai@huawei.com>
2016-11-07 10:24:18 +0800

    Update Enemy21.ts

--- public/tower/src/sprites/npcs/enemies/Enemy21.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy21.ts
@@ -1,11 +1,20 @@
-class Enemy21 extends Enemy {
+class Enemy22 extends GroupEnemy {
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy22_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy22_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy22_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy22_dying", "east-dying")
+            .addClip("enemy22_east_fighting", "east-fighting");
+    }
+
+    protected _enterGroup(enemy: Enemy) {
+    }
+
+    protected _leaveGroup(enemy: Enemy) {
+        if (!enemy.active()) {
+            this._born("Enemy22", enemy.getCenterX(), enemy.getCenterY());
+        }
     }
 }
