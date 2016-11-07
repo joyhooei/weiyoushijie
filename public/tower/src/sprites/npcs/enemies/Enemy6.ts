80720d582d6acf7d8401706945cc27c6282346c5
liweihai <liweihai@huawei.com>
2016-11-07 17:05:40 +0800

    Update Enemy6.ts

--- public/tower/src/sprites/npcs/enemies/Enemy6.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy6.ts
@@ -2,11 +2,11 @@ class Enemy6 extends Enemy {
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy6_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy6_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy6_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy6_dying", "east-dying")
+            .addClip("enemy6_east_fighting", "east-fighting");
     }
     
     public erase() {
