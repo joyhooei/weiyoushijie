90ff39fd517d1944c01a245bbafd0a01a3e9d470
liweihai <liweihai@huawei.com>
2016-11-07 09:39:13 +0800

    Update Enemy23.ts

--- public/tower/src/sprites/npcs/enemies/Enemy23.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy23.ts
@@ -2,10 +2,21 @@ class Enemy23 extends Enemy {
     public constructor() {
         super();
         
-        this.addClip("enemy1_east_moving", ["east-moving", "east-guarding"])
-            .addClip("enemy1_south_moving", ["south-moving", "south-guarding"])
-            .addClip("enemy1_north_moving", ["north-moving", "north-guarding"])
-            .addClip("enemy1_dying", "east-dying")
-            .addClip("enemy1_east_fighting", "east-fighting");
+        this.addClip("enemy23_east_moving", ["east-moving", "east-guarding"])
+            .addClip("enemy23_south_moving", ["south-moving", "south-guarding"])
+            .addClip("enemy23_north_moving", ["north-moving", "north-guarding"])
+            .addClip("enemy23_dying", "east-dying")
+            .addClip("enemy23_east_fighting", "east-fighting");
     }
+    
+    //攻击接触到的士兵
+    protected _hitOpponents() {
+        let soldiers = application.battle.findSoldiers(this.getCenterX(), this.getCenterY(), 50, [0]);
+        for(let i = 0; i < soldiers.length; i++) {
+            let s = soldiers[i];
+            if (s.hitBy(this)) {
+                this.rmvSoldier(s);
+            }
+        }
+    }    
 }
