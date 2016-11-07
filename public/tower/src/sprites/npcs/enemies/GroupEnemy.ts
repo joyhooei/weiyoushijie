2596db19db428859364d2268f4cb5412c144c129
liweihai <liweihai@huawei.com>
2016-11-07 10:04:51 +0800

    Create GroupEnemy.ts

---
+++ public/tower/src/sprites/npcs/enemies/GroupEnemy.ts
@@ -0,0 +1,56 @@
+abstract class GroupEnemy extends Enemy {
+    protected _group: Enemy[];
+
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._group = [];
+    }
+
+    private _existed(enemies: Enemy[], enemy: Enemy): boolean {
+        for(let j = 0; j < enemies; j++) {
+            if (e == enemies[j]) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public update(): boolean {
+        if (this.active()) {
+            let enemies = application.battle.findEnemies(this.getCenterX(), this.getCenterY(), 80, [-1, 0, 1]);
+            
+            for(let i = 0; i < enemies; i++) {
+                let e = enemies[i];
+                if (!this._existed(this._group, e)) {
+                     this._enterGroup(e);
+                }
+            }
+            
+            for(let i = 0; i < this._group; i++) {
+                let e = this._group[i];
+                if (!this._existed(enemies, e)) {
+                    if (e.active()) {
+                        this._leaveGroup(e);
+                    }
+                }
+            }
+            
+            this._group = enemies;
+        } else {
+            for(let i = 0; i < this._nearbyEnemies; i++) {
+                if (this._nearbyEnemies[i].active()) {
+                    this._leaveGroup(this._nearbyEnemies[i]);
+                }
+            }
+            
+            this._group = [];
+        }
+        
+        return super.update();
+    }
+
+    protected abstract _enterGroup(enemy: Enemy);
+    protected abstract _leaveGroup(enemy: Enemy);
+}
