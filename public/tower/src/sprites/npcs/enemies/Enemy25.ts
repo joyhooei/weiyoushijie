f619c11a8c06fdd10b9b2ad4ed99c384e8c6d988
liweihai <liweihai@huawei.com>
2016-11-08 09:00:01 +0800

    Update Enemy25.ts

--- public/tower/src/sprites/npcs/enemies/Enemy25.ts
+++ public/tower/src/sprites/npcs/enemies/Enemy25.ts
@@ -1,4 +1,6 @@
 class Enemy25 extends Enemy {
+    protected _hitTicks: number;
+
     public constructor() {
         super();
         
@@ -9,5 +11,30 @@ class Enemy25 extends Enemy {
             .addClip("enemy25_east_fighting", "east-fighting");
     }
 
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        this._hitTicks = 0;
+    }
+
     //如果在他攻击一名士兵，这个士兵就无法移动或者攻击，如果5秒钟内没有杀死他, 士兵会死亡，并且转变为e21
+    protected _hitOpponents() {
+        let s = this.firstSoldier();
+    	if (s) {
+            this._hitTicks++;
+            if (this._hitTicks >= 5 * application.frameRate) {
+                s.kill();
+                this._born("Enemy21", s.getCenterX(), s.getBottomY());
+                
+                this._hitTicks = 0;
+            } else {
+                s.frozen(0, 10, false, false);
+                if (s.hitBy(this)) {
+                    this.rmvSoldier(s);
+                }
+            }
+    	} else {
+    		this._moveAgain();
+    	}
+    }    
 }
