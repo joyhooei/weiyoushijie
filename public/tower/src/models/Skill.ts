0823f5c65a259f5633b203cc2a9cf6ccc09b5dbb
liweihai <liweihai@huawei.com>
2016-11-08 19:58:04 +0800

    Update Skill.ts

--- public/tower/src/models/Skill.ts
+++ public/tower/src/models/Skill.ts
@@ -5,8 +5,8 @@ class Skill {
         this.attrs = attrs;
     }
 
-    public save() {
-        application.dao.save("Skill", this.attrs);
+    public save(): Q.Promise<any> {
+        return application.dao.save("Skill", this.attrs);
     }
 
 	public static get(skills: Skill[], claz:string):Skill {
