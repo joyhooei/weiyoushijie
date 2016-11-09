fac58bf3a100d4d1ec58cd18dd6151c6ea5c757b
liweihai <liweihai@huawei.com>
2016-11-09 09:47:06 +0800

    Update ArrowTower.ts

--- public/tower/src/sprites/towers/ArrowTower.ts
+++ public/tower/src/sprites/towers/ArrowTower.ts
@@ -1,4 +1,30 @@
 class ArrowTower extends ShootTower {
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        if (this._skill) {
+            if (this._skill.attris.level == 1) {
+                this._guardRadius = Math.round(1.05 * this._guardRadius);
+            }
+
+            if (this._skill.attris.level == 3) {
+                this._forceHigh = Math.round(1.05 * this._forceHigh);
+                this._forceLow  = Math.round(1.05 * this._forceLow);
+            }
+
+            if (this._skill.attris.level == 4) {
+                this._guardRadius = Math.round(1.05 * this._guardRadius);
+                this._forceHigh   = Math.round(1.05 * this._forceHigh);
+                this._forceLow    = Math.round(1.05 * this._forceLow);
+            }
+
+            if (this._skill.attris.level == 6) {
+                this._forceHigh = Math.round(1.1 * this._forceHigh);
+                this._forceLow  = Math.round(1.1 * this._forceLow);
+            }                
+        }
+    }
+    
     protected _findEnemy(): Enemy {
         return application.battle.findEnemy(this.getCenterX(), this.getCenterY(), this._guardRadius, [0, 1]);
     }
