fb35dc29046b22910ce8c94b6d5ea5c3df8ae7c2
liweihai <liweihai@huawei.com>
2016-11-09 11:49:39 +0800

    Update Bomb.ts

--- public/tower/src/sprites/bullets/Bomb.ts
+++ public/tower/src/sprites/bullets/Bomb.ts
@@ -1,16 +1,24 @@
 class Bomb extends CastBullet {
     protected _hitRadius: number;
+
+    protected _skill: Skill;
     
     public initialize(properties:any) {
         super.initialize(properties);
 
         this._hitRadius = this._get(properties, 'hitRadius', 50);
+        
+        this._skill = Skill.get("BombTower", 0);
     }
     
     protected _hitTarget() {
         let enemies = application.battle.findEnemies(this.getCenterX(), this.getBottomY(), this._hitRadius, [0]);
         for (let i = 0; i < enemies.length; i++) {
+           if (this._skill && this._skill.attrs.level == 4 && Math.random() <= 0.1) {
+               enemies[i].kill();
+           } else {
                enemies[i].shootBy(this);
+           }
         }
     }
 
