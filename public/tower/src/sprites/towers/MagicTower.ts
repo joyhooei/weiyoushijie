900852db085af40e394f229efe10bdc816e85981
liweihai <liweihai@huawei.com>
2016-11-09 12:00:37 +0800

    Update MagicTower.ts

--- public/tower/src/sprites/towers/MagicTower.ts
+++ public/tower/src/sprites/towers/MagicTower.ts
@@ -1,2 +1,11 @@
 class MagicTower extends ShootTower {
+    public initialize(properties:any) {
+        super.initialize(properties);
+        
+        if (this._skill) {
+            if (this._skill.attrs.level == 5) {
+                this._guardRadius = Math.round(this._guardRadius * 1.05);
+            }
+        }
+    } 
 }
